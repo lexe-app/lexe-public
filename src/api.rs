18324662e3492cd204fefe3add42ce932dcf42d9
Max Fang <protonmail.com@maxfa.ng>
2022-06-29 22:53:19 -0700

    api: Allow overriding base urls using env vars

--- src/api.rs
+++ src/api.rs
@@ -1,14 +1,35 @@
+use std::env;
+
 use http::Method;
+use once_cell::sync::{Lazy, OnceCell};
 use reqwest::Client;
 use serde::de::DeserializeOwned;
 use serde::{Deserialize, Serialize};
 use thiserror::Error;
+use BaseUrl::*;
 
 use crate::types::{Port, UserId};
 
-/// The base url for the node-backend (persistence) API
-const PERSIST: &str = "http://127.0.0.1:3030/v1";
-const RUNNER: &str = "http://127.0.0.1:5050";
+/// The base url for the node-backend (persistence) API.
+/// Can be overridden with BACKEND_URL env var.
+static BACKEND_URL: Lazy<OnceCell<String>> = Lazy::new(|| {
+    env::var("BACKEND_URL")
+        .unwrap_or("http://127.0.0.1:3030/v1".to_string())
+        .into()
+});
+
+/// The base url for the runner. Can be overridden with RUNNER_URL env var.
+static RUNNER_URL: Lazy<OnceCell<String>> = Lazy::new(|| {
+    env::var("RUNNER_URL")
+        .unwrap_or("http://127.0.0.1:5050".to_string())
+        .into()
+});
+
+/// Enumerates the base urls that can be used in an API call.
+enum BaseUrl {
+    Backend,
+    Runner,
+}
 
 #[derive(Error, Debug)]
 pub enum ApiError {
@@ -57,7 +78,7 @@ pub async fn get_node(
     cli: &Client,
     user_id: UserId,
 ) -> Result<Option<Node>, ApiError> {
-    request(cli, Method::GET, PERSIST, "/node", GetByUserId { user_id }).await
+    request(cli, Method::GET, Backend, "/node", GetByUserId { user_id }).await
 }
 
 #[derive(Serialize, Deserialize)]
@@ -76,7 +97,7 @@ pub async fn get_instance(
         user_id,
         measurement,
     };
-    request(cli, Method::GET, PERSIST, "/instance", req).await
+    request(cli, Method::GET, Backend, "/instance", req).await
 }
 
 #[derive(Serialize, Deserialize)]
@@ -95,7 +116,7 @@ pub async fn get_enclave(
         user_id,
         measurement,
     };
-    request(cli, Method::GET, PERSIST, "/enclave", req).await
+    request(cli, Method::GET, Backend, "/enclave", req).await
 }
 
 #[derive(Serialize, Deserialize)]
@@ -110,7 +131,7 @@ pub async fn create_node_instance_enclave(
     req: NodeInstanceEnclave,
 ) -> Result<NodeInstanceEnclave, ApiError> {
     let endpoint = "/acid/node_instance_enclave";
-    request(cli, Method::POST, PERSIST, endpoint, req).await
+    request(cli, Method::POST, Backend, endpoint, req).await
 }
 
 #[derive(Serialize, Deserialize)]
@@ -125,7 +146,7 @@ pub async fn create_channel_monitor(
     cli: &Client,
     req: ChannelMonitor,
 ) -> Result<ChannelMonitor, ApiError> {
-    request(cli, Method::POST, PERSIST, "/channel_monitor", req).await
+    request(cli, Method::POST, Backend, "/channel_monitor", req).await
 }
 
 pub async fn get_channel_monitors(
@@ -133,14 +154,14 @@ pub async fn get_channel_monitors(
     instance_id: String,
 ) -> Result<Vec<ChannelMonitor>, ApiError> {
     let req = GetByInstanceId { instance_id };
-    request(cli, Method::GET, PERSIST, "/channel_monitor", req).await
+    request(cli, Method::GET, Backend, "/channel_monitor", req).await
 }
 
 pub async fn update_channel_monitor(
     cli: &Client,
     req: ChannelMonitor,
 ) -> Result<ChannelMonitor, ApiError> {
-    request(cli, Method::PUT, PERSIST, "/channel_monitor", req).await
+    request(cli, Method::PUT, Backend, "/channel_monitor", req).await
 }
 
 #[derive(Serialize, Deserialize)]
@@ -154,14 +175,14 @@ pub async fn get_channel_manager(
     instance_id: String,
 ) -> Result<Option<ChannelManager>, ApiError> {
     let req = GetByInstanceId { instance_id };
-    request(cli, Method::GET, PERSIST, "/channel_manager", req).await
+    request(cli, Method::GET, Backend, "/channel_manager", req).await
 }
 
 pub async fn create_or_update_channel_manager(
     cli: &Client,
     req: ChannelManager,
 ) -> Result<ChannelManager, ApiError> {
-    request(cli, Method::PUT, PERSIST, "/channel_manager", req).await
+    request(cli, Method::PUT, Backend, "/channel_manager", req).await
 }
 
 #[derive(Serialize, Deserialize)]
@@ -175,14 +196,14 @@ pub async fn get_probabilistic_scorer(
     instance_id: String,
 ) -> Result<Option<ProbabilisticScorer>, ApiError> {
     let req = GetByInstanceId { instance_id };
-    request(cli, Method::GET, PERSIST, "/probabilistic_scorer", req).await
+    request(cli, Method::GET, Backend, "/probabilistic_scorer", req).await
 }
 
 pub async fn create_or_update_probabilistic_scorer(
     cli: &Client,
     ps: ProbabilisticScorer,
 ) -> Result<ProbabilisticScorer, ApiError> {
-    request(cli, Method::PUT, PERSIST, "/probabilistic_scorer", ps).await
+    request(cli, Method::PUT, Backend, "/probabilistic_scorer", ps).await
 }
 
 #[derive(Serialize, Deserialize)]
@@ -196,14 +217,14 @@ pub async fn get_network_graph(
     instance_id: String,
 ) -> Result<Option<NetworkGraph>, ApiError> {
     let req = GetByInstanceId { instance_id };
-    request(cli, Method::GET, PERSIST, "/network_graph", req).await
+    request(cli, Method::GET, Backend, "/network_graph", req).await
 }
 
 pub async fn create_or_update_network_graph(
     cli: &Client,
     ng: NetworkGraph,
 ) -> Result<NetworkGraph, ApiError> {
-    request(cli, Method::PUT, PERSIST, "/network_graph", ng).await
+    request(cli, Method::PUT, Backend, "/network_graph", ng).await
 }
 
 #[derive(Serialize, Deserialize)]
@@ -217,7 +238,7 @@ pub async fn create_channel_peer(
     cli: &Client,
     channel_peer: ChannelPeer,
 ) -> Result<ChannelPeer, ApiError> {
-    request(cli, Method::POST, PERSIST, "/channel_peer", channel_peer).await
+    request(cli, Method::POST, Backend, "/channel_peer", channel_peer).await
 }
 
 pub async fn get_channel_peers(
@@ -225,7 +246,7 @@ pub async fn get_channel_peers(
     instance_id: String,
 ) -> Result<Vec<ChannelPeer>, ApiError> {
     let req = GetByInstanceId { instance_id };
-    request(cli, Method::GET, PERSIST, "/channel_peer", req).await
+    request(cli, Method::GET, Backend, "/channel_peer", req).await
 }
 
 #[derive(Serialize, Deserialize)]
@@ -238,18 +259,22 @@ pub async fn notify_runner(
     cli: &Client,
     req: UserPort,
 ) -> Result<UserPort, ApiError> {
-    request(cli, Method::POST, RUNNER, "/ready", req).await
+    request(cli, Method::POST, Runner, "/ready", req).await
 }
 
 /// Builds and executes the API request
 async fn request<D: Serialize, T: DeserializeOwned>(
     cli: &Client,
     method: Method,
-    base_url: &str,
+    base_url: BaseUrl,
     endpoint: &str,
     data: D,
 ) -> Result<T, ApiError> {
-    let mut url = format!("{}{}", base_url, endpoint);
+    let base = match base_url {
+        BaseUrl::Backend => BACKEND_URL.get().unwrap(),
+        BaseUrl::Runner => RUNNER_URL.get().unwrap(),
+    };
+    let mut url = format!("{}{}", base, endpoint);
 
     // If GET, serialize the data in a query string
     let query_str = match method {
