2ced3eaa57077112f4539a08289492631416b7de
Philip Hayes <philiphayes9@gmail.com>
2022-07-06 15:59:51 -0700

    cert: test a successful TLS handshake using client/node certs
    + Fix an issue where the client cert didn't include a SubjectAltName.
      The client cert really shouldn't have a SAN, but the mTLS verifier
      expects one regardless, even though it ignores the contents (i think).

--- node/src/cert.rs
+++ node/src/cert.rs
@@ -59,6 +59,11 @@ impl CaCert {
 
         Ok(Self(rcgen::Certificate::from_params(params)?))
     }
+
+    /// Serialize and sign the CA cert. The CA cert is self-signed.
+    pub fn serialize_der_signed(&self) -> Result<Vec<u8>, RcgenError> {
+        self.0.serialize_der()
+    }
 }
 
 // -- impl ClientCert -- //
@@ -76,9 +81,26 @@ impl ClientCert {
         params.not_before = date_time_ymd(1975, 1, 1);
         params.not_after = date_time_ymd(4096, 1, 1);
         params.distinguished_name = name;
-        params.subject_alt_names = Vec::new();
+        // The client auth expects a subject alt name extension, even though it
+        // ignores it...
+        // TODO(phlip9): is there something cleaner we can do here?
+        params.subject_alt_names =
+            vec![SanType::DnsName("example.com".to_owned())];
 
         Ok(Self(rcgen::Certificate::from_params(params)?))
+    }
+
+    /// Serialize and sign the client cert. The client cert is signed by the
+    /// client CA.
+    pub fn serialize_der_signed(
+        &self,
+        ca: &CaCert,
+    ) -> Result<Vec<u8>, RcgenError> {
+        self.0.serialize_der_with_signer(&ca.0)
+    }
+
+    pub fn serialize_key_der(&self) -> Vec<u8> {
+        self.0.serialize_private_key_der()
     }
 }
 
@@ -109,5 +131,153 @@ impl NodeCert {
         params.subject_alt_names = subject_alt_names;
 
         Ok(Self(rcgen::Certificate::from_params(params)?))
+    }
+
+    /// Serialize and sign the node cert. The node cert is signed by the
+    /// client CA.
+    pub fn serialize_der_signed(
+        &self,
+        ca: &CaCert,
+    ) -> Result<Vec<u8>, RcgenError> {
+        self.0.serialize_der_with_signer(&ca.0)
+    }
+
+    pub fn serialize_key_der(&self) -> Vec<u8> {
+        self.0.serialize_private_key_der()
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use std::sync::Arc;
+
+    use futures::future::join;
+    use secrecy::Secret;
+    use tokio::io::{duplex, AsyncReadExt, AsyncWriteExt};
+    use tokio_rustls::rustls;
+
+    use super::*;
+    use crate::types::RootSeed;
+
+    #[test]
+    fn test_certs_parse_successfully() {
+        let ca_key_pair = ed25519::from_seed(&[0x11; 32]);
+        let ca_cert = CaCert::from_key_pair(ca_key_pair).unwrap();
+        let ca_cert_der = ca_cert.serialize_der_signed().unwrap();
+
+        let _ = webpki::TrustAnchor::try_from_cert_der(&ca_cert_der).unwrap();
+
+        let client_key_pair = ed25519::from_seed(&[0x22; 32]);
+        let client_cert = ClientCert::from_key_pair(client_key_pair).unwrap();
+        let client_cert_der =
+            client_cert.serialize_der_signed(&ca_cert).unwrap();
+
+        let _ = webpki::EndEntityCert::try_from(client_cert_der.as_slice())
+            .unwrap();
+
+        let node_key_pair = ed25519::from_seed(&[0x33; 32]);
+        let node_names = vec!["example.node.lexe.tech".to_owned()];
+        let node_cert =
+            NodeCert::from_key_pair(node_key_pair, node_names).unwrap();
+        let node_cert_der = node_cert.serialize_der_signed(&ca_cert).unwrap();
+
+        let _ =
+            webpki::EndEntityCert::try_from(node_cert_der.as_slice()).unwrap();
+    }
+
+    #[test]
+    fn test_tls_handshake_succeeds() {
+        // a fake pair of connected streams
+        let (client_stream, server_stream) = duplex(4096);
+
+        let seed = [0x42; 32];
+        let dns_name = "example.com";
+
+        // client tries to connect
+        let client = async move {
+            // should be able to independently derive CA key pair
+            let seed = RootSeed::new(Secret::new(seed));
+            let ca_key_pair = seed.derive_client_ca_key_pair();
+            let ca_cert = CaCert::from_key_pair(ca_key_pair).unwrap();
+            let ca_cert_der = ca_cert.serialize_der_signed().unwrap();
+
+            let client_key_pair = ed25519::from_seed(&[0x69; 32]);
+            let client_cert =
+                ClientCert::from_key_pair(client_key_pair).unwrap();
+            let client_cert_der =
+                client_cert.serialize_der_signed(&ca_cert).unwrap();
+            let client_key_der = client_cert.serialize_key_der();
+
+            let mut certs = rustls::RootCertStore::empty();
+            certs.add(&rustls::Certificate(ca_cert_der)).unwrap();
+
+            let config = rustls::ClientConfig::builder()
+                .with_safe_defaults()
+                .with_root_certificates(certs)
+                .with_single_cert(
+                    vec![rustls::Certificate(client_cert_der)],
+                    rustls::PrivateKey(client_key_der),
+                )
+                .unwrap();
+
+            let connector = tokio_rustls::TlsConnector::from(Arc::new(config));
+            let sni = rustls::ServerName::try_from(dns_name).unwrap();
+            let mut stream =
+                connector.connect(sni, client_stream).await.unwrap();
+
+            stream.write_all(b"hello").await.unwrap();
+            stream.flush().await.unwrap();
+            stream.shutdown().await.unwrap();
+
+            let mut resp = Vec::new();
+            stream.read_to_end(&mut resp).await.unwrap();
+
+            assert_eq!(&resp, b"goodbye");
+        };
+
+        // node should accept handshake
+        let node = async move {
+            // should be able to independently derive CA key pair
+            let seed = RootSeed::new(Secret::new(seed));
+            let ca_key_pair = seed.derive_client_ca_key_pair();
+            let ca_cert = CaCert::from_key_pair(ca_key_pair).unwrap();
+            let ca_cert_der = ca_cert.serialize_der_signed().unwrap();
+
+            let node_key_pair = ed25519::from_seed(&[0xf0; 32]);
+            let dns_names = vec![dns_name.to_owned()];
+            let node_cert =
+                NodeCert::from_key_pair(node_key_pair, dns_names).unwrap();
+            let node_cert_der =
+                node_cert.serialize_der_signed(&ca_cert).unwrap();
+            let node_key_der = node_cert.serialize_key_der();
+
+            let mut certs = rustls::RootCertStore::empty();
+            certs.add(&rustls::Certificate(ca_cert_der)).unwrap();
+
+            let client_verifier =
+                rustls::server::AllowAnyAuthenticatedClient::new(certs);
+            let config = rustls::ServerConfig::builder()
+                .with_safe_defaults()
+                // .with_no_client_auth()
+                .with_client_cert_verifier(client_verifier)
+                .with_single_cert(
+                    vec![rustls::Certificate(node_cert_der)],
+                    rustls::PrivateKey(node_key_der),
+                )
+                .unwrap();
+
+            let acceptor = tokio_rustls::TlsAcceptor::from(Arc::new(config));
+            let mut stream = acceptor.accept(server_stream).await.unwrap();
+
+            let mut req = Vec::new();
+            stream.read_to_end(&mut req).await.unwrap();
+
+            assert_eq!(&req, b"hello");
+
+            stream.write_all(b"goodbye").await.unwrap();
+            stream.shutdown().await.unwrap();
+        };
+
+        futures::executor::block_on(join(client, node));
     }
 }
