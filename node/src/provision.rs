861cde580706637b56a52d74173adb12d8803408
Philip Hayes <philiphayes9@gmail.com>
2022-07-08 14:05:35 -0700

    provision: get test client to successfully make provision call
    + one issue: I unpleasantly remember again that `tokio::time::pause` has
      an unconfigurable auto-advance, which causes the timeout to trigger
      before the provision service has a chance to do anything... sometimes...
      For now, I've just disabled the paused time. Eventually I might look
      into using my old `TimeService`...

--- node/src/provision.rs
+++ node/src/provision.rs
@@ -21,7 +21,7 @@ use std::convert::Infallible;
 use std::net::SocketAddr;
 use std::time::Duration;
 
-use anyhow::{format_err, Context, Result};
+use anyhow::{Context, Result};
 use async_trait::async_trait;
 use http::{Response, StatusCode};
 use rcgen::date_time_ymd;
@@ -196,22 +196,31 @@ pub async fn provision<R: Runner>(
         )
         .context("Failed to build TLS config")?;
 
-    // we'll trigger this when we've completed the provisioning process
+    // we'll trigger `shutdown_tx` when we've completed the provisioning process
     let (shutdown_tx, mut shutdown_rx) = mpsc::channel(1);
+    let shutdown = async move {
+        // don't wait forever for the client
+        match tokio::time::timeout(PROVISION_TIMEOUT, shutdown_rx.recv()).await
+        {
+            Ok(_) => {
+                println!("provision: received shutdown");
+            }
+            Err(_) => {
+                println!("provision: timeout");
+            }
+        }
+    };
 
-    // Bind TCP listener on port (queues up any inbound connections).
+    // bind TCP listener on port (queues up any inbound connections).
     let addr = SocketAddr::from(([127, 0, 0, 1], args.port));
     let routes = provision_routes(shutdown_tx);
     let (listen_addr, service) = warp::serve(routes)
         .tls()
         .preconfigured_tls(tls_config)
-        .bind_ephemeral(addr);
+        .bind_with_graceful_shutdown(addr, shutdown);
     let port = listen_addr.port();
 
-    println!("provision: listening on port {port}");
-
-    // don't wait forever for the client
-    let timeout = tokio::time::sleep(PROVISION_TIMEOUT);
+    println!("provision: listening on {listen_addr}");
 
     // notify the runner that we're ready for a client connection
     runner
@@ -220,31 +229,105 @@ pub async fn provision<R: Runner>(
         .context("Failed to notify runner of our readiness")?;
 
     println!("provision: notified runner; awaiting client connection");
+    service.await;
 
-    tokio::select! {
-        // done provisioning
-        _ = shutdown_rx.recv() => {
-            Ok(())
-        }
-        _ = timeout => {
-            Err(format_err!("Timeout: Client failed to provision in time"))
-        }
-        _ = service => {
-            Err(format_err!("warp provisioning service future should never resolve"))
-        }
-    }
+    println!("provision: done provisioning");
+    Ok(())
 }
 
 #[cfg(test)]
 mod test {
+    use std::sync::Arc;
+    use std::time::SystemTime;
+
     use secrecy::Secret;
+    use tokio::sync::mpsc;
+    use tokio_rustls::rustls::client::{
+        ServerCertVerified, ServerCertVerifier,
+    };
+    use tokio_rustls::rustls::{Certificate, ServerName};
 
     use super::*;
     use crate::cli;
 
-    #[tokio::test(start_paused = true)]
-    #[ignore]
+    // TODO(phlip9): move
+    mod logger {
+        use std::str::FromStr;
+
+        use tracing::Level;
+
+        pub fn init_for_testing() {
+            let level = std::env::var("RUST_LOG")
+                .ok()
+                .and_then(|rust_log| Level::from_str(&rust_log).ok())
+                .unwrap_or(Level::INFO);
+
+            let _ = tracing_subscriber::fmt()
+                .without_time()
+                .with_max_level(level)
+                .try_init();
+        }
+    }
+
+    struct AttestCertVerifier;
+
+    impl ServerCertVerifier for AttestCertVerifier {
+        fn verify_server_cert(
+            &self,
+            end_entity: &Certificate,
+            intermediates: &[Certificate],
+            server_name: &ServerName,
+            scts: &mut dyn Iterator<Item = &[u8]>,
+            ocsp_response: &[u8],
+            now: SystemTime,
+        ) -> Result<ServerCertVerified, rustls::Error> {
+            println!("client: verify_server_cert");
+
+            // there should be no intermediate certs
+            if !intermediates.is_empty() {
+                return Err(rustls::Error::General(
+                    "received unexpected intermediate certs".to_owned(),
+                ));
+            }
+
+            // verify the self-signed cert "normally"; ensure everything is
+            // well-formed, signatures verify, validity ranges OK, SNI matches,
+            // etc...
+            let mut trust_roots = rustls::RootCertStore::empty();
+            trust_roots.add(end_entity).map_err(|err| {
+                rustls::Error::InvalidCertificateData(err.to_string())
+            })?;
+
+            let ct_policy = None;
+            let webpki_verifier =
+                rustls::client::WebPkiVerifier::new(trust_roots, ct_policy);
+
+            let verified_token = webpki_verifier.verify_server_cert(
+                end_entity,
+                &[],
+                server_name,
+                scts,
+                ocsp_response,
+                now,
+            )?;
+
+            // TODO(phlip9): parse quote
+
+            // 1. parse out subjectPublicKeyInfo
+            // 2. deserialize from pkcs#8 to ed25519 pubkey
+            // 3. parse out SgxAttestation extension
+            // 4. (if not dev mode) parse out quote and quote report
+            // 5. (if not dev mode) ensure report contains the pubkey hash
+            // 6. (if not dev mode) verify quote and quote report
+
+            Ok(verified_token)
+        }
+    }
+
+    #[tokio::test]
     async fn test_provision() {
+        logger::init_for_testing();
+
         struct MockRunner(mpsc::Sender<UserPort>);
 
         #[async_trait]
@@ -261,7 +344,7 @@ mod test {
         }
 
         let user_id: UserId = 123;
-        let node_dns_name = "node.example.com";
+        let node_dns_name = "localhost";
 
         let args = cli::ProvisionCommand {
             user_id,
@@ -274,22 +357,34 @@ mod test {
         let provision_task = provision(args, runner);
 
         let test_task = async {
+            // runner recv ready notification w/ listening port
             let runner_req = runner_req_rx.recv().await.unwrap();
             assert_eq!(runner_req.user_id, user_id);
             let port = runner_req.port;
 
+            println!("runner: received ready notification: port: {port}");
+
+            let mut tls_config = rustls::ClientConfig::builder()
+                .with_safe_defaults()
+                .with_custom_certificate_verifier(Arc::new(AttestCertVerifier))
+                .with_no_client_auth();
+            tls_config.alpn_protocols = vec!["h2".into(), "http/1.1".into()];
+
+            // client sends provision request to node
             let provision_req = ProvisionRequest {
                 root_seed: RootSeed::new(Secret::new([0x42; 32])),
             };
-
-            // TODO(phlip9): client TLS config
-            let client = reqwest::Client::builder().build().unwrap();
-            client
-                .post(format!("https://127.0.0.1:{port}/provision"))
+            let client = reqwest::Client::builder()
+                .use_preconfigured_tls(tls_config)
+                .build()
+                .unwrap();
+            let resp = client
+                .post(format!("https://localhost:{port}/provision"))
                 .json(&provision_req)
                 .send()
                 .await
                 .unwrap();
+            assert!(resp.status().is_success());
         };
 
         let (_, _) = tokio::join!(provision_task, test_task);
