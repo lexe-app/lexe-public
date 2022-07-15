0803bc0079ba0611dc62c63b29ad45d77f9db507
Philip Hayes <philiphayes9@gmail.com>
2022-07-15 04:32:22 +0000

    attest: successfully verifying real SGX Quote up to AK sig

--- common/src/attest/verify.rs
+++ common/src/attest/verify.rs
@@ -1,12 +1,54 @@
 //! Verify remote attestation endorsements directly or embedded in x509 certs.
 
+use std::io::Cursor;
 use std::time::SystemTime;
+use std::{fmt, include_bytes};
 
+use anyhow::{bail, ensure, format_err, Context, Result};
 use asn1_rs::FromDer;
+use dcap_ql::quote::{
+    Qe3CertDataPckCertChain, Quote, Quote3SignatureEcdsaP256,
+};
+use once_cell::sync::Lazy;
+use webpki::{TlsServerTrustAnchors, TrustAnchor};
 use x509_parser::certificate::X509Certificate;
 
 use crate::attest::cert::SgxAttestationExtension;
 use crate::ed25519;
+
+/// The DER-encoded Intel SGX trust anchor cert.
+const INTEL_SGX_ROOT_CA_CERT_DER: &[u8] =
+    include_bytes!("../../data/intel-sgx-root-ca.der");
+
+/// Lazily parse the Intel SGX trust anchor cert.
+///
+/// NOTE: It's easier to inline the cert DER bytes vs. PEM file, otherwise the
+/// `TrustAnchor` tries to borrow from a temporary `Vec<u8>`.
+static INTEL_SGX_TRUST_ANCHOR: Lazy<[TrustAnchor<'static>; 1]> =
+    Lazy::new(|| {
+        let trust_anchor = TrustAnchor::try_from_cert_der(
+            INTEL_SGX_ROOT_CA_CERT_DER,
+        )
+        .expect("Failed to deserialize Intel SGX root CA cert from der bytes");
+
+        [trust_anchor]
+    });
+
+/// From: <https://github.com/rustls/rustls/blob/v/0.20.6/rustls/src/verify.rs#L22>
+static SUPPORTED_SIG_ALGS: &[&webpki::SignatureAlgorithm] = &[
+    &webpki::ECDSA_P256_SHA256,
+    &webpki::ECDSA_P256_SHA384,
+    &webpki::ECDSA_P384_SHA256,
+    &webpki::ECDSA_P384_SHA384,
+    &webpki::ED25519,
+    &webpki::RSA_PSS_2048_8192_SHA256_LEGACY_KEY,
+    &webpki::RSA_PSS_2048_8192_SHA384_LEGACY_KEY,
+    &webpki::RSA_PSS_2048_8192_SHA512_LEGACY_KEY,
+    &webpki::RSA_PKCS1_2048_8192_SHA256,
+    &webpki::RSA_PKCS1_2048_8192_SHA384,
+    &webpki::RSA_PKCS1_2048_8192_SHA512,
+    &webpki::RSA_PKCS1_3072_8192_SHA384,
+];
 
 pub struct AttestEvidence<'a> {
     pub cert_pubkey: ed25519::PublicKey,
@@ -132,9 +174,221 @@ impl rustls::client::ServerCertVerifier for ServerCertVerifier {
     }
 }
 
+pub struct SgxQuoteVerifier {
+    pub expect_dummy_quote: bool,
+    pub now: SystemTime,
+}
+
+impl SgxQuoteVerifier {
+    pub fn verify(&self, quote: &[u8], _qe3_report: &[u8]) -> Result<()> {
+        let quote = Quote::parse(quote)
+            .map_err(DisplayErr::new)
+            .context("Failed to parse SGX Quote")?;
+
+        let sig = quote
+            .signature::<Quote3SignatureEcdsaP256>()
+            .map_err(DisplayErr::new)
+            .context("Failed to parse SGX ECDSA Quote signature")?;
+
+        let cert_chain_pem = sig
+            .certification_data::<Qe3CertDataPckCertChain>()
+            .map_err(DisplayErr::new)
+            .context("Failed to parse PCK cert chain")?
+            .certs;
+
+        // 1. Verify the local SGX platform PCK cert is endorsed by the Intel
+        //    SGX trust root CA.
+
+        // [0] : PCK cert
+        // [1] : PCK platform cert
+        // [2] : SGX root CA cert
+        ensure!(
+            cert_chain_pem.len() == 3,
+            "unexpected number of certificates"
+        );
+
+        let pck_cert_der = parse_cert_pem_to_der(&cert_chain_pem[0])
+            .context("Failed to parse PCK cert PEM string")?;
+        let pck_platform_cert_der =
+            parse_cert_pem_to_der(&cert_chain_pem[1])
+                .context("Failed to parse PCK platform cert PEM string")?;
+
+        let pck_cert = webpki::EndEntityCert::try_from(pck_cert_der.as_slice())
+            .context("Invalid PCK cert")?;
+
+        let now = webpki::Time::try_from(self.now)
+            .context("Our time source is bad")?;
+
+        // We don't use the full `rustls::WebPkiVerifier` here since the PCK
+        // certs aren't truly webpki certs, as they don't bind to DNS names.
+        //
+        // Instead, we skip the DNS checks by using the lower-level `EndEntity`
+        // verification methods directly.
+
+        pck_cert
+            .verify_is_valid_tls_server_cert(
+                SUPPORTED_SIG_ALGS,
+                &TlsServerTrustAnchors(&*INTEL_SGX_TRUST_ANCHOR),
+                &[&pck_platform_cert_der],
+                now,
+            )
+            .context("PCK cert chain failed validation")?;
+
+        let qe3_sig = get_ecdsa_sig_der(sig.qe3_signature())?;
+        let qe3_report_bytes = sig.qe3_report();
+
+        // 2. Verify the Platform Certification Enclave (PCE) endorses the
+        //    Quoting Enclave (QE) Report.
+
+        // TODO(phlip9): parse cert alg?
+        verify_sig_using_any_alg(&pck_cert, qe3_report_bytes, &qe3_sig)
+            .context(
+                "PCK cert's signature on the Quoting Enclave Report is invalid",
+            )?;
+
+        // 3. Verify the local Quoting Enclave's Report binds to its attestation
+        //    pubkey, which it uses to sign application enclave Reports.
+
+        let expected_report_data = sha256_parts(&[
+            sig.attestation_public_key(),
+            sig.authentication_data(),
+        ]);
+
+        let qe3_report = report_try_from_truncated(qe3_report_bytes)
+            .context("Invalid QE Report")?;
+
+        ensure!(
+            &qe3_report.reportdata[..32] == expected_report_data.as_ref(),
+            "Quoting Enclave's Report data doesn't match the Quote attestation pubkey",
+        );
+        ensure!(
+            qe3_report.reportdata[32..] == [0u8; 32],
+            "Quoting Enclave's Report contains unrecognized data",
+        );
+
+        // TODO(phlip9): verify QE identity
+        // TODO(phlip9): verify Quote signature signed by attestation pubkey
+        // TODO(phlip9): extract application enclave report
+
+        Ok(())
+    }
+}
+
+// dumb error type compatibility hack so we can propagate `failure::Fail` errors
+
+#[derive(Debug)]
+struct DisplayErr(String);
+
+impl DisplayErr {
+    fn new(err: impl fmt::Display) -> Self {
+        Self(format!("{:#}", err))
+    }
+}
+
+impl fmt::Display for DisplayErr {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.write_str(&self.0)
+    }
+}
+
+impl std::error::Error for DisplayErr {
+    fn description(&self) -> &str {
+        &self.0
+    }
+}
+
+fn parse_cert_pem_to_der(s: &str) -> Result<Vec<u8>> {
+    let mut cursor = Cursor::new(s.as_bytes());
+
+    let item = rustls_pemfile::read_one(&mut cursor)
+        .context("Expected at least one entry in the PEM file")?
+        .ok_or_else(|| format_err!("Not valid PEM-encoded cert"))?;
+
+    match item {
+        rustls_pemfile::Item::X509Certificate(der) => Ok(der),
+        _ => bail!("Not an x509 certificate PEM label"),
+    }
+}
+
+// TODO(phlip9): avoid using this...
+// From: <https://github.com/rustls/rustls/blob/v/0.20.6/rustls/src/verify.rs#L658>
+fn verify_sig_using_any_alg(
+    cert: &webpki::EndEntityCert,
+    message: &[u8],
+    sig: &[u8],
+) -> Result<(), webpki::Error> {
+    // TLS doesn't itself give us enough info to map to a single
+    // webpki::SignatureAlgorithm. Therefore, convert_algs maps to several
+    // and we try them all.
+    for alg in SUPPORTED_SIG_ALGS {
+        match cert.verify_signature(alg, message, sig) {
+            Err(webpki::Error::UnsupportedSignatureAlgorithmForPublicKey) => {
+                continue
+            }
+            res => return res,
+        }
+    }
+
+    Err(webpki::Error::UnsupportedSignatureAlgorithmForPublicKey)
+}
+
+/// Convert a (presumably) fixed `r || s` ECDSA signature to ASN.1 format
+///
+/// From: <https://github.com/fortanix/rust-sgx/blob/3fea4337f774fe9563a62352ce62d3cad7af746d/intel-sgx/dcap-ql/src/quote.rs#L212>
+/// See: [RFC 3279 2.2.3](https://datatracker.ietf.org/doc/html/rfc3279#section-2.2.3)
+///
+/// ```asn.1
+/// Ecdsa-Sig-Value ::= SEQUENCE {
+///     r INTEGER,
+///     s INTEGER
+/// }
+/// ```
+fn get_ecdsa_sig_der(sig: &[u8]) -> Result<Vec<u8>> {
+    if sig.len() % 2 != 0 {
+        bail!("sig not even: {}", sig.len());
+    }
+
+    // TODO(phlip9): avoid `num` dependency...
+    let (r_bytes, s_bytes) = sig.split_at(sig.len() / 2);
+    let r = num_bigint::BigUint::from_bytes_be(r_bytes);
+    let s = num_bigint::BigUint::from_bytes_be(s_bytes);
+
+    let der = yasna::construct_der(|writer| {
+        writer.write_sequence(|writer| {
+            writer.next().write_biguint(&r);
+            writer.next().write_biguint(&s);
+        })
+    });
+
+    Ok(der)
+}
+
+fn report_try_from_truncated(bytes: &[u8]) -> Result<sgx_isa::Report> {
+    use sgx_isa::Report;
+
+    let len = bytes.len();
+    let expected_len = Report::TRUNCATED_SIZE;
+    ensure!(
+        len == expected_len,
+        "report has the wrong size: {len}, expected: {expected_len}",
+    );
+
+    let mut unpadded = vec![0u8; Report::UNPADDED_SIZE];
+    unpadded[..Report::TRUNCATED_SIZE].copy_from_slice(bytes);
+
+    Ok(Report::try_copy_from(&unpadded).expect("Should never fail"))
+}
+
+fn sha256_parts(inputs: &[&[u8]]) -> ring::digest::Digest {
+    let mut ctx = ring::digest::Context::new(&ring::digest::SHA256);
+    for input in inputs {
+        ctx.update(input);
+    }
+    ctx.finish()
+}
+
 #[cfg(test)]
 mod test {
-    use std::io::Cursor;
     use std::time::Duration;
     use std::{include_str, iter};
 
@@ -149,24 +403,33 @@ mod test {
     const SGX_SERVER_CERT_PEM: &str =
         include_str!("../../test_data/attest_cert.pem");
 
-    fn parse_cert_pem_as_der(s: &str) -> Vec<u8> {
-        let mut cursor = Cursor::new(s.as_bytes());
+    const INTEL_SGX_ROOT_CA_CERT_PEM: &str =
+        include_str!("../../test_data/intel-sgx-root-ca.pem");
 
-        let item = rustls_pemfile::read_one(&mut cursor)
-            .expect("Expected at least one entry in the PEM file")
-            .expect("Not valid PEM-encoded cert");
+    #[test]
+    fn test_intel_sgx_trust_anchor_der_pem_equal() {
+        let sgx_trust_anchor_der = INTEL_SGX_ROOT_CA_CERT_DER;
+        let sgx_trust_anchor_pem =
+            parse_cert_pem_to_der(INTEL_SGX_ROOT_CA_CERT_PEM).unwrap();
+        assert_eq!(sgx_trust_anchor_der, sgx_trust_anchor_pem);
 
-        match item {
-            rustls_pemfile::Item::X509Certificate(der) => der,
-            _ => panic!("Not an x509 certificate: {:?}", item),
-        }
+        // this should not panic
+        let _sgx_trust_anchor = &*INTEL_SGX_TRUST_ANCHOR;
     }
 
     #[test]
     fn test_verify_sgx_server_cert() {
-        let cert_der = parse_cert_pem_as_der(SGX_SERVER_CERT_PEM);
-        let _evidence = AttestEvidence::parse_cert_der(&cert_der).unwrap();
+        let cert_der = parse_cert_pem_to_der(SGX_SERVER_CERT_PEM).unwrap();
+        let evidence = AttestEvidence::parse_cert_der(&cert_der).unwrap();
         let _expected_mrenclave = hex::decode(MRENCLAVE_HEX.trim()).unwrap();
+
+        let verifier = SgxQuoteVerifier {
+            expect_dummy_quote: false,
+            now: SystemTime::now(),
+        };
+        verifier
+            .verify(&evidence.attest.quote, &evidence.attest.qe_report)
+            .unwrap();
     }
 
     #[test]
