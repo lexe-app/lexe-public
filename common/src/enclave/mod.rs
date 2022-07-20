509452ba381113526cc07421878b09004471a5d5
Philip Hayes <philiphayes9@gmail.com>
2022-07-20 00:30:24 +0000

    sealing: add SGX unsealing. use KeyRequest newtype

--- common/src/enclave/mod.rs
+++ common/src/enclave/mod.rs
@@ -10,9 +10,11 @@ mod sgx;
 mod mock;
 
 use std::borrow::Cow;
+use std::fmt;
 
 use thiserror::Error;
 
+use crate::hex;
 use crate::rng::Crng;
 
 #[derive(Debug, Error)]
@@ -47,29 +49,39 @@ impl From<ring::error::Unspecified> for Error {
 pub struct Sealed<'a> {
     /// A truncated key request
     keyrequest: Cow<'a, [u8]>,
-    /// Include the sealing enclave's attributes and miscselect flags so we can
-    /// tell that some sealed data won't be unsealable (rather than a wrong key
-    /// error).
-    attributes: [u64; 2],
-    miscselect: u32,
     /// Encrypted ciphertext
     ciphertext: Cow<'a, [u8]>,
 }
 
+impl fmt::Debug for Sealed<'_> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("Sealed")
+            .field("keyrequest", &hex::display(&self.keyrequest))
+            .field("ciphertext", &hex::display(&self.ciphertext))
+            .finish()
+    }
+}
+
 pub fn seal<'a>(
     rng: &mut dyn Crng,
-    label: [u8; 16],
+    label: &[u8],
     data: &[u8], // TODO(phlip9): allow sealing in-place
 ) -> Result<Sealed<'a>, Error> {
-    #[cfg(target_env = "sgx")]
-    let result = sgx::seal(rng, label, data);
-
     #[cfg(not(target_env = "sgx"))]
     let result = mock::seal(rng, label, data);
+
+    #[cfg(target_env = "sgx")]
+    let result = sgx::seal(rng, label, data);
 
     result
 }
 
-pub fn unseal(_label: [u8; 16], _sealed: Sealed<'_>) -> Result<Vec<u8>, Error> {
-    Err(Error::Other)
+pub fn unseal(label: &[u8], sealed: Sealed<'_>) -> Result<Vec<u8>, Error> {
+    #[cfg(not(target_env = "sgx"))]
+    let result = mock::unseal(label, sealed);
+
+    #[cfg(target_env = "sgx")]
+    let result = sgx::unseal(label, sealed);
+
+    result
 }
