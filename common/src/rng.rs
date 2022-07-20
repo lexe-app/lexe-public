4c43ca34d39575caa7635a83af6103977e865ce1
Philip Hayes <philiphayes9@gmail.com>
2022-07-20 01:17:10 +0000

    rng: add `SmallRng` for testing

--- common/src/rng.rs
+++ common/src/rng.rs
@@ -2,7 +2,8 @@
 
 use std::num::NonZeroU32;
 
-use rand_core::{CryptoRng, RngCore};
+use rand_core::le::read_u32_into;
+use rand_core::{CryptoRng, RngCore, SeedableRng};
 use ring::rand::SecureRandom;
 
 const RAND_ERROR_CODE: NonZeroU32 =
@@ -33,10 +34,12 @@ impl Default for SysRng {
 impl CryptoRng for SysRng {}
 
 impl RngCore for SysRng {
+    #[inline]
     fn next_u32(&mut self) -> u32 {
         rand_core::impls::next_u32_via_fill(self)
     }
 
+    #[inline]
     fn next_u64(&mut self) -> u64 {
         rand_core::impls::next_u64_via_fill(self)
     }
@@ -55,4 +58,99 @@ impl RngCore for SysRng {
             // empty/unspecified anyway, so not a big deal.
             .map_err(|_| rand_core::Error::from(RAND_ERROR_CODE))
     }
+}
+
+/// A small, fast, _non-cryptographic_ rng with decent statistical properties.
+/// Useful for sampling non-security sensitive data or as a deterministic RNG
+/// for tests (instead of the [`SysRng`] above, which uses the global OS RNG).
+///
+/// The implementation is the same as [`Xoroshiro64Star`].
+///
+/// [`Xoroshiro64Star`]: https://github.com/rust-random/rngs/blob/master/rand_xoshiro/src/xoroshiro64star.rs
+#[derive(Debug)]
+pub struct SmallRng {
+    s0: u32,
+    s1: u32,
+}
+
+impl SmallRng {
+    pub fn new() -> Self {
+        Self {
+            s0: 0xdeadbeef,
+            s1: 0xf00baa44,
+        }
+    }
+}
+
+impl Default for SmallRng {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+// TODO(phlip9): use a feature flag to enable this for users outside the crate?
+/// Only enable [`CryptoRng`] for this rng when testing.
+#[cfg(test)]
+impl CryptoRng for SmallRng {}
+
+impl RngCore for SmallRng {
+    #[inline]
+    fn next_u32(&mut self) -> u32 {
+        let r = self.s0.wrapping_mul(0x9e3779bb);
+        self.s1 ^= self.s0;
+        self.s0 = self.s0.rotate_left(26) ^ self.s1 ^ (self.s1 << 9);
+        self.s1 = self.s1.rotate_left(13);
+        r
+    }
+
+    #[inline]
+    fn next_u64(&mut self) -> u64 {
+        rand_core::impls::next_u64_via_u32(self)
+    }
+
+    #[inline]
+    fn fill_bytes(&mut self, dest: &mut [u8]) {
+        rand_core::impls::fill_bytes_via_next(self, dest);
+    }
+
+    #[inline]
+    fn try_fill_bytes(
+        &mut self,
+        dest: &mut [u8],
+    ) -> Result<(), rand_core::Error> {
+        self.fill_bytes(dest);
+        Ok(())
+    }
+}
+
+impl SeedableRng for SmallRng {
+    type Seed = [u8; 8];
+
+    fn from_seed(seed: Self::Seed) -> Self {
+        // zero is a pathological case for Xoroshiro64Star, just map it to
+        // the default seed
+        if seed == [0u8; 8] {
+            Self::new()
+        } else {
+            let mut parts = [0u32, 0u32];
+            read_u32_into(&seed, &mut parts);
+            Self {
+                s0: parts[0],
+                s1: parts[1],
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+pub fn arb_rng() -> impl proptest::strategy::Strategy<Value = SmallRng> {
+    use proptest::arbitrary::any;
+    use proptest::strategy::Strategy;
+
+    // We use `no_shrink` here since shrinking an RNG seed won't produce
+    // "simpler" output samples. This setting lets `proptest` know not to waste
+    // time trying to shrink the rng seed.
+    let arb_seed = any::<[u8; 8]>().no_shrink();
+
+    arb_seed.prop_map(SmallRng::from_seed)
 }
