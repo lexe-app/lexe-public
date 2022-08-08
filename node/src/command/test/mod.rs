8c223155e8ece703399129c0d758c1db96d21aa3
Max Fang <protonmail.com@maxfa.ng>
2022-08-08 16:58:44 -0700

    node(test): Hack around the bitcoind exe path error

--- node/src/command/test/mod.rs
+++ node/src/command/test/mod.rs
@@ -55,6 +55,39 @@ fn default_args_for_user(user_pk: UserPk) -> StartArgs {
     }
 }
 
+/// Hacks around the recurring 'No such file or directory' error when trying to
+/// locate the local bitcoind executable.
+///
+/// https://github.com/RCasatta/bitcoind/issues/77
+fn bitcoind_exe_path() -> String {
+    use std::env;
+    // "/Users/fang/lexe/client/target/debug/build/bitcoind-65c3b20abafd4893/out/bitcoin/bitcoin-22.0/bin/bitcoind"
+    // The path prior to `target` is wrong, everything after is correct
+    let bitcoind_path = bitcoind::downloaded_exe_path()
+        .expect("Didn't specify bitcoind version in feature flags");
+
+    // Construct the workspace path based on env::current_dir()
+    // "/Users/fang/lexe/dev/client/node"
+    let crate_dir = env::current_dir().unwrap();
+    // "/Users/fang/lexe/dev/client"
+    let workspace_dir = crate_dir.parent().unwrap().to_str().unwrap();
+
+    // Split on `target` to grab the correct half of the bitcoind_path string
+    let mut path_halves = bitcoind_path.split("target");
+    let _wrong_half = path_halves.next();
+    // "/debug/build/bitcoind-65c3b20abafd4893/out/bitcoin/bitcoin-22.0/bin/bitcoind"
+    let right_half = path_halves.next().unwrap();
+
+    let exe_path = format!("{workspace_dir}/target{right_half}");
+
+    dbg!(&bitcoind_path);
+    dbg!(&crate_dir);
+    dbg!(&workspace_dir);
+    dbg!(&exe_path);
+
+    exe_path
+}
+
 struct CommandTestHarness {
     bitcoind: BitcoinD,
     node: LexeNode,
@@ -69,8 +102,7 @@ impl CommandTestHarness {
         conf.args.push("-rpcauth=kek:b6c15926aee7ebfbd3669ec8a6515c79$2dba596a7d651187021b1f56d339f0fe465c2ab1b81c37b05e07a320b07822d7");
 
         // Init bitcoind
-        let exe_path = bitcoind::downloaded_exe_path()
-            .expect("Didn't specify bitcoind version in feature flags");
+        let exe_path = bitcoind_exe_path();
         let bitcoind = BitcoinD::with_conf(exe_path, &conf)
             .expect("Failed to init bitcoind");
         let host = bitcoind.params.rpc_socket.ip().to_string();
