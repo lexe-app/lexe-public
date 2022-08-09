3b4e11fe8b6e7ac100e0e96a5b313025d9206511
Philip Hayes <philiphayes9@gmail.com>
2022-08-09 11:26:48 -0700

    chore: move quote gen and node tls config to `common` crate

--- node/src/attest/mod.rs
+++
@@ -1,3 +0,0 @@
-mod quote;
-
-pub use quote::quote_enclave;
--- node/src/provision.rs
+++ node/src/provision.rs
@@ -12,39 +12,36 @@
 //! that they trust and the software is running inside an up-to-date secure
 //! enclave. We do this using a variant of RA-TLS (Remote Attestation TLS),
 //! where the enclave platform endorsements and enclave measurements are bundled
-//! into a self-signed TLS certificate, which users must verify when connecting
-//! to the provisioning endpoint.
+//! in&&to a self-signed TLS certificate, which users must verify when
+//! connecting to the provisioning endpoint.
 
 #![allow(dead_code)]
 
 use std::convert::Infallible;
 use std::net::SocketAddr;
-use std::time::{Duration, Instant};
+use std::time::Duration;
 
-use anyhow::{ensure, Context, Result};
+use anyhow::{ensure, Context};
 use bitcoin::secp256k1::PublicKey;
 use common::api::provision::{
     Instance, Node, NodeInstanceSeed, ProvisionRequest, SealedSeed,
 };
 use common::api::runner::UserPort;
 use common::api::UserPk;
-use common::attest::cert::AttestationCert;
 use common::cli::ProvisionArgs;
+use common::client::tls::node_provision_tls_config;
 use common::enclave::{self, MachineId, Measurement, Sealed, MIN_SGX_CPUSVN};
 use common::rng::{Crng, SysRng};
 use common::root_seed::RootSeed;
-use common::{ed25519, hex};
 use http::{Response, StatusCode};
 use secrecy::ExposeSecret;
 use thiserror::Error;
 use tokio::sync::mpsc;
-use tokio_rustls::rustls;
 use tracing::{debug, info, instrument, warn};
 use warp::hyper::Body;
 use warp::reject::Reject;
 use warp::{Filter, Rejection, Reply};
 
-use crate::attest;
 use crate::types::ApiClientType;
 
 const PROVISION_TIMEOUT: Duration = Duration::from_secs(10);
@@ -93,13 +90,13 @@ struct ProvisionedSecrets {
 impl ProvisionedSecrets {
     const LABEL: &'static [u8] = b"provisioned secrets";
 
-    fn seal(&self, rng: &mut dyn Crng) -> Result<Sealed<'_>> {
+    fn seal(&self, rng: &mut dyn Crng) -> anyhow::Result<Sealed<'_>> {
         let root_seed_ref = self.root_seed.expose_secret().as_slice();
         enclave::seal(rng, Self::LABEL, root_seed_ref.into())
             .context("Failed to seal provisioned secrets")
     }
 
-    fn unseal(sealed: Sealed<'_>) -> Result<Self> {
+    fn unseal(sealed: Sealed<'_>) -> anyhow::Result<Self> {
         let bytes = enclave::unseal(Self::LABEL, sealed)
             .context("Failed to unseal provisioned secrets")?;
         let root_seed = RootSeed::try_from(bytes.as_slice())
@@ -112,7 +109,7 @@ fn verify_provision_request<R: Crng>(
     rng: &mut R,
     expected_user_id: UserPk,
     req: ProvisionRequest,
-) -> Result<(UserPk, PublicKey, ProvisionedSecrets)> {
+) -> anyhow::Result<(UserPk, PublicKey, ProvisionedSecrets)> {
     ensure!(req.user_pk == expected_user_id);
 
     let derived_node_pk =
@@ -203,58 +200,16 @@ fn provision_routes(
 /// which user we should provision to and have a simple method to authenticate
 /// their connection.
 #[instrument(skip_all)]
-pub async fn provision(
+pub async fn provision<R: Crng>(
     args: ProvisionArgs,
     measurement: Measurement,
     api: ApiClientType,
-    rng: &mut dyn Crng,
-) -> Result<()> {
+    rng: &mut R,
+) -> anyhow::Result<()> {
     debug!(%args.user_pk, args.port, %args.machine_id, %args.node_dns_name, "provisioning");
 
-    // TODO(phlip9): zeroize secrets
-
-    // Generate a fresh key pair, which we'll use for the provisioning cert.
-    let cert_key_pair = ed25519::gen_key_pair(rng);
-    let cert_pk = cert_key_pair.public_key_raw();
-    debug!(cert_pk = %hex::display(cert_pk), "attesting to pk");
-
-    // Get our enclave measurement and cert pk quoted by the enclave
-    // platform. This process binds the cert pk to the quote evidence. When
-    // a client verifies the Quote, they can also trust that the cert was
-    // generated on a valid, genuine enclave. Once this trust is settled,
-    // they can safely provision secrets onto the enclave via the newly
-    // established secure TLS channel.
-    //
-    // Returns the quote as an x509 cert extension that we'll embed in our
-    // self-signed provisioning cert.
-    let attest_start = Instant::now();
-    let cert_pk = ed25519::PublicKey::try_from(&cert_key_pair).unwrap();
-    let attestation = attest::quote_enclave(rng, &cert_pk)
-        .context("Failed to get node enclave quoted")?;
-
-    // Generate a self-signed x509 cert with the remote attestation embedded.
-    let dns_names = vec![args.node_dns_name];
-    let cert = AttestationCert::new(cert_key_pair, dns_names, attestation)
-        .context("Failed to generate remote attestation cert")?;
-    let cert_der = cert
-        .serialize_der_signed()
-        .expect("Failed to sign and serialize attestation cert");
-    let cert_key_der = cert.serialize_key_der();
-
-    debug!(
-        "acquired attestation: cert size: {} B, time elapsed: {:?}",
-        cert_der.len(),
-        attest_start.elapsed()
-    );
-
-    let tls_config = rustls::ServerConfig::builder()
-        .with_safe_defaults()
-        .with_no_client_auth()
-        .with_single_cert(
-            vec![rustls::Certificate(cert_der)],
-            rustls::PrivateKey(cert_key_der),
-        )
-        .context("Failed to build TLS config")?;
+    let tls_config = node_provision_tls_config(rng, args.node_dns_name)
+        .context("Failed to build TLS config for provisioning")?;
 
     // we'll trigger `shutdown_tx` when we've completed the provisioning process
     let (shutdown_tx, mut shutdown_rx) = mpsc::channel(1);
@@ -315,6 +270,7 @@ mod test {
     use common::cli::ProvisionArgs;
     use common::rng::SysRng;
     use secrecy::Secret;
+    use tokio_rustls::rustls;
 
     use super::*;
     use crate::api::mock::MockApiClient;
@@ -324,15 +280,16 @@ mod test {
     #[test]
     #[ignore] // << uncomment to dump fresh attestation cert
     fn dump_attest_cert() {
+        use common::ed25519;
+
         let mut rng = SysRng::new();
         let cert_key_pair = ed25519::from_seed(&[0x42; 32]);
         let cert_pk = ed25519::PublicKey::try_from(&cert_key_pair).unwrap();
-        let attestation =
-            crate::attest::quote_enclave(&mut rng, &cert_pk).unwrap();
+        let attestation = attest::quote_enclave(&mut rng, &cert_pk).unwrap();
         let dns_names = vec!["localhost".to_string()];
 
         let attest_cert =
-            AttestationCert::new(cert_key_pair, dns_names, attestation)
+            attest::AttestationCert::new(cert_key_pair, dns_names, attestation)
                 .unwrap();
 
         println!("measurement: '{}'", enclave::measurement());
@@ -366,20 +323,22 @@ mod test {
     async fn test_provision() {
         logger::init_for_testing();
 
+        let root_seed = RootSeed::new(Secret::new([0x42; 32]));
+        let user_pk = UserPk::new([0x69; 32]);
         let args = ProvisionArgs {
+            user_pk,
             // we're not going through a proxy and can't change DNS resolution
             // here (yet), so just bind cert to "localhost".
             node_dns_name: "localhost".to_owned(),
             ..ProvisionArgs::default()
         };
-        let user_pk = args.user_pk;
 
-        let mut rng = SysRng::new();
         let api = Arc::new(MockApiClient::new());
         let mut notifs_rx = api.notifs_rx();
         let measurement = enclave::measurement();
 
         let provision_task = async {
+            let mut rng = SysRng::new();
             provision(args, measurement, api, &mut rng).await.unwrap();
         };
 
@@ -406,7 +365,7 @@ mod test {
             let provision_req = ProvisionRequest {
                 user_pk,
                 node_pk: "031f7d233e27e9eaa68b770717c22fddd3bdd58656995d9edc32e84e6611182241".parse().unwrap(),
-                root_seed: RootSeed::new(Secret::new([0x42; 32])),
+                root_seed,
             };
             let client = reqwest::Client::builder()
                 .use_preconfigured_tls(tls_config)
