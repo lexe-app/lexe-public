588a941a809dca5d5fd8e3dc927d76e5d657a08e
Philip Hayes <philiphayes9@gmail.com>
2022-09-27 01:36:19 +0000

    node: flush `stdout` to ensure last log message actually gets logged

--- node/src/main.rs
+++ node/src/main.rs
@@ -1,22 +1,39 @@
+use std::io::Write;
+use std::process::ExitCode;
 use std::time::Instant;
 
 use lexe_ln::logger;
 use node::cli::Args;
-use tracing::{debug, error};
+use tracing::{error, info};
 
-pub fn main() {
+pub fn main() -> ExitCode {
     let start = Instant::now();
+
+    // Get useful, human-readable, symbolized backtraces even in an SGX enclave.
+    #[cfg(target_env = "sgx")]
+    sgx_panic_backtrace::set_panic_hook();
+
     logger::init();
 
     let args = argh::from_env::<Args>();
     let result = args.run();
 
     let time_elapsed = start.elapsed();
-    match result {
-        Ok(()) => debug!(?time_elapsed, "completed without error"),
-        Err(error) => {
-            error!(?time_elapsed, "error running command: {:#}", error);
-            std::process::exit(1);
+    let exit_code = match result {
+        Ok(()) => {
+            info!("completed without error: time elapsed: {time_elapsed:?}");
+            ExitCode::SUCCESS
         }
-    }
+        Err(error) => {
+            error!(
+                "error running command: {error:#}, time_elapsed: {time_elapsed:?}",
+            );
+            ExitCode::FAILURE
+        }
+    };
+
+    // ensure stdout flushes so we don't lose any buffered log messages.
+    std::io::stdout().flush().unwrap();
+
+    exit_code
 }
