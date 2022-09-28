0804e12b58c4c801d6ed627278eae0db0027e48c
Max Fang <protonmail.com@maxfa.ng>
2022-09-28 00:10:00 -0700

    test_utils: Add `get_ephemeral_port`

--- common/src/test_utils/mod.rs
+++ common/src/test_utils/mod.rs
@@ -3,3 +3,24 @@
 pub mod bitcoind;
 /// Quickly create roundtrip proptest for various serialization schemes.
 pub mod roundtrip;
+
+use std::net::{TcpListener, TcpStream};
+
+use crate::api::runner::Port;
+
+/// Returns an ephemeral port assigned by the OS which should be available for
+/// the next ~60s after this function is called
+pub fn get_ephemeral_port() -> Port {
+    // Request a random available port from the OS
+    let listener = TcpListener::bind(("localhost", 0))
+        .expect("Could not bind TcpListener");
+    let addr = listener.local_addr().unwrap();
+
+    // Create and accept a connection (which we'll promptly drop) in order to
+    // force the port into the TIME_WAIT state, ensuring that the port will be
+    // reserved from some limited amount of time (~60s on some Linux systems)
+    let _sender = TcpStream::connect(addr).unwrap();
+    let _incoming = listener.accept().unwrap();
+
+    addr.port()
+}
