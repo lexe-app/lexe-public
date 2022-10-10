517442356cc5ed455ba5645b5abda546501742b0
Philip Hayes <philiphayes9@gmail.com>
2022-10-10 13:10:32 -0700

    node: ensure `handle_event` completes before returning
    * The LDK `EventHandler` API contract requires that `handle_event`
      completely finishes before returning.
    
    * Our `handle_event` is an async function, but the LDK trait is sync
      (inside an outer async context!!).
    
    * (Sensibly) tokio panics if you try to `block_on` a task in a
      single-threaded runtime context, since it would starve all
      other tasks.
    
    * To get around this, we add a quick hack, `LazyBlockingTaskRt`, which
      is just a lazily spawned OS thread for running tasks one-at-a-time,
      and allows you to `block_on` a task from inside a single-threaded
      async context. This should definitely be removed once `EventHandler`
      supports async properly.

--- common/src/task.rs
+++ common/src/task.rs
@@ -2,6 +2,8 @@ use std::future::Future;
 use std::pin::Pin;
 use std::task::{Context, Poll};
 
+use once_cell::sync::Lazy;
+use tokio::sync::{mpsc, oneshot};
 use tokio::task::{JoinError, JoinHandle};
 use tracing::error;
 
@@ -19,6 +21,7 @@ pub struct LxTask<T> {
 }
 
 impl<T> LxTask<T> {
+    #[inline]
     #[allow(clippy::disallowed_methods)]
     pub fn spawn_named<F>(name: &'static str, future: F) -> LxTask<F::Output>
     where
@@ -31,6 +34,7 @@ impl<T> LxTask<T> {
         }
     }
 
+    #[inline]
     pub fn spawn<F>(future: F) -> LxTask<F::Output>
     where
         F: Future<Output = T> + Send + 'static,
@@ -44,8 +48,11 @@ impl<T> LxTask<T> {
         self.name
     }
 
-    pub fn result_with_name(self) -> LxTaskWithNameFut<T> {
-        LxTaskWithNameFut(self)
+    /// Make await'ing on an `LxTask` return the name along with the result:
+    /// `(Result<T, JoinError>, name)`
+    #[inline]
+    pub fn result_with_name(self) -> LxTaskWithName<T> {
+        LxTaskWithName(self)
     }
 }
 
@@ -78,7 +85,11 @@ impl<T> Future for LxTask<T> {
     }
 }
 
-pub fn join_res_label(join_res: Result<(), JoinError>) -> &'static str {
+/// A small helper that gives a human-readable label for a joined task's
+/// resulting output.
+pub fn joined_task_state_label(
+    join_res: Result<(), JoinError>,
+) -> &'static str {
     match join_res {
         Ok(()) => "finished",
         Err(err) if err.is_cancelled() => "canceled",
@@ -87,16 +98,18 @@ pub fn join_res_label(join_res: Result<(), JoinError>) -> &'static str {
     }
 }
 
-pub struct LxTaskWithNameFut<T>(LxTask<T>);
+/// A small wrapper `Future` for `LxTask` that returns the task name alongside
+/// the task output.
+pub struct LxTaskWithName<T>(LxTask<T>);
 
-impl<T> LxTaskWithNameFut<T> {
+impl<T> LxTaskWithName<T> {
     #[inline]
     pub fn name(&self) -> &'static str {
         self.0.name()
     }
 }
 
-impl<T> Future for LxTaskWithNameFut<T> {
+impl<T> Future for LxTaskWithName<T> {
     type Output = (Result<T, JoinError>, &'static str);
 
     fn poll(
@@ -116,5 +129,72 @@ impl<T> Future for LxTaskWithNameFut<T> {
         };
 
         Poll::Ready(result)
+    }
+}
+
+type BoxFut = Pin<Box<dyn Future<Output = ()> + Send + 'static>>;
+type BoxFutWithTx = (BoxFut, oneshot::Sender<()>);
+
+/// A lazily spawned thread for running async tasks from inside a sync context
+/// (that is itself inside an async context... don't ask...). Please don't use
+/// this unless you have a very good reason. : )
+///
+/// XXX: remove when LDK `EventHandler` trait is made properly async.
+pub struct LazyBlockingTaskRt(Lazy<mpsc::Sender<BoxFutWithTx>>);
+
+impl LazyBlockingTaskRt {
+    pub const fn new() -> Self {
+        Self(Lazy::new(|| {
+            // Only run one task at a time.
+            let (task_tx, mut task_rx) = mpsc::channel::<BoxFutWithTx>(1);
+
+            std::thread::spawn(move || {
+                let rt = tokio::runtime::Builder::new_current_thread()
+                    .enable_all()
+                    .build()
+                    .unwrap();
+
+                rt.block_on(async move {
+                    // Only run one task at a time.
+                    while let Some((task, res_tx)) = task_rx.recv().await {
+                        task.await;
+                        let _ = res_tx.send(());
+                    }
+                });
+            });
+
+            task_tx
+        }))
+    }
+
+    /// Block on `fut` until it runs to completion. The only difference b/w this
+    /// and [`tokio::task::block_in_place`] is that this "technically" works
+    /// inside a current-thread runtime (though it's certainly not recommended).
+    pub fn block_on(&self, fut: impl Future<Output = ()> + Send + 'static) {
+        self.block_on_boxed(Box::pin(fut));
+    }
+
+    pub fn block_on_boxed(&self, task: BoxFut) {
+        let blocking_task_tx = &*self.0;
+        let (res_tx, res_rx) = oneshot::channel();
+
+        // NOTE: _must_ be `futures::executor::block_on`, as
+        // `Handle::current().block_on()` will (sensibly) panic if used in an
+        // async current-thread runtime, since it will block all tasks in the
+        // rt.
+        futures::executor::block_on(async {
+            if blocking_task_tx.send((task, res_tx)).await.is_err() {
+                panic!("event handler runtime task channel closed");
+            }
+            res_rx
+                .await
+                .expect("event handler runtime panicked while running task?");
+        });
+    }
+}
+
+impl Default for LazyBlockingTaskRt {
+    fn default() -> Self {
+        Self::new()
     }
 }
