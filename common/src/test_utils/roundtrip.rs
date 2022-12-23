77deffac063c751704d51e09779228c4efb5d857
Max Fang <protonmail.com@maxfa.ng>
2022-12-23 16:04:01 -0600

    wallet::db+roundtrip: Reduce proptest cases
    Some tests were taking too long, so we only run 1 test case instead of
    the default 256. Also modifies the `*_with_strategy` roundtrip utility
    functions to be renamed to `*_custom`, and allow specifying a custom
    proptest test runner `Config`.

--- common/src/test_utils/roundtrip.rs
+++ common/src/test_utils/roundtrip.rs
@@ -3,6 +3,7 @@ use std::str::FromStr;
 
 use proptest::arbitrary::{any, Arbitrary};
 use proptest::strategy::Strategy;
+use proptest::test_runner::Config;
 use proptest::{prop_assert_eq, proptest};
 use serde::de::DeserializeOwned;
 use serde::Serialize;
@@ -49,18 +50,26 @@ pub fn json_value_canonical_proptest<T>()
 where
     T: Arbitrary + PartialEq + Serialize + DeserializeOwned,
 {
-    json_value_with_strategy(any::<T>());
+    json_value_custom(any::<T>(), Config::default());
 }
 
-/// Like [`json_value_canonical_proptest`], but allows specifying a custom
-/// canonical strategy. Useful for testing foreign types for which we cannot
-/// implement [`Arbitrary`].
-pub fn json_value_with_strategy<S, T>(strategy: S)
+/// Create a [`serde_json::Value`] canonical roundtrip proptest using a custom
+/// canonical strategy and custom proptest [`Config`]. Useful for testing
+/// foreign types for which we cannot implement [`Arbitrary`], or reducing the
+/// number of iterations on proptests that would otherwise take too long.
+///
+/// ```
+/// # use common::test_utils::{arbitrary, roundtrip};
+/// # use proptest::test_runner::Config;
+/// let config = Config::with_cases(1);
+/// roundtrip::json_value_custom(arbitrary::any_raw_tx(), config);
+/// ```
+pub fn json_value_custom<S, T>(strategy: S, config: Config)
 where
     S: Strategy<Value = T>,
     T: PartialEq + Serialize + DeserializeOwned + Debug,
 {
-    proptest!(|(value1 in strategy)| {
+    proptest!(config, |(value1 in strategy)| {
         let json_value1 = serde_json::to_value(&value1).unwrap();
         let value2 = serde_json::from_value(json_value1.clone()).unwrap();
         let json_value2 = serde_json::to_value(&value2).unwrap();
@@ -120,23 +129,27 @@ where
     T: Arbitrary + PartialEq + FromStr + Display,
     <T as FromStr>::Err: Debug,
 {
-    fromstr_display_with_strategy(any::<T>());
+    fromstr_display_custom(any::<T>(), Config::default());
 }
 
-/// Quickly create a roundtrip proptest for a [`FromStr`] / [`Display`] impl
-/// using a custom canonical strategy. Useful for testing foreign types for
-/// which we cannot implement [`Arbitrary`].
+/// Create a roundtrip proptest for a [`FromStr`] / [`Display`] impl using a
+/// custom canonical strategy and custom proptest [`Config`]. Useful for testing
+/// foreign types for which we cannot implement [`Arbitrary`], or reducing the
+/// number of iterations on proptests that would otherwise take too long.
 ///
-/// ```ignore
-/// fromstr_display_with_strategy(any::<NodePk>());
 /// ```
-pub fn fromstr_display_with_strategy<S, T>(strategy: S)
+/// # use common::test_utils::{arbitrary, roundtrip};
+/// # use proptest::test_runner::Config;
+/// let config = Config::with_cases(1);
+/// roundtrip::fromstr_display_custom(arbitrary::any_outpoint(), config);
+/// ```
+pub fn fromstr_display_custom<S, T>(strategy: S, config: Config)
 where
     S: Strategy<Value = T>,
     T: PartialEq + FromStr + Display + Debug,
     <T as FromStr>::Err: Debug,
 {
-    proptest!(|(value1 in strategy)| {
+    proptest!(config, |(value1 in strategy)| {
         let value2 = T::from_str(&value1.to_string()).unwrap();
         prop_assert_eq!(value1, value2)
     });
@@ -152,23 +165,27 @@ where
     T: Arbitrary + PartialEq + FromStr + LowerHex,
     <T as FromStr>::Err: Debug,
 {
-    fromstr_lowerhex_with_strategy(any::<T>());
+    fromstr_lowerhex_custom(any::<T>(), Config::default());
 }
 
-/// Quickly create a roundtrip proptest for a [`FromStr`] / [`LowerHex`] impl
-/// using a custom canonical strategy. Useful for testing foreign types for
-/// which we cannot implement [`Arbitrary`].
+/// Create a roundtrip proptest for a [`FromStr`] / [`LowerHex`] impl using a
+/// custom canonical strategy and custom proptest [`Config`]. Useful for testing
+/// foreign types for which we cannot implement [`Arbitrary`], or reducing the
+/// number of iterations on proptests that would otherwise take too long.
 ///
-/// ```ignore
-/// hex_with_strategy(any::<NodePk>());
 /// ```
-pub fn fromstr_lowerhex_with_strategy<S, T>(strategy: S)
+/// # use common::test_utils::{arbitrary, roundtrip};
+/// # use proptest::test_runner::Config;
+/// let config = Config::with_cases(1);
+/// roundtrip::fromstr_lowerhex_custom(arbitrary::any_script(), config);
+/// ```
+pub fn fromstr_lowerhex_custom<S, T>(strategy: S, config: Config)
 where
     S: Strategy<Value = T>,
     T: PartialEq + FromStr + LowerHex + Debug,
     <T as FromStr>::Err: Debug,
 {
-    proptest!(|(value1 in strategy)| {
+    proptest!(config, |(value1 in strategy)| {
         let hex = format!("{value1:x}");
         let value2 = T::from_str(hex.as_str()).unwrap();
         prop_assert_eq!(value1, value2)
