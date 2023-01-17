27dfeed913d00a06ab00efd33277eeedb8e40140
Max Fang <protonmail.com@maxfa.ng>
2023-01-16 20:27:30 -0800

    refactor: Use `LexeEsplora` as fee estimator

--- lexe-ln/src/bitcoind/mod.rs
+++ lexe-ln/src/bitcoind/mod.rs
@@ -1,40 +1,28 @@
 use std::collections::HashMap;
-use std::sync::atomic::{AtomicU32, Ordering};
 use std::sync::Arc;
-use std::time::Duration;
 
 use anyhow::{ensure, Context};
 use bitcoin::hash_types::Txid;
 use common::cli::{BitcoindRpcInfo, Network};
-use common::shutdown::ShutdownChannel;
-use common::task::LxTask;
 use lightning::chain::chaininterface::{ConfirmationTarget, FeeEstimator};
 use lightning_block_sync::http::HttpEndpoint;
 use lightning_block_sync::rpc::RpcClient;
-use tokio::time;
-use tracing::{debug, error};
+use tracing::debug;
+
+use crate::esplora::LexeEsplora;
 
 mod types;
 
 pub use types::*;
 
-const POLL_FEE_ESTIMATE_INTERVAL: Duration = Duration::from_secs(60 * 60);
-/// The minimum feerate we are allowed to send, as specified by LDK.
-const MIN_FEERATE: u32 = 253;
-
 pub struct LexeBitcoind {
     rpc_client: Arc<RpcClient>,
-    background_fees: Arc<AtomicU32>,
-    normal_fees: Arc<AtomicU32>,
-    high_prio_fees: Arc<AtomicU32>,
-    shutdown: ShutdownChannel,
 }
 
 impl LexeBitcoind {
     pub async fn init(
         bitcoind_rpc: BitcoindRpcInfo,
         network: Network,
-        shutdown: ShutdownChannel,
     ) -> anyhow::Result<Self> {
         debug!(%network, "Initializing bitcoind client");
 
@@ -45,17 +33,7 @@ impl LexeBitcoind {
             .context("Could not initialize RPC client")?;
         let rpc_client = Arc::new(rpc_client);
 
-        let background_fees = Arc::new(AtomicU32::new(MIN_FEERATE));
-        let normal_fees = Arc::new(AtomicU32::new(2000));
-        let high_prio_fees = Arc::new(AtomicU32::new(5000));
-
-        let client = Self {
-            rpc_client,
-            background_fees,
-            normal_fees,
-            high_prio_fees,
-            shutdown,
-        };
+        let client = Self { rpc_client };
 
         // Make an initial test call to check that the RPC client is working
         // correctly, and also check that the bitcoind we've connected to is
@@ -84,103 +62,6 @@ impl LexeBitcoind {
         Ok(client)
     }
 
-    pub fn spawn_refresh_fees_task(&self) -> LxTask<()> {
-        let rpc_client = self.rpc_client.clone();
-        let background_fees = self.background_fees.clone();
-        let normal_fees = self.normal_fees.clone();
-        let high_prio_fees = self.high_prio_fees.clone();
-        let mut shutdown = self.shutdown.clone();
-
-        // TODO(max): Instrument with shutdown
-        LxTask::spawn_named("refresh fees", async move {
-            let mut poll_interval = time::interval(POLL_FEE_ESTIMATE_INTERVAL);
-
-            loop {
-                tokio::select! {
-                    _ = poll_interval.tick() => {}
-                    () = shutdown.recv() => break,
-                }
-
-                let poll_res = Self::refresh_fees(
-                    background_fees.as_ref(),
-                    normal_fees.as_ref(),
-                    high_prio_fees.as_ref(),
-                    rpc_client.as_ref(),
-                )
-                .await;
-
-                match poll_res {
-                    Ok(()) => {}
-                    Err(e) => {
-                        error!("Error while polling fee estimates: {:#}", e);
-                    }
-                }
-            }
-        })
-    }
-
-    async fn refresh_fees(
-        background_fees: &AtomicU32,
-        normal_fees: &AtomicU32,
-        high_prio_fees: &AtomicU32,
-        rpc_client: &RpcClient,
-    ) -> anyhow::Result<()> {
-        let background_estimate = {
-            let background_conf_target = serde_json::json!(144);
-            let background_estimate_mode = serde_json::json!("ECONOMICAL");
-            let resp = rpc_client
-                .call_method::<FeeResponse>(
-                    "estimatesmartfee",
-                    &[background_conf_target, background_estimate_mode],
-                )
-                .await
-                .context("Failed to get background estimate")?;
-            match resp.feerate_sat_per_kw {
-                Some(feerate) => std::cmp::max(feerate, MIN_FEERATE),
-                None => MIN_FEERATE,
-            }
-        };
-
-        let normal_estimate = {
-            let normal_conf_target = serde_json::json!(18);
-            let normal_estimate_mode = serde_json::json!("ECONOMICAL");
-            let resp = rpc_client
-                .call_method::<FeeResponse>(
-                    "estimatesmartfee",
-                    &[normal_conf_target, normal_estimate_mode],
-                )
-                .await
-                .context("Failed to get normal estimate")?;
-            match resp.feerate_sat_per_kw {
-                Some(feerate) => std::cmp::max(feerate, MIN_FEERATE),
-                None => 2000,
-            }
-        };
-
-        let high_prio_estimate = {
-            let high_prio_conf_target = serde_json::json!(6);
-            let high_prio_estimate_mode = serde_json::json!("CONSERVATIVE");
-            let resp = rpc_client
-                .call_method::<FeeResponse>(
-                    "estimatesmartfee",
-                    &[high_prio_conf_target, high_prio_estimate_mode],
-                )
-                .await
-                .context("Failed to get high priority estimate")?;
-
-            match resp.feerate_sat_per_kw {
-                Some(feerate) => std::cmp::max(feerate, MIN_FEERATE),
-                None => 5000,
-            }
-        };
-
-        background_fees.store(background_estimate, Ordering::Release);
-        normal_fees.store(normal_estimate, Ordering::Release);
-        high_prio_fees.store(high_prio_estimate, Ordering::Release);
-
-        Ok(())
-    }
-
     pub async fn create_raw_transaction(
         &self,
         outputs: Vec<HashMap<String, f64>>,
@@ -198,14 +79,19 @@ impl LexeBitcoind {
     pub async fn fund_raw_transaction(
         &self,
         raw_tx: RawTx,
+        esplora: &LexeEsplora,
     ) -> anyhow::Result<FundedTx> {
         let raw_tx_json = serde_json::json!(raw_tx.0);
+        let fee_rate = esplora
+            .get_est_sat_per_1000_weight(ConfirmationTarget::Normal)
+            as f64
+            / 250.0;
         let options = serde_json::json!({
             // LDK gives us feerates in satoshis per KW but Bitcoin Core here
             // expects fees denominated in satoshis per vB. First we need to
             // multiply by 4 to convert weight units to virtual bytes, then
             // divide by 1000 to convert KvB to vB.
-            "fee_rate": self.get_est_sat_per_1000_weight(ConfirmationTarget::Normal) as f64 / 250.0,
+            "fee_rate": fee_rate,
             // While users could "cancel" a channel open by RBF-bumping and
             // paying back to themselves, we don't allow it here as its easy to
             // have users accidentally RBF bump and pay to the channel funding
@@ -248,24 +134,5 @@ impl LexeBitcoind {
             .call_method::<BlockchainInfo>("getblockchaininfo", &[])
             .await
             .context("getblockchaininfo RPC call failed")
-    }
-}
-
-impl FeeEstimator for LexeBitcoind {
-    fn get_est_sat_per_1000_weight(
-        &self,
-        confirmation_target: ConfirmationTarget,
-    ) -> u32 {
-        match confirmation_target {
-            ConfirmationTarget::Background => {
-                self.background_fees.load(Ordering::Acquire)
-            }
-            ConfirmationTarget::Normal => {
-                self.normal_fees.load(Ordering::Acquire)
-            }
-            ConfirmationTarget::HighPriority => {
-                self.high_prio_fees.load(Ordering::Acquire)
-            }
-        }
     }
 }
