2ba3a78b93e3ef20ac30344d2193420a70c5e082
Philip Hayes <philiphayes9@gmail.com>
2023-01-26 20:57:17 -0800

    app: support building macOS app

--- app/macos/Runner/AppDelegate.swift
+++ app/macos/Runner/AppDelegate.swift
@@ -4,6 +4,10 @@ import FlutterMacOS
 @NSApplicationMain
 class AppDelegate: FlutterAppDelegate {
   override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+    // reference a dummy method that references all our exported ffi symbols so
+    // Xcode won't strip our symbols.
+    print(dummy_method_to_enforce_bundling())
+
     return true
   }
 }
