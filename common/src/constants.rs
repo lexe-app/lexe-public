87b0550a378b242b5f002436b8605709089b481a
Max Fang <protonmail.com@maxfa.ng>
2023-02-03 18:50:58 -0800

    common: Remove unneeded constants

--- common/src/constants.rs
+++ common/src/constants.rs
@@ -1,14 +1,6 @@
-use std::net::SocketAddr;
-
-use once_cell::sync::Lazy;
 use rcgen::{DistinguishedName, DnType};
-use secrecy::Secret;
 
 use crate::api::ports::Port;
-use crate::api::NodePk;
-use crate::ln::peer::ChannelPeer;
-use crate::rng::SysRng;
-use crate::root_seed::RootSeed;
 
 pub const DEFAULT_CHANNEL_SIZE: usize = 256;
 pub const SMALLER_CHANNEL_SIZE: usize = 16;
@@ -28,26 +20,14 @@ pub const DEFAULT_RUNNER_URL: &str = "http://127.0.0.1:5050";
 pub const DEFAULT_LSP_URL: &str = "http://127.0.0.1:6060";
 pub const DEFAULT_ESPLORA_URL: &str = "http://127.0.0.1:7070";
 
-/// The node pubkey the user node expects when connecting to the LSP.
-// TODO: Replace this with the LSP's real NodePk.
-pub static DEFAULT_LSP_NODE_PK: Lazy<NodePk> = Lazy::new(|| {
-    let mut rng = SysRng::new();
-    let root_seed = RootSeed::new(Secret::new([42u8; 32]));
-    root_seed.derive_node_pk(&mut rng)
-});
-
-/// The node pubkey and [`SocketAddr`] of the LSP. Note that the socket address
-/// is used by *user nodes* to establish a P2P connection to the LSP. External
-/// LN nodes use the [`STANDARD_LIGHTNING_P2P_PORT`].
-// TODO: Replace this with the LSP's real NodePk.
-pub static DEFAULT_LSP: Lazy<ChannelPeer> = Lazy::new(|| {
-    let node_pk = *DEFAULT_LSP_NODE_PK;
-    let addr = SocketAddr::from(([127, 0, 0, 1], 6060));
-    ChannelPeer { node_pk, addr }
-});
-
 /// The standard port used for Lightning Network P2P connections
 pub const STANDARD_LIGHTNING_P2P_PORT: Port = 9735;
+
+// Blockstream Esplora API
+pub const BLOCKSTREAM_ESPLORA_MAINNET_URL: &str =
+    "https://blockstream.info/api/";
+pub const BLOCKSTREAM_ESPLORA_TESTNET_URL: &str =
+    "https://blockstream.info/testnet/api/";
 
 /// Fake DNS name used by the node reverse proxy to route owner requests to a
 /// node awaiting provisioning. This DNS name doesn't actually resolve.
