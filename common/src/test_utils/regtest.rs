23f8f12414befefc7ead4ca46160c61c51e76380
Max Fang <protonmail.com@maxfa.ng>
2023-02-13 19:44:54 -0800

    regtest: Fix flaky esplora block sync
    See the new comment for details.

--- common/src/test_utils/regtest.rs
+++ common/src/test_utils/regtest.rs
@@ -1,5 +1,5 @@
-use std::env;
 use std::time::Duration;
+use std::{cmp, env};
 
 use bitcoin::hash_types::PubkeyHash;
 use bitcoin::hashes::Hash;
@@ -30,14 +30,14 @@ impl Regtest {
         // Init bitcoind
         let bitcoind_exe_path = bitcoind::downloaded_exe_path()
             .expect("Didn't specify bitcoind version in feature flags");
-        // dbg!(&bitcoind_exe_path);
+        debug!(%bitcoind_exe_path);
         let bitcoind =
             BitcoinD::new(bitcoind_exe_path).expect("Failed to init bitcoind");
 
         // Construct electrsd conf
         let electrsd_exe_path = electrsd::downloaded_exe_path()
             .expect("Didn't specify electrsd version in feature flags");
-        // dbg!(&electrsd_exe_path);
+        debug!(%electrsd_exe_path);
         let mut electrsd_conf = electrsd::Conf::default();
         // Expose esplora endpoint
         electrsd_conf.http_enabled = true;
@@ -61,7 +61,7 @@ impl Regtest {
             Some(ref url) => format!("http://{url}"),
             None => panic!("Missing esplora feature or not enabled in Conf"),
         };
-        // dbg!(&esplora_url);
+        debug!(%esplora_url);
 
         let regtest = Self {
             bitcoind,
@@ -151,10 +151,11 @@ impl Regtest {
         // esplora server is up-to-date before telling nodes to resync.
         // There does not appear to be any clean blocking or async API for this.
         let mut poll_timer = tokio::time::interval(Duration::from_secs(1));
+        let expected_height = pre_height + num_blocks;
+        let mut highest_seen_height = pre_height;
         for i in 0..60 {
             poll_timer.tick().await;
             debug!("Polling for block header notification");
-            let expected = pre_height + num_blocks;
             // We use .block_headers_subscribe() instead of .block_headers_pop()
             // because the latter often fails to actually notify us, causing
             // tests to hang for the full 60 seconds.
@@ -164,29 +165,44 @@ impl Regtest {
                 .block_headers_subscribe()
                 .expect("Could not fetch latest block header")
                 .height;
-            if post_height >= expected {
+            highest_seen_height = cmp::max(highest_seen_height, post_height);
+            if post_height >= expected_height {
                 debug!(
-                    "Got to height {post_height} in {i}s, expected {expected}"
+                    "Got to height {post_height} in {i}s, \
+                    expected height {expected_height}"
                 );
                 return blockhashes;
             } else {
                 // - When we call .block_headers_subscribe(), electrs-esplora
                 //   only returns the latest *cached* header.
-                // - Every 5 seconds, electrs-esplora: (1) queries the latest
+                // - Every 5 secs, Blockstream/electrs: (1) queries the latest
                 //   block, (2) updates its mempool, and (3) updates its
                 //   subscribers. Prior to this tick, a subsequent call to
                 //   .block_headers_subscribe() will only return the same cached
                 //   header.
-                // - However, we can call .trigger() to make electrs-esplora
+                // - However*, we can call .trigger() to make electrs-esplora
                 //   update on-demand, so that the block generation process can
                 //   proceed faster. Hence, we keep on retriggering until we've
                 //   electrs has indexed the desired # of blocks. More info:
                 //   https://github.com/lexe-tech/lexe/pull/85/files#r1080589441
-                self.electrsd.trigger().expect("Couldn't trigger electrs");
+                // - *However, Blockstream/electrs currently has a bug where
+                //   sending a trigger signal more than once every 5 seconds
+                //   will cause Waiter::wait to recurse indefinitely, resulting
+                //   in the main thread never getting woken to sync. Thus, the
+                //   trigger below is currently commented out. The fix is in
+                //   https://github.com/Blockstream/electrs/pull/53. Once it has
+                //   been merged to Blockstream/electrs and electrsd has
+                //   released a Blockstream/electrs binary (via the "esplora_*"
+                //   feature flag) which contains the fix, we should enable this
+                //   line again to speed up our tests a bit.
+                // self.electrsd.trigger().expect("Couldn't trigger electrs");
             }
         }
 
-        panic!("Failed to mine {num_blocks} blocks to {address}")
+        panic!(
+            "Failed to mine {num_blocks} blocks to {address}. \
+            Expected height {expected_height}, got {highest_seen_height}."
+        );
     }
 }
 
