66c765ec01c889f393084e5062bbd19ee42a010c
Philip Hayes <philiphayes9@gmail.com>
2023-02-16 18:29:45 -0800

    app: successful signup

--- app/integration_test/bindings_test.dart
+++ app/integration_test/bindings_test.dart
@@ -3,16 +3,19 @@ import 'package:integration_test/integration_test.dart'
     show IntegrationTestWidgetsFlutterBinding;
 
 import 'package:lexeapp/bindings.dart' show api;
+import 'package:lexeapp/bindings_generated_api.dart' show Config;
 import 'package:lexeapp/cfg.dart' as cfg;
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
+  final config = Config.regtest(bridge: api);
+
   test("cfg.test is FALSE in integration tests", () {
     expect(cfg.test, false);
   });
 
-  test("bindings work", () {
-    expect(api.hello(), "hello!");
+  test("fresh app has no persisted state", () async {
+    expect(await api.appLoad(config: config), false);
   });
 }
--- app/lib/cfg.dart
+++ app/lib/cfg.dart
@@ -1,6 +1,9 @@
 import 'dart:io' show Platform;
 import 'package:flutter/foundation.dart' show kDebugMode;
 
+import 'bindings.dart' show api;
+import 'bindings_generated_api.dart' show Config, Network, BuildVariant;
+
 /// `true` when the flutter app is built in debug mode with debugging info and
 /// debug symbols built-in (i.e., not profile or release mode).
 ///
@@ -11,3 +14,11 @@ const bool debug = kDebugMode;
 /// `true` only in unit tests.
 /// `false` in integration tests and run mode.
 final bool test = Platform.environment.containsKey("FLUTTER_TEST");
+
+/// TODO(phlip9): make this configurable via
+/// [flutter build flavors](https://docs.flutter.dev/deployment/flavors)
+final Config config = Config(
+  bridge: api,
+  buildVariant: BuildVariant.Development,
+  network: Network.Regtest,
+);
