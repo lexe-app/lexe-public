d5f77392a0ad39c4ae82cc73f76df4ad1505639e
Max Fang <protonmail.com@maxfa.ng>
2023-02-21 18:37:45 -0800

    node: Implement per-service api clients

--- node/src/api/mod.rs
+++ node/src/api/mod.rs
@@ -1,3 +1,5 @@
+use std::sync::Arc;
+
 use async_trait::async_trait;
 use common::api::auth::UserAuthToken;
 use common::api::def::{
@@ -6,10 +8,11 @@ use common::api::def::{
 use common::api::error::BackendApiError;
 use common::api::vfs::NodeFile;
 
+/// Real clients.
+pub(crate) mod client;
+/// Mock clients.
 #[cfg(any(test, not(target_env = "sgx")))]
 pub mod mock;
-
-mod client;
 
 pub use client::*;
 
@@ -32,4 +35,89 @@ pub trait ApiClient:
         auth: UserAuthToken,
         retries: usize,
     ) -> Result<(), BackendApiError>;
+}
+
+/// A trait for a client that can implements both backend API traits, plus some
+/// methods which allow the caller to specify the number of retries.
+#[async_trait]
+pub trait BackendApiClient: NodeBackendApi + UserBackendApi {
+    async fn create_file_with_retries(
+        &self,
+        file: &NodeFile,
+        auth: UserAuthToken,
+        retries: usize,
+    ) -> Result<NodeFile, BackendApiError>;
+
+    async fn upsert_file_with_retries(
+        &self,
+        file: &NodeFile,
+        auth: UserAuthToken,
+        retries: usize,
+    ) -> Result<(), BackendApiError>;
+}
+
+/// Helper to initiate a client to the backend.
+#[allow(dead_code)] // TODO(max): Remove
+#[allow(unused_variables)] // `mock` isn't read in sgx
+pub(crate) fn new_backend_api(
+    mock: bool,
+    backend_url: String,
+) -> Arc<dyn BackendApiClient + Send + Sync> {
+    cfg_if::cfg_if! {
+        if #[cfg(all(target_env = "sgx", not(test)))] {
+            // Can only use the real backend client in production (sgx)
+            Arc::new(client::BackendClient::new(backend_url))
+        } else {
+            // Can use real OR mock client during development
+            if mock {
+                Arc::new(mock::MockBackendClient::new())
+            } else {
+                Arc::new(client::BackendClient::new(backend_url))
+            }
+        }
+    }
+}
+
+/// Helper to initiate a client to the LSP.
+#[allow(dead_code)] // TODO(max): Remove
+#[allow(unused_variables)] // `mock` isn't read in sgx
+pub(crate) fn new_lsp_api(
+    mock: bool,
+    lsp_url: String,
+) -> Arc<dyn NodeLspApi + Send + Sync> {
+    cfg_if::cfg_if! {
+        if #[cfg(all(target_env = "sgx", not(test)))] {
+            // Can only use the real lsp client in production (sgx)
+            Arc::new(client::LspClient::new(lsp_url))
+        } else {
+            // Can use real OR mock client during development
+            if mock {
+                Arc::new(mock::MockLspClient)
+            } else {
+                Arc::new(client::LspClient::new(lsp_url))
+            }
+        }
+    }
+}
+
+/// Helper to initiate a client to the runner.
+#[allow(dead_code)] // TODO(max): Remove
+#[allow(unused_variables)] // `mock` isn't read in sgx
+pub(crate) fn new_runner_api(
+    mock: bool,
+    runner_url: String,
+) -> Arc<dyn NodeRunnerApi + Send + Sync> {
+    cfg_if::cfg_if! {
+        if #[cfg(all(target_env = "sgx", not(test)))] {
+            // Can only use the real runner client in production (sgx)
+            Arc::new(client::RunnerClient::new(runner_url))
+        } else {
+            // Can use real OR mock client during development
+            if mock {
+                Arc::new(mock::MockRunnerClient::new())
+            } else {
+                Arc::new(client::RunnerClient::new(runner_url))
+            }
+        }
+    }
 }
