9116d6968f3818b1954010ba8c35b490a68aff40
Max Fang <protonmail.com@maxfa.ng>
2023-02-21 18:37:45 -0800

    node: Implement per-service api clients

--- node/src/api/client.rs
+++ node/src/api/client.rs
@@ -16,7 +16,244 @@ use common::api::vfs::{NodeDirectory, NodeFile, NodeFileId};
 use common::api::{NodePk, Scid, User, UserPk};
 use common::ed25519;
 
-use crate::api::ApiClient;
+use crate::api::{ApiClient, BackendApiClient};
+
+pub(crate) struct RunnerClient {
+    rest: RestClient,
+    runner_url: String,
+}
+
+impl RunnerClient {
+    #[allow(dead_code)] // TODO(max): Remove
+    pub(crate) fn new(runner_url: String) -> Self {
+        Self {
+            rest: RestClient::new(),
+            runner_url,
+        }
+    }
+}
+
+#[async_trait]
+impl NodeRunnerApi for RunnerClient {
+    async fn ready(
+        &self,
+        data: UserPorts,
+    ) -> Result<UserPorts, RunnerApiError> {
+        let runner = &self.runner_url;
+        let req = self.rest.post(format!("{runner}/ready"), &data);
+        // TODO(phlip9): authenticate runner callbacks?
+        // .bearer_auth(&self.auth_token().await?);
+        self.rest.send(req).await
+    }
+}
+
+pub(crate) struct LspClient {
+    rest: RestClient,
+    lsp_url: String,
+}
+
+impl LspClient {
+    #[allow(dead_code)] // TODO(max): Remove
+    pub(crate) fn new(lsp_url: String) -> Self {
+        Self {
+            rest: RestClient::new(),
+            lsp_url,
+        }
+    }
+}
+
+#[async_trait]
+impl NodeLspApi for LspClient {
+    async fn get_new_scid(&self, node_pk: NodePk) -> Result<Scid, LspApiError> {
+        let lsp = &self.lsp_url;
+        let data = GetByNodePk { node_pk };
+        let req = self.rest.get(format!("{lsp}/node/v1/scid"), &data);
+        self.rest.send(req).await
+    }
+}
+
+pub(crate) struct BackendClient {
+    rest: RestClient,
+    backend_url: String,
+}
+
+impl BackendClient {
+    #[allow(dead_code)] // TODO(max): Remove
+    pub(crate) fn new(backend_url: String) -> Self {
+        Self {
+            rest: RestClient::new(),
+            backend_url,
+        }
+    }
+}
+
+#[async_trait]
+impl BackendApiClient for BackendClient {
+    async fn create_file_with_retries(
+        &self,
+        data: &NodeFile,
+        auth: UserAuthToken,
+        retries: usize,
+    ) -> Result<NodeFile, BackendApiError> {
+        let backend = &self.backend_url;
+        let url = format!("{backend}/v1/file");
+        let req = self.rest.post(url, data).bearer_auth(&auth);
+        self.rest.send_with_retries(req, retries, &[]).await
+    }
+
+    async fn upsert_file_with_retries(
+        &self,
+        data: &NodeFile,
+        auth: UserAuthToken,
+        retries: usize,
+    ) -> Result<(), BackendApiError> {
+        let backend = &self.backend_url;
+        let url = format!("{backend}/v1/file");
+        let req = self.rest.put(url, data).bearer_auth(&auth);
+        self.rest.send_with_retries(req, retries, &[]).await
+    }
+}
+
+#[async_trait]
+impl UserBackendApi for BackendClient {
+    async fn signup(
+        &self,
+        _signed_req: ed25519::Signed<UserSignupRequest>,
+    ) -> Result<(), BackendApiError> {
+        unimplemented!()
+    }
+
+    async fn user_auth(
+        &self,
+        signed_req: ed25519::Signed<UserAuthRequest>,
+    ) -> Result<UserAuthResponse, BackendApiError> {
+        let backend = &self.backend_url;
+        let url = format!("{backend}/user_auth");
+        let req = self
+            .rest
+            .builder(POST, url)
+            .signed_bcs(signed_req)
+            .map_err(BackendApiError::bcs_serialize)?;
+        self.rest.send_with_retries(req, 3, &[]).await
+    }
+}
+
+#[async_trait]
+impl NodeBackendApi for BackendClient {
+    // not authenticated, node calls this to get sealed seed on startup
+    async fn get_user(
+        &self,
+        user_pk: UserPk,
+    ) -> Result<Option<User>, BackendApiError> {
+        let backend = &self.backend_url;
+        let data = GetByUserPk { user_pk };
+        let req = self.rest.get(format!("{backend}/v1/user"), &data);
+        self.rest.send(req).await
+    }
+
+    // not authenticated, node calls this to get sealed seed on startup
+    async fn get_sealed_seed(
+        &self,
+        data: SealedSeedId,
+    ) -> Result<Option<SealedSeed>, BackendApiError> {
+        let backend = &self.backend_url;
+        let req = self.rest.get(format!("{backend}/v1/sealed_seed"), &data);
+        self.rest.send(req).await
+    }
+
+    async fn create_sealed_seed(
+        &self,
+        data: SealedSeed,
+        auth: UserAuthToken,
+    ) -> Result<(), BackendApiError> {
+        let backend = &self.backend_url;
+        let req = self
+            .rest
+            .post(format!("{backend}/v1/sealed_seed"), &data)
+            .bearer_auth(&auth);
+        self.rest.send(req).await
+    }
+
+    async fn get_scid(
+        &self,
+        node_pk: NodePk,
+    ) -> Result<Option<Scid>, BackendApiError> {
+        let backend = &self.backend_url;
+        let data = GetByNodePk { node_pk };
+        let req = self.rest.get(format!("{backend}/v1/scid"), &data);
+        self.rest.send(req).await
+    }
+
+    async fn get_file(
+        &self,
+        data: &NodeFileId,
+        auth: UserAuthToken,
+    ) -> Result<Option<NodeFile>, BackendApiError> {
+        let backend = &self.backend_url;
+        let req = self
+            .rest
+            .get(format!("{backend}/v1/file"), data)
+            .bearer_auth(&auth);
+        self.rest.send(req).await
+    }
+
+    async fn create_file(
+        &self,
+        data: &NodeFile,
+        auth: UserAuthToken,
+    ) -> Result<NodeFile, BackendApiError> {
+        let backend = &self.backend_url;
+        let req = self
+            .rest
+            .post(format!("{backend}/v1/file"), data)
+            .bearer_auth(&auth);
+        self.rest.send(req).await
+    }
+
+    async fn upsert_file(
+        &self,
+        data: &NodeFile,
+        auth: UserAuthToken,
+    ) -> Result<(), BackendApiError> {
+        let backend = &self.backend_url;
+        let req = self
+            .rest
+            .put(format!("{backend}/v1/file"), data)
+            .bearer_auth(&auth);
+        self.rest.send(req).await
+    }
+
+    // TODO We want to delete channel peers / monitors when channels close
+    /// Returns "OK" if exactly one row was deleted.
+    #[allow(dead_code)]
+    async fn delete_file(
+        &self,
+        data: &NodeFileId,
+        auth: UserAuthToken,
+    ) -> Result<String, BackendApiError> {
+        let backend = &self.backend_url;
+        let req = self
+            .rest
+            .delete(format!("{backend}/v1/file"), data)
+            .bearer_auth(&auth);
+        self.rest.send(req).await
+    }
+
+    async fn get_directory(
+        &self,
+        data: &NodeDirectory,
+        auth: UserAuthToken,
+    ) -> Result<Vec<NodeFile>, BackendApiError> {
+        let backend = &self.backend_url;
+        let req = self
+            .rest
+            .get(format!("{backend}/v1/directory"), data)
+            .bearer_auth(&auth);
+        self.rest.send(req).await
+    }
+}
+
+// TODO(max): Remove everything beyond this point
 
 pub struct NodeApiClient {
     rest: RestClient,
--- node/src/api/mock.rs
+++ node/src/api/mock.rs
@@ -24,7 +24,7 @@ use common::root_seed::RootSeed;
 use once_cell::sync::Lazy;
 use tokio::sync::mpsc;
 
-use crate::api::ApiClient;
+use crate::api::{ApiClient, BackendApiClient};
 use crate::persister;
 
 type FileName = String;
@@ -86,6 +86,189 @@ fn node_pk(user_pk: UserPk) -> NodePk {
 fn measurement(_user_pk: UserPk) -> Measurement {
     // It's the same for now but we may want to use different ones later
     enclave::measurement()
+}
+
+// --- The mock clients --- //
+
+pub(crate) struct MockRunnerClient {
+    notifs_tx: mpsc::Sender<UserPorts>,
+    notifs_rx: Mutex<Option<mpsc::Receiver<UserPorts>>>,
+}
+
+impl MockRunnerClient {
+    pub(crate) fn new() -> Self {
+        let (notifs_tx, notifs_rx) = mpsc::channel(8);
+        let notifs_rx = Mutex::new(Some(notifs_rx));
+        Self {
+            notifs_tx,
+            notifs_rx,
+        }
+    }
+
+    pub(crate) fn notifs_rx(&self) -> mpsc::Receiver<UserPorts> {
+        self.notifs_rx
+            .lock()
+            .unwrap()
+            .take()
+            .expect("Someone already subscribed")
+    }
+}
+
+#[async_trait]
+impl NodeRunnerApi for MockRunnerClient {
+    async fn ready(
+        &self,
+        user_ports: UserPorts,
+    ) -> Result<UserPorts, RunnerApiError> {
+        let _ = self.notifs_tx.try_send(user_ports);
+        Ok(user_ports)
+    }
+}
+
+pub(super) struct MockLspClient;
+
+#[async_trait]
+impl NodeLspApi for MockLspClient {
+    async fn get_new_scid(
+        &self,
+        _node_pk: NodePk,
+    ) -> Result<Scid, LspApiError> {
+        Ok(DUMMY_SCID)
+    }
+}
+
+pub(crate) struct MockBackendClient(Mutex<VirtualFileSystem>);
+
+impl MockBackendClient {
+    pub(crate) fn new() -> Self {
+        Self(Mutex::new(VirtualFileSystem::new()))
+    }
+}
+
+#[async_trait]
+impl BackendApiClient for MockBackendClient {
+    async fn create_file_with_retries(
+        &self,
+        data: &NodeFile,
+        auth: UserAuthToken,
+        _retries: usize,
+    ) -> Result<NodeFile, BackendApiError> {
+        self.create_file(data, auth).await
+    }
+
+    async fn upsert_file_with_retries(
+        &self,
+        data: &NodeFile,
+        auth: UserAuthToken,
+        _retries: usize,
+    ) -> Result<(), BackendApiError> {
+        self.upsert_file(data, auth).await
+    }
+}
+
+#[async_trait]
+impl UserBackendApi for MockBackendClient {
+    async fn signup(
+        &self,
+        _signed_req: ed25519::Signed<UserSignupRequest>,
+    ) -> Result<(), BackendApiError> {
+        Ok(())
+    }
+
+    async fn user_auth(
+        &self,
+        _signed_req: ed25519::Signed<UserAuthRequest>,
+    ) -> Result<UserAuthResponse, BackendApiError> {
+        // TODO(phlip9): return something we can verify
+        Ok(UserAuthResponse {
+            user_auth_token: UserAuthToken(ByteStr::new()),
+        })
+    }
+}
+
+#[async_trait]
+impl NodeBackendApi for MockBackendClient {
+    /// Always return the dummy version
+    async fn get_user(
+        &self,
+        user_pk: UserPk,
+    ) -> Result<Option<User>, BackendApiError> {
+        Ok(Some(User {
+            user_pk,
+            node_pk: node_pk(user_pk),
+        }))
+    }
+
+    /// Always return the dummy version
+    async fn get_sealed_seed(
+        &self,
+        data: SealedSeedId,
+    ) -> Result<Option<SealedSeed>, BackendApiError> {
+        Ok(Some(sealed_seed(&data.user_pk)))
+    }
+
+    async fn create_sealed_seed(
+        &self,
+        _data: SealedSeed,
+        _auth: UserAuthToken,
+    ) -> Result<(), BackendApiError> {
+        Ok(())
+    }
+
+    async fn get_scid(
+        &self,
+        _node_pk: NodePk,
+    ) -> Result<Option<Scid>, BackendApiError> {
+        Ok(Some(DUMMY_SCID))
+    }
+
+    async fn get_file(
+        &self,
+        file_id: &NodeFileId,
+        _auth: UserAuthToken,
+    ) -> Result<Option<NodeFile>, BackendApiError> {
+        let file_opt = self.0.lock().unwrap().get(file_id.clone());
+        Ok(file_opt)
+    }
+
+    async fn create_file(
+        &self,
+        file: &NodeFile,
+        _auth: UserAuthToken,
+    ) -> Result<NodeFile, BackendApiError> {
+        let file_opt = self.0.lock().unwrap().insert(file.clone());
+        assert!(file_opt.is_none());
+        Ok(file.clone())
+    }
+
+    async fn upsert_file(
+        &self,
+        file: &NodeFile,
+        _auth: UserAuthToken,
+    ) -> Result<(), BackendApiError> {
+        self.0.lock().unwrap().insert(file.clone());
+        Ok(())
+    }
+
+    /// Returns "OK" if exactly one row was deleted.
+    async fn delete_file(
+        &self,
+        file_id: &NodeFileId,
+        _auth: UserAuthToken,
+    ) -> Result<String, BackendApiError> {
+        let file_opt = self.0.lock().unwrap().remove(file_id.clone());
+        assert!(file_opt.is_none());
+        Ok(String::from("OK"))
+    }
+
+    async fn get_directory(
+        &self,
+        dir: &NodeDirectory,
+        _auth: UserAuthToken,
+    ) -> Result<Vec<NodeFile>, BackendApiError> {
+        let files_vec = self.0.lock().unwrap().get_dir(dir.clone());
+        Ok(files_vec)
+    }
 }
 
 // --- The MockApiClient ---
