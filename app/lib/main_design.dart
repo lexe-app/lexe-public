3663c039e5e67a730e26f1a490489ed3a9370148
Philip Hayes <philiphayes9@gmail.com>
2023-03-06 20:06:38 -0800

    app: add alternate app entrypoint specifically for designing components
    * allows designer to build UIs without having to spin up the whole app
      state

---
+++ app/lib/main_design.dart
@@ -0,0 +1,68 @@
+// An alternate application entrypoint specifically for designing pages
+// and components in isolation, without actually touching any real backends.
+
+import 'package:flutter/material.dart';
+
+import 'route/landing.dart' show LandingPage;
+import 'route/backup_wallet.dart' show BackupWalletPage;
+import 'route/wallet.dart' show WalletPage;
+
+import 'style.dart' show Fonts, LxColors, Space;
+
+Future<void> main() async {
+  runApp(MaterialApp(
+    title: 'Lexe',
+    themeMode: ThemeMode.light,
+    home: ComponentList(components: [
+      Component("LandingPage", (_) => const LandingPage()),
+      Component("BackupWalletPage", (_) => const SizedBox()), // TODO
+      Component("WalletPage", (_) => const WalletPage()),
+    ]),
+  ));
+}
+
+class Component {
+  const Component(this.name, this.builder);
+
+  final String name;
+  final WidgetBuilder builder;
+}
+
+class ComponentList extends StatelessWidget {
+  const ComponentList({super.key, required this.components});
+
+  final List<Component> components;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: LxColors.background,
+      padding: const EdgeInsets.only(top: Space.s700),
+      child: ListView.builder(
+          padding: const EdgeInsets.all(Space.s400),
+          itemCount: components.length,
+          itemBuilder: (BuildContext context, int index) {
+            final component = components[index];
+
+            return GestureDetector(
+              onTap: () {
+                Navigator.of(context).push(MaterialPageRoute(
+                  maintainState: false,
+                  builder: component.builder,
+                ));
+              },
+              child: SizedBox(
+                height: Space.s700,
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    component.name,
+                    style: Fonts.fontBody,
+                  ),
+                ),
+              ),
+            );
+          }),
+    );
+  }
+}
