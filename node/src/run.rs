9d1c3d7e74d815b7a2f9a45b7bb766917e505773
Max Fang <protonmail.com@maxfa.ng>
2023-03-07 20:37:43 -0800

    node: Allow inheriting parent span
    This is useful when we are e.g. running multiple nodes in one
    integration test and need to differentiate between them

--- node/src/run.rs
+++ node/src/run.rs
@@ -107,7 +107,7 @@ impl UserNode {
     // into two stages: (1) fetch and (2) deserialize. Optimistically fetch all
     // the data in ~one roundtrip to the API, and then deserialize the data in
     // the required order.
-    #[instrument(skip_all, parent = None, name = "(node)")]
+    #[instrument(skip_all, name = "(node)")]
     pub async fn init<R: Crng>(
         rng: &mut R,
         args: RunArgs,
@@ -490,7 +490,7 @@ impl UserNode {
         })
     }
 
-    #[instrument(skip_all, parent = None, name = "(node)")]
+    #[instrument(skip_all, name = "(node)")]
     pub async fn sync(&mut self) -> anyhow::Result<()> {
         info!("Starting sync");
         let ctxt = self.sync.take().expect("sync() must be called only once");
@@ -545,7 +545,7 @@ impl UserNode {
         Ok(())
     }
 
-    #[instrument(skip_all, parent = None, name = "(node)")]
+    #[instrument(skip_all, name = "(node)")]
     pub async fn run(mut self) -> anyhow::Result<()> {
         info!("Running...");
         assert!(self.sync.is_none(), "Must sync before run");
