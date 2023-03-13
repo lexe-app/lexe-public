8834885782a2811348d2fcee40f234895c22166a
Max Fang <protonmail.com@maxfa.ng>
2023-03-12 23:06:12 -0700

    refactor: Remove `spawn_p2p_listener` from `lexe-ln`

--- lexe-ln/src/p2p.rs
+++ lexe-ln/src/p2p.rs
@@ -9,12 +9,11 @@ use common::ln::peer::ChannelPeer;
 use common::shutdown::ShutdownChannel;
 use common::task::LxTask;
 use futures::future;
-use futures::stream::{FuturesUnordered, StreamExt};
 use lightning::ln::msgs::NetAddress;
-use tokio::net::{TcpListener, TcpStream};
+use tokio::net::TcpStream;
 use tokio::sync::mpsc;
 use tokio::time;
-use tracing::{debug, error, info, info_span, warn, Instrument};
+use tracing::{debug, info, info_span, warn, Instrument};
 
 use crate::traits::{LexeChannelManager, LexePeerManager, LexePersister};
 
@@ -283,79 +282,4 @@ where
         }
         .instrument(info_span!("(p2p-reconnector)")),
     )
-}
-
-/// Given a [`TcpListener`], spawns a task to await on inbound connections,
-/// handing off the resultant `TcpStream`s for the `PeerManager` to manage.
-pub fn spawn_p2p_listener<CM, PM, PS>(
-    listener: TcpListener,
-    peer_manager: PM,
-    mut shutdown: ShutdownChannel,
-) -> LxTask<()>
-where
-    CM: LexeChannelManager<PS>,
-    PM: LexePeerManager<CM, PS>,
-    PS: LexePersister,
-{
-    LxTask::spawn_named("p2p listener", async move {
-        let mut child_tasks = FuturesUnordered::new();
-
-        loop {
-            tokio::select! {
-                accept_res = listener.accept() => {
-                    // TcpStream boilerplate
-                    let (tcp_stream, peer_addr) = match accept_res {
-                        Ok(ts) => ts,
-                        Err(e) => {
-                            warn!("Failed to accept connection: {e:#}");
-                            continue;
-                        }
-                    };
-                    debug!("Accepted connection from {peer_addr}");
-                    let tcp_stream = match tcp_stream.into_std() {
-                        Ok(s) => s,
-                        Err(e) => {
-                            warn!("Couldn't convert to std TcpStream: {e:#}");
-                            continue;
-                        }
-                    };
-
-                    // Spawn a task to await on the connection
-                    let peer_manager_clone = peer_manager.clone();
-                    let child_task = LxTask::spawn(async move {
-                        // `setup_inbound()` returns a future that completes
-                        // when the connection is closed. The main thread calls
-                        // peer_manager.disconnect_all_peers() once it receives
-                        // a shutdown signal so there is no need to pass in a
-                        // `shutdown`s here.
-                        let connection_closed = lightning_net_tokio::setup_inbound(
-                            peer_manager_clone,
-                            tcp_stream,
-                        );
-                        connection_closed.await;
-                    });
-
-                    child_tasks.push(child_task);
-                }
-                // To prevent a memory leak of LxTasks, we select! on the
-                // futures unordered so that we can clear out LxTasks for peers
-                // that disconnect before the node shuts down.
-                Some(join_res) = child_tasks.next() => {
-                    if let Err(e) = join_res {
-                        error!("P2P connection task panicked: {e:#}");
-                    }
-                }
-                _ = shutdown.recv() => break,
-            }
-        }
-
-        // Wait on all child tasks to finish (i.e. all connections close).
-        while let Some(join_res) = child_tasks.next().await {
-            if let Err(e) = join_res {
-                error!("P2P connection task panicked: {e:#}");
-            }
-        }
-
-        info!("LN P2P listen task complete");
-    }.instrument(info_span!("(p2p-listener)")))
 }
