a4873e87549c4f20a200fec7ef52f76fbabdc5ac
Max Fang <protonmail.com@maxfa.ng>
2023-03-12 19:34:23 -0700

    backoff: Add `get_backoff_iter_custom`

--- common/src/backoff.rs
+++ common/src/backoff.rs
@@ -23,9 +23,23 @@ const_assert!(INITIAL_WAIT_MS != 0);
 /// # }
 /// ```
 pub fn get_backoff_iter() -> impl Iterator<Item = Duration> {
-    (0u32..).map(|index| {
+    iter_with_initial_wait_ms(INITIAL_WAIT_MS)
+}
+
+/// Like [`get_backoff_iter`], but allows specifying the initial wait time in
+/// milliseconds.
+// Haven't seen a good use case for customizing the maximum wait time yet, so
+// for now we don't expose `maximum_wait_ms` as a parameter.
+pub fn iter_with_initial_wait_ms(
+    initial_wait_ms: u64,
+) -> impl Iterator<Item = Duration> {
+    // The initial wait being greater than the maximum wait won't cause any
+    // problems, but the programmer probably didn't intend this.
+    debug_assert!(initial_wait_ms <= MAXIMUM_WAIT_MS);
+
+    (0u32..).map(move |index| {
         let factor = EXP_BASE.saturating_pow(index);
-        let wait_ms = INITIAL_WAIT_MS.saturating_mul(factor);
+        let wait_ms = initial_wait_ms.saturating_mul(factor);
         let bounded_wait_ms = min(wait_ms, MAXIMUM_WAIT_MS);
         Duration::from_millis(bounded_wait_ms)
     })
