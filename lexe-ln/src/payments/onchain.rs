362b76c549a1b911ba42ff22922c2dc5185030d6
Max Fang <protonmail.com@maxfa.ng>
2023-03-18 00:01:49 -0700

    payments: Implemnt Payment::id

--- lexe-ln/src/payments/onchain.rs
+++ lexe-ln/src/payments/onchain.rs
@@ -1,11 +1,16 @@
+use bitcoin::Txid;
 use serde::{Deserialize, Serialize};
 
 // --- Onchain deposits --- //
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
-pub struct OnchainDeposit {}
+pub struct OnchainDeposit {
+    pub txid: Txid,
+}
 
 // --- Onchain withdrawals --- //
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
-pub struct OnchainWithdrawal {}
+pub struct OnchainWithdrawal {
+    pub txid: Txid,
+}
