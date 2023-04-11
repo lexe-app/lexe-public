86d5b08df30251a24414081de1c09d0e96b785ed
Max Fang <protonmail.com@maxfa.ng>
2023-04-10 19:42:14 -0700

    command+: Allow caller to specify fallback amount

--- common/src/api/command.rs
+++ common/src/api/command.rs
@@ -2,6 +2,7 @@ use serde::{Deserialize, Serialize};
 
 use crate::api::NodePk;
 use crate::ln::channel::LxChannelDetails;
+use crate::ln::invoice::LxInvoice;
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct NodeInfo {
@@ -22,4 +23,13 @@ pub struct CreateInvoiceRequest {
     pub expiry_secs: u32,
     pub amt_msat: Option<u64>,
     pub description: String,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct PayInvoiceRequest {
+    /// The invoice we want to pay.
+    pub invoice: LxInvoice,
+    /// Specifies the msat amount we will pay if the invoice to be paid is
+    /// amountless. This field must be [`Some`] for amountless invoices.
+    pub fallback_amt_msat: Option<u64>,
 }
