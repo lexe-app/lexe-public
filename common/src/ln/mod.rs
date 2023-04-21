dd3084a783fe2b046f024b2d92e0be3a2a2378bd
Max Fang <protonmail.com@maxfa.ng>
2023-04-21 13:06:03 -0700

    ln: Add `ConfirmationPriority` newtype

--- common/src/ln/mod.rs
+++ common/src/ln/mod.rs
@@ -1,6 +1,11 @@
 //! Bitcoin / Lightning Lexe newtypes which have to be in `common` for some
 //! reason, likely because they are referenced in an API definition somewhere.
 
+use lightning::chain::chaininterface::ConfirmationTarget;
+#[cfg(any(test, feature = "test-utils"))]
+use proptest_derive::Arbitrary;
+use serde::{Deserialize, Serialize};
+
 /// `Amount`.
 pub mod amount;
 /// Channel outpoint, details, counterparty
@@ -13,3 +18,22 @@ pub mod invoice;
 pub mod payments;
 /// `ChannelPeer`.
 pub mod peer;
+
+/// A newtype for [`ConfirmationTarget`] with [`serde`] and proptest impls.
+#[derive(Copy, Clone, Debug, Eq, PartialEq, Serialize, Deserialize)]
+#[cfg_attr(any(test, feature = "test-utils"), derive(Arbitrary))]
+pub enum ConfirmationPriority {
+    High,
+    Normal,
+    Background,
+}
+
+impl From<ConfirmationPriority> for ConfirmationTarget {
+    fn from(lexe_newtype: ConfirmationPriority) -> Self {
+        match lexe_newtype {
+            ConfirmationPriority::High => Self::HighPriority,
+            ConfirmationPriority::Normal => Self::Normal,
+            ConfirmationPriority::Background => Self::Background,
+        }
+    }
+}
