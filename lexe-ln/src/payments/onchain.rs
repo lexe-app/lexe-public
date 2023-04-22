d1f877da0c3fe7df5141b4c28d3fd82b8a173437
Max Fang <protonmail.com@maxfa.ng>
2023-04-21 20:47:00 -0700

    payments: Add more onchain payments states

--- lexe-ln/src/payments/onchain.rs
+++ lexe-ln/src/payments/onchain.rs
@@ -25,8 +25,7 @@ pub struct OnchainSend {
     pub fees: Amount,
     pub status: OnchainSendStatus,
     pub created_at: TimestampMs,
-    /// An optional personal note for this payment. The user can only add a
-    /// note after this onchain receive has been detected.
+    /// An optional personal note for this payment.
     #[cfg_attr(test, proptest(strategy = "arbitrary::any_option_string()"))]
     pub note: Option<String>,
     pub finalized_at: Option<TimestampMs>,
@@ -35,11 +34,43 @@ pub struct OnchainSend {
 #[derive(Copy, Clone, Debug, Eq, PartialEq, Serialize, Deserialize)]
 #[cfg_attr(test, derive(Arbitrary))]
 pub enum OnchainSendStatus {
-    AwaitingBroadcast,
-    Confirming,
-    Completed,
-    Replaced,
-    Reorged,
+    /// (Pending, not broadcasted) The tx has been created and signed but
+    /// hasn't been broadcasted yet.
+    Created,
+
+    /// (Pending, zeroconf) The tx has been broadcasted and is awaiting its
+    /// first confirmation.
+    Broadcasted,
+    /// (Pending, zeroconf) We broadcasted a RBF replacement or some other
+    /// transaction that spends at least one of this transaction's inputs, with
+    /// the intention of confirming the newer transaction.
+    ReplacementBroadcasted,
+
+    /// (Pending, 1-5 confs) The tx has at least 1 conf, but no greater than 5.
+    PartiallyConfirmed,
+    /// (Pending, 1-5 confs) At least one of this tx's inputs has been included
+    /// in a different tx which has at least 1 conf, but no greater than 5.
+    PartiallyReplaced,
+
+    /// (Finalized-Completed, 6+ confs) The tx has 6 or more confirmations.
+    FullyConfirmed,
+    /// (Finalized-Failed, 6+ confs) At least one of this tx's inputs has been
+    /// spent by a different tx which has between 6 or greater confirmations.
+    FullyReplaced,
+    /// (Finalized-Failed, zeroconf) All of the following are true:
+    ///
+    /// - This tx has not received a single confirmation.
+    /// - We have not detected a replacement tx spending at least one of this
+    ///   tx's inputs with 1 or more confirmations.
+    /// - It has been at least 14 days since we first created this transaction.
+    ///
+    /// 14 days is the default `-mempoolexpiry` value in Bitcoin Core. It is
+    /// likely that most nodes will have evicted our transaction from their
+    /// mempool by now. There is a small chance that this transaction ends up
+    /// getting confirmed, but we'll mark it as failed in our payments manager
+    /// and move on, since this isn't security-critical; the user will still
+    /// see the successful send reflected in their wallet balance.
+    Dropped,
 }
 
 impl OnchainSend {
@@ -50,7 +81,7 @@ impl OnchainSend {
             priority: req.priority,
             amount: req.amount,
             fees,
-            status: OnchainSendStatus::AwaitingBroadcast,
+            status: OnchainSendStatus::Created,
             created_at: TimestampMs::now(),
             note: req.note,
             finalized_at: None,
@@ -66,14 +97,17 @@ impl OnchainSend {
         ensure!(broadcasted_txid == &self.txid, "Txids don't match");
 
         match self.status {
-            AwaitingBroadcast => (),
-            Confirming => warn!("Transaction was already broadcasted"),
-            Completed | Replaced | Reorged => bail!("Tx already finalized"),
+            Created => (),
+            Broadcasted => warn!("We broadcasted this transaction twice"),
+            PartiallyConfirmed => bail!("Tx already has confirmations"),
+            ReplacementBroadcasted => bail!("Tx was being replaced"),
+            PartiallyReplaced => bail!("Tx already partially replaced"),
+            FullyConfirmed | FullyReplaced | Dropped => bail!("Tx was final"),
         }
 
         // Everything ok; return a clone with the updated state
         let mut clone = self.clone();
-        clone.status = Confirming;
+        clone.status = Broadcasted;
 
         Ok(clone)
     }
@@ -99,8 +133,34 @@ pub struct OnchainReceive {
 #[derive(Copy, Clone, Debug, Eq, PartialEq, Serialize, Deserialize)]
 #[cfg_attr(test, derive(Arbitrary))]
 pub enum OnchainReceiveStatus {
-    Confirming,
-    Completed,
-    Replaced,
-    Reorged,
+    /// (Pending, zeroconf) We detected the inbound tx, but it is still
+    /// awaiting its first confirmation.
+    Zeroconf,
+
+    /// (Pending, 1-5 confs) The tx has at least 1 conf, but no greater than 5.
+    PartiallyConfirmed,
+    /// (Pending, 1-5 confs) At least one of this tx's inputs has been included
+    /// in a different tx which has at least 1 conf, but no greater than 5.
+    PartiallyReplaced,
+
+    /// (Finalized-Completed, 6+ confs) The tx has 6 or more confirmations.
+    FullyConfirmed,
+    /// (Finalized-Failed, 6+ confs) At least one of this tx's inputs has been
+    /// spent by a different tx which has between 6 or greater confirmations.
+    FullyReplaced,
+    /// (Finalized-Failed, zeroconf) All of the following are true:
+    ///
+    /// - This tx has not received a single confirmation.
+    /// - We have not detected a replacement tx spending at least one of this
+    ///   tx's inputs with 1 or more confirmations.
+    /// - It has been at least 14 days since we first detected this
+    ///   transaction.
+    ///
+    /// 14 days is the default `-mempoolexpiry` value in Bitcoin Core. It is
+    /// likely that most nodes will have evicted our transaction from their
+    /// mempool by now. There is a small chance that this transaction ends up
+    /// getting confirmed, but we'll mark it as failed in our payments manager
+    /// and move on, since this isn't security-critical; the user will still
+    /// see the successful receive reflected in their wallet balance.
+    Dropped,
 }
