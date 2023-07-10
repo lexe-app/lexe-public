ea196950c63486d366313338ade01cc0156da60d
Philip Hayes <philiphayes9@gmail.com>
2023-07-10 16:50:35 -0700

    app: actually get the value from onchain send page

--- app/lib/route/send.dart
+++ app/lib/route/send.dart
@@ -19,15 +19,17 @@ class SendPaymentPage extends StatefulWidget {
 }
 
 class SendPaymentPageState extends State<SendPaymentPage> {
-  final formKey = GlobalKey<FormState>();
+  final GlobalKey<FormFieldState<String>> addressFieldKey = GlobalKey();
 
   void onQrPressed() {
     info("pressed QR button");
   }
 
-  void onNextPressed() {
-    if (this.formKey.currentState!.validate()) {
-      info("success");
+  void onNext() {
+    final fieldState = this.addressFieldKey.currentState!;
+    if (fieldState.validate()) {
+      final address = fieldState.value;
+      info("success: address = $address");
     }
   }
 
@@ -46,6 +48,8 @@ class SendPaymentPageState extends State<SendPaymentPage> {
 
   @override
   Widget build(BuildContext context) {
+    // TODO(phlip9): autofill address from user's clipboard if one exists
+
     return Scaffold(
       appBar: AppBar(
         leading: const LxCloseButton(kind: LxCloseButtonKind.closeFromRoot),
@@ -57,73 +61,75 @@ class SendPaymentPageState extends State<SendPaymentPage> {
           const SizedBox(width: Space.s100),
         ],
       ),
-      body: Form(
-        key: this.formKey,
-        child: ScrollableSinglePageBody(
-          body: [
-            const SizedBox(height: Space.s500),
-            Text(
-              "Who are we paying?",
-              style: Fonts.fontUI.copyWith(
-                fontSize: Fonts.size600,
-                // color: LxColors.grey500,
-                fontVariations: [Fonts.weightMedium],
-                letterSpacing: -0.5,
+      body: ScrollableSinglePageBody(
+        body: [
+          const SizedBox(height: Space.s500),
+          Text(
+            "Who are we paying?",
+            style: Fonts.fontUI.copyWith(
+              fontSize: Fonts.size600,
+              fontVariations: [Fonts.weightMedium],
+              letterSpacing: -0.5,
+            ),
+          ),
+          const SizedBox(height: Space.s600),
+          TextFormField(
+            key: this.addressFieldKey,
+            autofocus: true,
+            // `visiblePassword` gives ready access to letters + numbers
+            keyboardType: TextInputType.visiblePassword,
+            textDirection: TextDirection.ltr,
+            textInputAction: TextInputAction.next,
+            validator: this.validateBitcoinAddress,
+            onEditingComplete: this.onNext,
+            decoration: const InputDecoration.collapsed(
+              hintText: "Bitcoin address",
+              hintStyle: TextStyle(
+                color: LxColors.grey750,
               ),
             ),
-            const SizedBox(height: Space.s600),
-            TextFormField(
-              autofocus: true,
-              decoration: const InputDecoration.collapsed(
-                hintText: "Bitcoin address",
-                hintStyle: TextStyle(
-                  color: LxColors.grey750,
+            style: Fonts.fontUI.copyWith(
+              fontSize: Fonts.size700,
+              fontVariations: [Fonts.weightMedium],
+              // Use unambiguous character alternatives (0OIl1) to avoid
+              // confusion in the unfortunate event that a user has to
+              // manually type in an address.
+              fontFeatures: [Fonts.featDisambugation],
+              letterSpacing: -0.5,
+            ),
+          ),
+          const SizedBox(height: Space.s800),
+        ],
+        bottom: FilledButton(
+          onPressed: this.onNext,
+          style: FilledButton.styleFrom(
+            backgroundColor: LxColors.grey1000,
+            disabledBackgroundColor: LxColors.grey850,
+            foregroundColor: LxColors.foreground,
+            disabledForegroundColor: LxColors.grey725,
+            maximumSize: const Size.fromHeight(Space.s700),
+          ),
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              Text(
+                "Next",
+                style: Fonts.fontInter.copyWith(
+                  fontSize: Fonts.size300,
+                  fontVariations: [Fonts.weightMedium],
                 ),
               ),
-              validator: this.validateBitcoinAddress,
-              style: Fonts.fontUI.copyWith(
-                fontSize: Fonts.size700,
-                fontVariations: [Fonts.weightMedium],
-                // Use unambiguous character alternatives (0OIl1) to avoid
-                // confusion in the unfortunate event that a user has to
-                // manually type in an address.
-                fontFeatures: [Fonts.featDisambugation],
-                letterSpacing: -0.5,
-              ),
-            ),
-            const SizedBox(height: Space.s800),
-          ],
-          bottom: FilledButton(
-            onPressed: this.onNextPressed,
-            style: FilledButton.styleFrom(
-              backgroundColor: LxColors.grey1000,
-              disabledBackgroundColor: LxColors.grey850,
-              foregroundColor: LxColors.foreground,
-              disabledForegroundColor: LxColors.grey725,
-              maximumSize: const Size.fromHeight(Space.s700),
-            ),
-            child: Stack(
-              alignment: Alignment.center,
-              children: [
-                Text(
-                  "Next",
-                  style: Fonts.fontInter.copyWith(
-                    fontSize: Fonts.size300,
-                    fontVariations: [Fonts.weightMedium],
+              const Align(
+                alignment: Alignment.centerRight,
+                child: Padding(
+                  padding: EdgeInsets.symmetric(vertical: Space.s200),
+                  child: Icon(
+                    Icons.arrow_forward_rounded,
+                    size: Fonts.size300,
                   ),
                 ),
-                const Align(
-                  alignment: Alignment.centerRight,
-                  child: Padding(
-                    padding: EdgeInsets.symmetric(vertical: Space.s200),
-                    child: Icon(
-                      Icons.arrow_forward_rounded,
-                      size: Fonts.size300,
-                    ),
-                  ),
-                ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
