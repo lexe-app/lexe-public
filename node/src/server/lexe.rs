8c68bf28e21df9740de09010d075c6d3cc6b43f3
Max Fang <protonmail.com@maxfa.ng>
2023-07-14 15:43:33 -0700

    node::server: cfg out `/open_channel` in prod

--- node/src/server/lexe.rs
+++ node/src/server/lexe.rs
@@ -1,18 +1,14 @@
-use anyhow::Context;
 use common::{
     api::{
         command::OpenChannelRequest, error::NodeApiError, qs::GetByUserPk,
         UserPk,
     },
     ln::peer::ChannelPeer,
-    rng::SysRng,
     shutdown::ShutdownChannel,
 };
-use lexe_ln::{channel, channel::ChannelRelationship};
 
 use crate::{
-    channel_manager, channel_manager::NodeChannelManager,
-    peer_manager::NodePeerManager,
+    channel_manager::NodeChannelManager, peer_manager::NodePeerManager,
 };
 
 pub async fn status(
@@ -34,19 +30,37 @@ pub async fn open_channel(
     peer_manager: NodePeerManager,
     lsp_channel_peer: ChannelPeer,
 ) -> anyhow::Result<()> {
-    let mut rng = SysRng::new();
-    let user_channel_id = channel::get_random_u128(&mut rng);
-    let relationship = ChannelRelationship::UserToLsp { lsp_channel_peer };
-    lexe_ln::channel::open_channel(
-        channel_manager,
-        peer_manager,
-        user_channel_id,
-        req.value,
-        relationship,
-        channel_manager::USER_CONFIG,
-    )
-    .await
-    .context("Failed to open channel to LSP")
+    cfg_if::cfg_if! {
+        // TODO(max): This needs to switch to #[cfg(feature = "test-utils")],
+        // otherwise this will break the SGX integration tests.
+        if #[cfg(any(test, not(target_env = "sgx")))] {
+            use anyhow::Context;
+            use common::rng::SysRng;
+            use lexe_ln::{channel, channel::ChannelRelationship};
+            use crate::channel_manager;
+
+            let mut rng = SysRng::new();
+            let user_channel_id = channel::get_random_u128(&mut rng);
+            let relationship =
+                ChannelRelationship::UserToLsp { lsp_channel_peer };
+            lexe_ln::channel::open_channel(
+                channel_manager,
+                peer_manager,
+                user_channel_id,
+                req.value,
+                relationship,
+                channel_manager::USER_CONFIG,
+            )
+            .await
+            .context("Failed to open channel to LSP")
+        } else {
+            let _ = req;
+            let _ = channel_manager;
+            let _ = peer_manager;
+            let _ = lsp_channel_peer;
+            anyhow::bail!("This endpoint is disabled in staging/prod");
+        }
+    }
 }
 
 pub fn shutdown(
