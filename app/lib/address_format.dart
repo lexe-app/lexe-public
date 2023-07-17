22b736a376d780d6eb69e3310b9e6dedd3e87378
Philip Hayes <philiphayes9@gmail.com>
2023-07-17 15:37:11 -0700

    app: add `address_format.ellipsizeBtcAddress`

---
+++ app/lib/address_format.dart
@@ -0,0 +1,25 @@
+/// Bitcoin and lightning address formatting
+
+/// Shorten a long bitcoin address by inserting an ellipsis (...) into the
+/// _middle_ while leaving the start and end intact. Users often compare
+/// addresses by both the first and last characters. In contrast, the default
+/// flutter ellipsis truncation only shows the initial characters.
+///
+/// ### Examples
+///
+/// ```dart
+/// assert(ellipsizeAddress("bc1qw508d6qejxtdg4y5r3zarvary0c5xw7kv8f3t4") ==
+///     "bc1qw508…v8f3t4");
+/// );
+/// ```
+String ellipsizeBtcAddress(String address) {
+  if (address.length <= 15) {
+    return address;
+  }
+
+  final prefix = address.substring(0, 8);
+  final suffix = address.substring(address.length - 6);
+
+  // \u2026 == "…" (horizontal ellipsis)
+  return "$prefix\u2026$suffix";
+}
---
+++ app/test/address_format_test.dart
@@ -0,0 +1,20 @@
+import 'package:flutter_test/flutter_test.dart' show expect, test;
+
+import 'package:lexeapp/address_format.dart' as address_format;
+
+void main() {
+  test("address_format.ellipsizeBtcAddress", () {
+    expect(
+      address_format
+          .ellipsizeBtcAddress("bc1qw508d6qejxtdg4y5r3zarvary0c5xw7kv8f3t4"),
+      "bc1qw508\u2026v8f3t4",
+    );
+
+    const address = "bc1qw508d6qejxtdg4y5r3zarvary0c5xw7kv8f3t4";
+
+    for (var i = 0; i <= address.length; i += 1) {
+      final prefix = address.substring(0, i);
+      assert(address_format.ellipsizeBtcAddress(prefix).length <= 15);
+    }
+  });
+}
