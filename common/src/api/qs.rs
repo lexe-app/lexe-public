e442be50b46668f69352e9bcd903b2a6ad4bc904
Philip Hayes <philiphayes9@gmail.com>
2023-07-27 20:43:50 -0700

    common: qs: add proptest roundtrip assertions for querystring types

--- common/src/api/qs.rs
+++ common/src/api/qs.rs
@@ -1,3 +1,5 @@
+#[cfg(any(test, feature = "test-utils"))]
+use proptest_derive::Arbitrary;
 use serde::{Deserialize, Serialize};
 
 #[cfg(doc)]
@@ -11,6 +13,13 @@ use crate::{
 // structs (instead of e.g. using UserPk directly), otherwise `serde_qs` errors
 // with "top-level serializer supports only maps and structs."
 
+// Also beware when using `#[serde(flatten)]` on a field. All inner fields must
+// be string-ish types (&str, String, Cow<'_, str>, etc...) OR use
+// `SerializeDisplay` and `DeserializeFromStr` from `serde_with`.
+//
+// This issue is due to a limitation in serde. See:
+// <https://github.com/serde-rs/serde/issues/1183>
+
 /// Query parameter struct for fetching with no data attached.
 ///
 /// Is defined with {} otherwise serde_qs vomits
@@ -18,25 +27,29 @@ use crate::{
 pub struct EmptyData {}
 
 /// Query parameter struct for fetching by user pk
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+#[cfg_attr(any(test, feature = "test-utils"), derive(Arbitrary))]
 pub struct GetByUserPk {
     pub user_pk: UserPk,
 }
 
 /// Query parameter struct for fetching by node pk
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+#[cfg_attr(any(test, feature = "test-utils"), derive(Arbitrary))]
 pub struct GetByNodePk {
     pub node_pk: NodePk,
 }
 
 /// Query parameter struct for fetching by scid
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+#[cfg_attr(any(test, feature = "test-utils"), derive(Arbitrary))]
 pub struct GetByScid {
     pub scid: Scid,
 }
 
 /// Query parameter struct for fetching a payment by its index.
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+#[cfg_attr(any(test, feature = "test-utils"), derive(Arbitrary))]
 pub struct GetPaymentByIndex {
     /// The index of the payment to be fetched.
     // We use index instead of id so the backend can query by primary key.
@@ -45,7 +58,8 @@ pub struct GetPaymentByIndex {
 
 /// Query parameter struct for syncing batches of new payments to local storage.
 /// Results are returned in ascending `(created_at, payment_id)` order.
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+#[cfg_attr(any(test, feature = "test-utils"), derive(Arbitrary))]
 pub struct GetNewPayments {
     /// Optional [`PaymentIndex`] at which the results should start, exclusive.
     /// Payments with an index less than or equal to this will not be returned.
@@ -74,4 +88,35 @@ pub struct UpdatePaymentNote {
     pub index: PaymentIndex,
     /// The updated note.
     pub note: Option<String>,
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::test_utils::roundtrip::query_string_roundtrip_proptest;
+
+    #[test]
+    fn get_by_user_pk_roundtrip() {
+        query_string_roundtrip_proptest::<GetByUserPk>();
+    }
+
+    #[test]
+    fn get_by_node_pk_roundtrip() {
+        query_string_roundtrip_proptest::<GetByNodePk>();
+    }
+
+    #[test]
+    fn get_by_scid_roundtrip() {
+        query_string_roundtrip_proptest::<GetByScid>();
+    }
+
+    #[test]
+    fn get_payment_by_index_roundtrip() {
+        query_string_roundtrip_proptest::<GetPaymentByIndex>();
+    }
+
+    #[test]
+    fn get_new_payments_roundtrip() {
+        query_string_roundtrip_proptest::<GetNewPayments>();
+    }
 }
