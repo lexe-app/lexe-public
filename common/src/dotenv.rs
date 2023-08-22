2c93318f7b972f8f02343cb695f0ca827975e988
Max Fang <protonmail.com@maxfa.ng>
2023-08-22 12:57:08 -0700

    common: Add common::envy
    This allows us to work with vars defined in `.env` in cleaner and more
    explicit ways.

---
+++ common/src/dotenv.rs
@@ -0,0 +1,38 @@
+use std::env;
+
+/// A version of [`dotenvy::dotenv`] which only loads whitelisted keys.
+pub fn dotenv_filtered(filter_keys: &[&str]) -> Result<(), dotenvy::Error> {
+    // `dotenv_iter` finds an .env file in the current directory (or parents),
+    // then returns an iterator over it without loading the variables within.
+    for try_kv in dotenvy::dotenv_iter()? {
+        let (key, value) = try_kv?;
+        if filter_keys.contains(&key.as_str()) {
+            // Like dotenvy::dotenv(), do not override existing keys.
+            if env::var(&key).is_err() {
+                env::set_var(&key, value);
+            }
+        }
+    }
+
+    Ok(())
+}
+
+/// Fetches the given key from env or `.env`, but without loading any of the
+/// keys in `.env` into our env (including the requested key).
+/// In other words, it is a "pure" version of [`dotenvy::var`].
+pub fn var_pure(given_key: &str) -> Result<String, dotenvy::Error> {
+    // Early return if the key already existed in env.
+    if let Ok(value) = env::var(given_key) {
+        return Ok(value);
+    }
+
+    // Look for the key in .env. We do not set the key in env if it was found.
+    for try_kv in dotenvy::dotenv_iter()? {
+        let (key, value) = try_kv?;
+        if key == given_key {
+            return Ok(value);
+        }
+    }
+
+    Err(dotenvy::Error::EnvVar(env::VarError::NotPresent))
+}
