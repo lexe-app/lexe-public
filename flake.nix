4f56572b5b0eb6511a04728b5e0eaa3b7566df86
Philip Hayes <philiphayes9@gmail.com>
2023-09-20 20:15:22 -0700

    nix: add `alejandra` nix file formatter
    - run with `nix fmt`

--- flake.nix
+++ flake.nix
@@ -5,5 +5,51 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
-  outputs = {self, nixpkgs}: {};
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    # supported systems
+    systems = [
+      "x86_64-linux"
+      "aarch64-linux"
+      "aarch64-darwin"
+      "x86_64-darwin"
+    ];
+
+    # genAttrs :: [ String ] -> (String -> Any) -> AttrSet
+    #
+    # ```
+    # > genAttrs [ "bob" "joe" ] (name: "hello ${name}")
+    # { bob = "hello bob"; joe = "hello joe" }
+    # ```
+    genAttrs = nixpkgs.lib.genAttrs;
+
+    # eachSystem :: (String -> AttrSet) -> AttrSet
+    #
+    # ```
+    # > eachSystem (system: { a = 123; b = "cool ${system}"; })
+    # {
+    #   "aarch64-darwin" = {
+    #     a = 123;
+    #     b = "cool aarch64-darwin";
+    #   };
+    #   "x86_64-linux" = {
+    #     a = 123;
+    #     b = "cool x86_64-linux";
+    #   };
+    # }
+    # ```
+    eachSystem = builder: genAttrs systems builder;
+
+    # eachSystemPkgs :: (Nixpkgs -> AttrSet) -> AttrSet
+    eachSystemPkgs = builder:
+      eachSystem (
+        system:
+          builder nixpkgs.legacyPackages.${system}
+      );
+  in {
+    # The *.nix file formatter.
+    formatter = eachSystemPkgs (pkgs: pkgs.alejandra);
+  };
 }
