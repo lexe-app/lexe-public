4c3becb38c462c5c3ed6bb4a4ec5f8c315ce0088
Philip Hayes <philiphayes9@gmail.com>
2023-09-26 11:12:39 -0700

    nix: node failing to link b/c ring is adding -fstack-protector
    - will remove this in a ring patch in next commit

--- sgx-libc-shim/include/string.h
+++ sgx-libc-shim/include/string.h
@@ -1,6 +1,10 @@
 #ifndef	_STRING_H
 #define	_STRING_H	1
 
+#define	__need_size_t
+#define	__need_NULL
+#include <stddef.h>
+
 extern void *memcpy (void *__restrict dest, const void *__restrict src, size_t n);
 extern void *memmove (void *dest, const void *src, size_t n);
 extern void *memset (void *s, int c, size_t n);
