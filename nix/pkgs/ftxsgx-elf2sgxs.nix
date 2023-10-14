7ae4b0df104b95dc79f3ea247cc7938648be6df1
Philip Hayes <philiphayes9@gmail.com>
2023-10-13 18:05:47 -0700

    nix: extract `parseCargoLockGitDep` to lib

--- nix/pkgs/ftxsgx-elf2sgxs.nix
+++ nix/pkgs/ftxsgx-elf2sgxs.nix
@@ -8,34 +8,12 @@
 # exact same enclave measurement as the SGX platform, without having to actually
 # load the enclave.
 {
-  lib,
+  lexePubLib,
   craneLib,
 }: let
-  # Parse the git revision of a git dependency from a `Cargo.lock` file.
-  # ex return: "4aa8f13487c772dd4d24b7cc54bd2d5432803f7a"
-  parseCargoLockGitDep = {
-    # ex: `builtins.readFile ./Cargo.lock`
-    cargoLockContents,
-    # ex: "https://github.com/lexe-app/rust-sgx"
-    githubUrl,
-  }: let
-    inherit (builtins) elemAt filter head match;
-    inherit (lib) splitString escapeRegex;
-
-    lines = splitString "\n" cargoLockContents;
-    escapedUrl = escapeRegex githubUrl;
-    pattern = "source = \"git\\+${escapedUrl}\\?branch=([^#]+)#([0-9a-f]{40})\"";
-    firstMatchingLine = head (filter (line: (match pattern line) != null) lines);
-    matches = match pattern firstMatchingLine;
-  in {
-    url = "${githubUrl}.git";
-    ref = elemAt matches 0;
-    rev = elemAt matches 1;
-  };
-
-  rustSgxRepo = parseCargoLockGitDep {
-    githubUrl = "https://github.com/lexe-app/rust-sgx";
+  rustSgxRepo = lexePubLib.parseCargoLockGitDep {
     cargoLockContents = builtins.readFile ../../Cargo.lock;
+    githubUrl = "https://github.com/lexe-app/rust-sgx";
   };
 
   rustSgxSrc = builtins.fetchGit {
