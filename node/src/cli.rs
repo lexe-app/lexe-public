788d529bd997e399e246aedece46d66a725a2284
Max Fang <protonmail.com@maxfa.ng>
2024-01-23 20:27:48 +0800

    cli: Parse CLI args manually, remove argh

--- node/src/cli.rs
+++ node/src/cli.rs
@@ -1,9 +1,9 @@
-use std::sync::Arc;
+use std::{env, str::FromStr, sync::Arc};
 
-use anyhow::Context;
-use argh::FromArgs;
+use anyhow::{bail, Context};
 use common::{
     cli::node::{ProvisionArgs, RunArgs},
+    enclave,
     rng::SysRng,
 };
 
@@ -11,48 +11,52 @@ use crate::{
     api::client::{BackendClient, RunnerClient},
     provision,
     run::UserNode,
+    DEV_VERSION, SEMVER_VERSION,
 };
 
-/// A wrapper around [`NodeCommand`] that serves as [`argh::TopLevelCommand`].
-#[derive(Debug, Eq, PartialEq, FromArgs)]
-pub struct NodeArgs {
-    /// show the current version, then exit.
-    #[argh(switch)]
-    pub version: bool,
-
-    // Has to be Option otherwise --version doesn't work
-    #[argh(subcommand)]
-    command: Option<NodeCommand>,
-}
-
 /// Commands accepted by the user node.
-#[derive(Clone, Debug, Eq, PartialEq, FromArgs)]
-#[argh(subcommand)]
-#[allow(clippy::large_enum_variant)] // It will be Run most of the time
 pub enum NodeCommand {
-    Run(RunArgsWrapper),
-    Provision(ProvisionArgsWrapper),
+    Run(RunArgs),
+    Provision(ProvisionArgs),
 }
 
-/// A `FromArgs` impl which takes [`RunArgs`] as a positional argument.
-#[derive(Clone, Debug, Eq, PartialEq, FromArgs)]
-#[argh(subcommand, name = "run")]
-pub struct RunArgsWrapper {
-    /// the JSON string-serialized [`RunArgs`].
-    #[argh(positional)]
-    pub args: RunArgs,
-}
+impl NodeCommand {
+    /// Try to parse a [`NodeCommand`] from CLI args.
+    /// Returns [`None`] if we simply printed version or help.
+    pub fn from_env() -> anyhow::Result<Option<Self>> {
+        let mut args = env::args();
+        let _bin_path = args.next().context("No executable??")?;
 
-/// A `FromArgs` impl which takes [`ProvisionArgs`] as a positional argument.
-#[derive(Clone, Debug, Eq, PartialEq, FromArgs)]
-#[argh(subcommand, name = "provision")]
-pub struct ProvisionArgsWrapper {
-    /// the JSON string-serialized [`ProvisionArgs`].
-    #[argh(positional)]
-    pub args: ProvisionArgs,
-}
+        match (args.next().as_deref(), args.next()) {
+            // If --version or --help was given, just print and exit.
+            (Some("version"), _) | (Some("--version"), _) => {
+                let dev_version_str = DEV_VERSION.unwrap_or("None");
+                let measurement = enclave::measurement();
+                println!(
+                    "node-{SEMVER_VERSION} (Dev version: {dev_version_str})"
+                );
+                println!("Measurement: {measurement}");
+                Ok(None)
+            }
+            (Some("help"), _) | (Some("--help"), _) => {
+                print_help();
+                Ok(None)
+            }
+            (Some("run"), Some(args_str)) => {
+                let args = RunArgs::from_str(&args_str)
+                    .context("Invalid RunArgs JSON string")?;
+                Ok(Some(NodeCommand::Run(args)))
+            }
+            (Some("provision"), Some(args_str)) => {
+                let args = ProvisionArgs::from_str(&args_str)
+                    .context("Invalid ProvisionArgs JSON string")?;
+                Ok(Some(NodeCommand::Provision(args)))
+            }
+            _ => bail!("Invalid CLI options"),
+        }
+    }
 
-impl NodeArgs {
+    /// Run this [`NodeCommand`].
     pub fn run(self) -> anyhow::Result<()> {
         // We have 2 total threads configured in our `Cargo.toml`.
         //
@@ -68,12 +72,8 @@ impl NodeArgs {
             .context("Failed to build Tokio runtime")?;
         let mut rng = SysRng::new();
 
-        let command = self
-            .command
-            .context("Missing subcommand: try 'help', 'run', or 'provision'")?;
-
-        match command {
-            NodeCommand::Run(RunArgsWrapper { args }) => rt
+        match self {
+            Self::Run(args) => rt
                 .block_on(async {
                     let mut node = UserNode::init(&mut rng, args)
                         .await
@@ -82,11 +82,12 @@ impl NodeArgs {
                     node.run().await.context("Error while running")
                 })
                 .context("Error running node"),
-            NodeCommand::Provision(ProvisionArgsWrapper { args }) => {
+            Self::Provision(args) => {
                 let runner_api =
                     Arc::new(RunnerClient::new(args.runner_url.clone()));
                 let backend_api =
                     Arc::new(BackendClient::new(args.backend_url.clone()));
+
                 rt.block_on(provision::provision_node(
                     &mut rng,
                     args,
@@ -97,4 +98,12 @@ impl NodeArgs {
             }
         }
     }
+}
+
+/// Print out CLI help.
+pub fn print_help() {
+    println!(
+        "CLI format: <bin_path> <help|version|run|provision> \
+         [<JSON-string-serialized `RunArgs` or `ProvisionArgs`>]"
+    );
 }
