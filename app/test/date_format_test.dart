a81942697cdbf8d5224b0c2afc25c04706b57aee
Philip Hayes <philiphayes9@gmail.com>
2024-01-25 16:30:52 +0800

    app: fix failing test b/c of changed timezone...
    turns out DateTime.fromMillisecondsSinceEpoch doesn't default to UTC...
    very annoying

--- app/test/date_format_test.dart
+++ app/test/date_format_test.dart
@@ -6,11 +6,14 @@ void main() {
   test("date_format.formatDateCompact", () async {
     await date_format.initializeDateLocaleData();
 
+    DateTime dateTimeFromUnix(int msSinceEpoch) =>
+        DateTime.fromMillisecondsSinceEpoch(msSinceEpoch, isUtc: true);
+
     // now = "Jun 21, 2023"
-    final now = DateTime.fromMillisecondsSinceEpoch(1687385095000);
+    final now = dateTimeFromUnix(1687385095000);
 
     // -2d 15h 5m 3s = "2d"
-    final days2 = DateTime.fromMillisecondsSinceEpoch(1687157992000);
+    final days2 = dateTimeFromUnix(1687157992000);
     expect(
         date_format.formatDateCompact(now: now, then: days2, locale: "en_US"),
         "2d");
@@ -20,7 +23,7 @@ void main() {
         "2j");
 
     // -15h 5m 3s = "2d"
-    final hours15 = DateTime.fromMillisecondsSinceEpoch(1687330792000);
+    final hours15 = dateTimeFromUnix(1687330792000);
     expect(
         date_format.formatDateCompact(now: now, then: hours15, locale: "en_US"),
         "15h");
@@ -30,7 +33,7 @@ void main() {
         "15h");
 
     // -5m 3s = "5min"
-    final min5 = DateTime.fromMillisecondsSinceEpoch(1687384792000);
+    final min5 = dateTimeFromUnix(1687384792000);
     expect(date_format.formatDateCompact(now: now, then: min5, locale: "en_US"),
         "5min");
     expect(date_format.formatDateCompact(now: now, then: min5, locale: "nb"),
@@ -39,7 +42,7 @@ void main() {
         "5min");
 
     // -15 secs = "15s"
-    final secs15 = DateTime.fromMillisecondsSinceEpoch(1687385080000);
+    final secs15 = dateTimeFromUnix(1687385080000);
     expect(
         date_format.formatDateCompact(now: now, then: secs15, locale: "en_US"),
         "15s");
@@ -49,7 +52,7 @@ void main() {
         "15s");
 
     // -5d ish = June 16, 2023 = "Jun 16"
-    final jun16 = DateTime.fromMillisecondsSinceEpoch(1686938392000);
+    final jun16 = dateTimeFromUnix(1686938392000);
     expect(
         date_format.formatDateCompact(now: now, then: jun16, locale: "en_US"),
         "Jun 16");
@@ -59,7 +62,7 @@ void main() {
         "16 juin");
 
     // -75d = April 7, 2023 = "Apr 7"
-    final apr7 = DateTime.fromMillisecondsSinceEpoch(1680890392000);
+    final apr7 = dateTimeFromUnix(1680890392000);
     expect(date_format.formatDateCompact(now: now, then: apr7, locale: "en_US"),
         "Apr 7");
     expect(date_format.formatDateCompact(now: now, then: apr7, locale: "nb"),
@@ -68,7 +71,7 @@ void main() {
         "7 avr.");
 
     // -180d = December 23, 2022 = "Dec 23"
-    final dec23 = DateTime.fromMillisecondsSinceEpoch(1671818392000);
+    final dec23 = dateTimeFromUnix(1671818392000);
     expect(
         date_format.formatDateCompact(now: now, then: dec23, locale: "en_US"),
         "Dec 23");
@@ -78,7 +81,7 @@ void main() {
         "23 déc.");
 
     // -200d = December 3 2022 = "12/03/22"
-    final dec3_22 = DateTime.fromMillisecondsSinceEpoch(1670090392000);
+    final dec3_22 = dateTimeFromUnix(1670090392000);
     expect(
         date_format.formatDateCompact(now: now, then: dec3_22, locale: "en_US"),
         "12/3/2022");
@@ -88,7 +91,7 @@ void main() {
         "03/12/2022");
 
     // -654d = September 5, 2021 = "09/05/21"
-    final sep5_22 = DateTime.fromMillisecondsSinceEpoch(1630864792000);
+    final sep5_22 = dateTimeFromUnix(1630864792000);
     expect(
         date_format.formatDateCompact(now: now, then: sep5_22, locale: "en_US"),
         "9/5/2021");
