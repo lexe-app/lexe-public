a76c6adb513a48096d75cb0786002f54594a7e06
Philip Hayes <philiphayes9@gmail.com>
2024-03-20 18:00:54 -0700

    app: reorganizing dummy payments

--- app/lib/main_design.dart
+++ app/lib/main_design.dart
@@ -4,6 +4,7 @@
 import 'dart:async';
 import 'dart:typed_data' show Uint8List;
 
+import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart' show Intl;
 import 'package:rxdart_ext/rxdart_ext.dart';
@@ -259,80 +260,10 @@ class _LexeDesignPageState extends State<LexeDesignPage> {
           ),
           Component(
             "PaymentDetailPage",
-            subtitle: "btc pending outbound",
+            subtitle: "btc failed outbound",
             (context) => PaymentDetailPageInner(
               app: mockApp,
-              payment: Payment(
-                index:
-                    "0000001687309696000-bc_238eb9f1b1db5e39877da642126783e2d6a043e047bbbe8872df3e7fdc3dca68",
-                kind: PaymentKind.Onchain,
-                direction: PaymentDirection.Outbound,
-                invoice: null,
-                amountSat: 77000,
-                feesSat: 3349,
-                status: PaymentStatus.Pending,
-                statusStr: "partially confirmed (1-5 confirmations)",
-                note: "Brunch w/ friends",
-                createdAt: DateTime.now()
-                    .subtract(const Duration(minutes: 3))
-                    .millisecondsSinceEpoch,
-              ),
-              paymentDateUpdates: this.paymentDateUpdates,
-              fiatRate: this.makeFiatRateStream(),
-              isRefreshing: ValueNotifier(false),
-              triggerRefresh: () {},
-            ),
-          ),
-          Component(
-            "PaymentDetailPage",
-            subtitle: "btc completed outbound",
-            (context) => PaymentDetailPageInner(
-              app: mockApp,
-              payment: Payment(
-                index:
-                    "0000001687309696000-bc_238eb9f1b1db5e39877da642126783e2d6a043e047bbbe8872df3e7fdc3dca68",
-                kind: PaymentKind.Onchain,
-                direction: PaymentDirection.Outbound,
-                invoice: null,
-                amountSat: 77000,
-                feesSat: 3349,
-                status: PaymentStatus.Completed,
-                statusStr: "fully confirmed (6+ confirmations)",
-                note: "Brunch w/ friends",
-                createdAt: DateTime.now()
-                    .subtract(const Duration(minutes: 75))
-                    .millisecondsSinceEpoch,
-                finalizedAt: DateTime.now()
-                    .subtract(const Duration(
-                        minutes: 15, seconds: 43, milliseconds: 111))
-                    .millisecondsSinceEpoch,
-              ),
-              paymentDateUpdates: this.paymentDateUpdates,
-              fiatRate: this.makeFiatRateStream(),
-              isRefreshing: ValueNotifier(false),
-              triggerRefresh: () {},
-            ),
-          ),
-          Component(
-            "PaymentDetailPage",
-            subtitle: "btc pending inbound",
-            (context) => PaymentDetailPageInner(
-              app: mockApp,
-              payment: Payment(
-                index:
-                    "0000001687309696000-bc_238eb9f1b1db5e39877da642126783e2d6a043e047bbbe8872df3e7fdc3dca68",
-                kind: PaymentKind.Onchain,
-                direction: PaymentDirection.Inbound,
-                invoice: null,
-                amountSat: 1469,
-                feesSat: 0,
-                status: PaymentStatus.Pending,
-                statusStr: "partially confirmed (1-5 confirmations)",
-                note: "Brunch w/ friends",
-                createdAt: DateTime.now()
-                    .subtract(const Duration(minutes: 15))
-                    .millisecondsSinceEpoch,
-              ),
+              payment: dummyOnchainOutboundFailed01,
               paymentDateUpdates: this.paymentDateUpdates,
               fiatRate: this.makeFiatRateStream(),
               isRefreshing: ValueNotifier(false),
@@ -344,50 +275,7 @@ class _LexeDesignPageState extends State<LexeDesignPage> {
             subtitle: "btc completed inbound",
             (context) => PaymentDetailPageInner(
               app: mockApp,
-              payment: Payment(
-                index:
-                    "0000001687309696000-bc_238eb9f1b1db5e39877da642126783e2d6a043e047bbbe8872df3e7fdc3dca68",
-                kind: PaymentKind.Onchain,
-                direction: PaymentDirection.Inbound,
-                invoice: null,
-                amountSat: 1469,
-                feesSat: 0,
-                status: PaymentStatus.Completed,
-                statusStr: "fully confirmed (6+ confirmations)",
-                note: "Brunch w/ friends",
-                createdAt: DateTime.now()
-                    .subtract(const Duration(days: 2, hours: 3))
-                    .millisecondsSinceEpoch,
-                finalizedAt: DateTime.now()
-                    .subtract(const Duration(days: 1, hours: 17, seconds: 55))
-                    .millisecondsSinceEpoch,
-              ),
-              paymentDateUpdates: this.paymentDateUpdates,
-              fiatRate: this.makeFiatRateStream(),
-              isRefreshing: ValueNotifier(false),
-              triggerRefresh: () {},
-            ),
-          ),
-          Component(
-            "PaymentDetailPage",
-            subtitle: "btc failed inbound",
-            (context) => PaymentDetailPageInner(
-              app: mockApp,
-              payment: Payment(
-                index:
-                    "0000001687309696000-bc_238eb9f1b1db5e39877da642126783e2d6a043e047bbbe8872df3e7fdc3dca68",
-                kind: PaymentKind.Onchain,
-                direction: PaymentDirection.Inbound,
-                invoice: null,
-                amountSat: 1469,
-                feesSat: 0,
-                status: PaymentStatus.Failed,
-                statusStr: "dropped from mempool",
-                note: "Brunch w/ friends",
-                createdAt: DateTime.now()
-                    .subtract(const Duration(days: 5))
-                    .millisecondsSinceEpoch,
-              ),
+              payment: dummyOnchainInboundCompleted01,
               paymentDateUpdates: this.paymentDateUpdates,
               fiatRate: this.makeFiatRateStream(),
               isRefreshing: ValueNotifier(false),
@@ -399,30 +287,7 @@ class _LexeDesignPageState extends State<LexeDesignPage> {
             subtitle: "ln invoice pending inbound",
             (context) => PaymentDetailPageInner(
               app: mockApp,
-              payment: Payment(
-                index:
-                    "0000001687309696000-bc_238eb9f1b1db5e39877da642126783e2d6a043e047bbbe8872df3e7fdc3dca68",
-                kind: PaymentKind.Invoice,
-                direction: PaymentDirection.Inbound,
-                invoice: const Invoice(
-                  string:
-                      "lnbcrt803490n1pjg7g5qxq8pjg7gh0pp5cmj7gmzeyec3f7gavn0sup5mpkhpwmu6zdr9dqsthg0xzep33xqqsp5dfhkjumxv3hkj6npwdhkgenfdfshxmmfv3nx5mmfwdskg6nxda5s9qrsgqdq42d6kuerp0ysxyun4de3kscqpcnp4qwla7nx7p5e5nau5k2hh2gxf736rhw0naslthr3jmyu5jqk8gjx7v7tm22urnycvxd86jt9yze25gj9akn38ye980rhc6cgttteuwur0jpmjgs60dwnfhfq7ccrudq634petu6v0dq29wv7et4dv5eh7g7jsq34v77q",
-                  createdAt: 1687102080710,
-                  expiresAt: 1687102191305,
-                  amountSats: 80349,
-                  description: "Sunday brunch",
-                  payeePubkey:
-                      "e68d44c7024939d9328ebb3eecf3b93b74f4c92075afb294f749330dde4cdfbfe5a75ff4cbb752a40e1c4947255d2a9c0ae88c826b5f47d6d660ce9b7c6ebca1",
-                ),
-                amountSat: 77000,
-                feesSat: 3349,
-                status: PaymentStatus.Pending,
-                statusStr: "invoice generated",
-                note: "Brunch w/ friends",
-                createdAt: DateTime.now()
-                    .subtract(const Duration(minutes: 3))
-                    .millisecondsSinceEpoch,
-              ),
+              payment: dummyInvoiceInboundPending01,
               paymentDateUpdates: this.paymentDateUpdates,
               fiatRate: this.makeFiatRateStream(),
               isRefreshing: ValueNotifier(false),
@@ -451,91 +316,13 @@ class MockAppHandle extends AppHandle {
 
   // Some sample data
   List<Payment> payments = [
-    const Payment(
-      index:
-          "0000001687090000000-bc_551df4ef3b67b3f2ca53f3e668eb73c2a9b3a77dea84b340fd2407ec5542aa66",
-      kind: PaymentKind.Onchain,
-      direction: PaymentDirection.Inbound,
-      amountSat: 20000,
-      feesSat: 2233,
-      status: PaymentStatus.Completed,
-      statusStr: "fully confirmed (6+ confirmations)",
-      createdAt: 1670090392000,
-    ),
-    const Payment(
-      index:
-          "0000001687120000000-bc_46e52089b60b00de067c84ce58d34a75ffd71a106f720855bc099f20da11700c",
-      kind: PaymentKind.Onchain,
-      direction: PaymentDirection.Outbound,
-      amountSat: 95000000,
-      feesSat: 433,
-      status: PaymentStatus.Failed,
-      statusStr: "dropped from mempool",
-      note: "Sweep from Muun",
-      createdAt: 1671818392000,
-    ),
-    const Payment(
-      index:
-          "0000001687130000000-ln_6973b3c58738403ceb3fccec470365a44361f34f4c2664ccae04f0f39fe71dc0",
-      kind: PaymentKind.Spontaneous,
-      direction: PaymentDirection.Outbound,
-      amountSat: 123000,
-      feesSat: 615,
-      status: PaymentStatus.Pending,
-      statusStr: "pending",
-      note: "🍑🍑🍑🍆🍆🍆😂😂😂",
-      createdAt: 1686938392000,
-    ),
-    const Payment(
-      index:
-          "0000001687150000000-ln_6f9dad93ceb2e78181ef5cb73601a28930e9774204d6fb335297b1f4add83d30",
-      kind: PaymentKind.Invoice,
-      direction: PaymentDirection.Inbound,
-      invoice: Invoice(
-        string:
-            "lnbcrt1pvjluezpp5qqqsyqcyq5rqwzqfqqqsyqcyq5rqwzqfqqqsyqcyq5rqwzqfqypqdq5vdhkven9v5sxyetpdeessp5zyg3zyg3zyg3zyg3zyg3zyg3zyg3zyg3zyg3zyg3zyg3zyg3zygs9q5sqqqqqqqqqqqqqqqqsgq2a25dxl5hrntdtn6zvydt7d66hyzsyhqs4wdynavys42xgl6sgx9c4g7me86a27t07mdtfry458rtjr0v92cnmswpsjscgt2vcse3sgpz3uapa",
-        description: "some note the invoice creator set",
-        createdAt: 1687140000000,
-        expiresAt: 1687160000000,
-      ),
-      amountSat: 4470000,
-      feesSat: 22350,
-      status: PaymentStatus.Pending,
-      statusStr: "claiming",
-      note:
-          "My super long note that really is too long it just keeps going and going",
-      createdAt: 1687150000000,
-    ),
-    const Payment(
-      index:
-          "0000001687200000000-ln_6fc9375017dd3d911fe4ee52f4becd2f376384f42053381a09c99cca61dbf87a",
-      kind: PaymentKind.Invoice,
-      direction: PaymentDirection.Inbound,
-      invoice: Invoice(
-        string:
-            "lnbcrt1metc0ejddlfthml0fz7z0e3zjk7wu2ltewa7lmms49a7lm6gh0h77l89yus9par2qqcw60rr4058g6slcty49q4uv5420qhu9tk7d080dys8e2lwy5u2q2uvstpynnc76u7x9f0y03sx06zlqfwcyjtuuu42j4eu9z478jtuu24wawlwalycdlp847j9087ftu90cfl9d57wnt3vqtu2q2uthj3j6tcz6y7z0etdqm7z5erx32wme699587wj2ndwghteta8d4gasv9tw34q49cmqf7xttrx8jh66m4saw9mcelt5epyjj5eqfezaz4uykh65hlmamhusq5ffuc25lryvmks7spuyljk60rpah4ek0r95nhng6s4rlyu4zucrfyu3r577z5598yg32whcfl9d5wd2l92535hcmrpardvhz05dg23fv3wnj7xqtpqpn72httdspp5dcs72ef655jh50zvzl9q08m8c9vx2pyqpn50kaf459fktsslvwtqsp5zmlelhnpqnxf5vfs8g3dpjjpwnuw5p50z3xzulya5wl8lp5xn7uq9qrsgqcqdtnm4um9mexh07x098dsnfrj0g27dux806kdtn5sumqu2v8dwm6xmc6u4khmmfv29n9m3mnhp7ta59gv49z6tflmmv89wmpa3jehplhqefudz2dc9huagppr2yu5",
-        createdAt: 1687100000000,
-        expiresAt: 1687300000000,
-      ),
-      amountSat: 222000,
-      feesSat: 1466,
-      status: PaymentStatus.Completed,
-      statusStr: "completed",
-      createdAt: 1687200000000,
-    ),
-    const Payment(
-      index:
-          "0000001687309696000-bc_238eb9f1b1db5e39877da642126783e2d6a043e047bbbe8872df3e7fdc3dca68",
-      kind: PaymentKind.Onchain,
-      direction: PaymentDirection.Outbound,
-      amountSat: 77000,
-      feesSat: 2881,
-      status: PaymentStatus.Completed,
-      statusStr: "fully confirmed (6+ confirmations)",
-      note: "Brunch w/ friends",
-      createdAt: 1687385080000,
-    ),
-  ];
+    dummyOnchainInboundCompleted01,
+    dummyOnchainOutboundFailed01,
+    dummySpontaneousOutboundPending01,
+    dummyInvoiceInboundPending01,
+    dummyInvoiceInboundCompleted01,
+    dummyOnchainOutboundCompleted01,
+  ].sortedBy((payment) => payment.index);
 
   @override
   Future<NodeInfo> nodeInfo({dynamic hint}) => Future.delayed(
@@ -727,3 +514,125 @@ extension PaymentExt on Payment {
         createdAt: this.createdAt,
       );
 }
+
+// Dummy payments data
+
+const Payment dummyOnchainInboundPending01 = Payment(
+  index:
+      "0000001687309696000-bc_238eb9f1b1db5e39877da642126783e2d6a043e047bbbe8872df3e7fdc3dca68",
+  kind: PaymentKind.Onchain,
+  direction: PaymentDirection.Inbound,
+  amountSat: 1469,
+  feesSat: 0,
+  status: PaymentStatus.Pending,
+  statusStr: "partially confirmed (1-5 confirmations)",
+  note: null,
+  createdAt: 1687309696000,
+  finalizedAt: null,
+  replacement: null,
+);
+
+const Payment dummyOnchainInboundCompleted01 = Payment(
+  index:
+      "0000001670090492000-bc_551df4ef3b67b3f2ca53f3e668eb73c2a9b3a77dea84b340fd2407ec5542aa66",
+  kind: PaymentKind.Onchain,
+  direction: PaymentDirection.Inbound,
+  amountSat: 20000,
+  feesSat: 0,
+  status: PaymentStatus.Completed,
+  statusStr: "fully confirmed (6+ confirmations)",
+  note: "Brunch w/ friends",
+  createdAt: 1670090492000,
+  finalizedAt: 1670090502000,
+  replacement: null,
+);
+
+const Payment dummyOnchainOutboundCompleted01 = Payment(
+  index:
+      "0000001687385080000-bc_238eb9f1b1db5e39877da642126783e2d6a043e047bbbe8872df3e7fdc3dca68",
+  kind: PaymentKind.Onchain,
+  direction: PaymentDirection.Outbound,
+  amountSat: 77000,
+  feesSat: 2881,
+  status: PaymentStatus.Completed,
+  statusStr: "fully confirmed (6+ confirmations)",
+  note: "Funding exchange",
+  createdAt: 1687385080000,
+  finalizedAt: 1687385380000,
+);
+
+const Payment dummyOnchainOutboundFailed01 = Payment(
+  index:
+      "0000001671818392000-bc_46e52089b60b00de067c84ce58d34a75ffd71a106f720855bc099f20da11700c",
+  kind: PaymentKind.Onchain,
+  direction: PaymentDirection.Outbound,
+  amountSat: 95000000,
+  feesSat: 5433,
+  status: PaymentStatus.Failed,
+  statusStr: "dropped from mempool",
+  note: "Sweep from Muun",
+  createdAt: 1671818392000,
+  finalizedAt: 1671918392000,
+  replacement: null,
+);
+
+const Payment dummySpontaneousOutboundPending01 = Payment(
+  index:
+      "0000001686938392000-ln_6973b3c58738403ceb3fccec470365a44361f34f4c2664ccae04f0f39fe71dc0",
+  kind: PaymentKind.Spontaneous,
+  direction: PaymentDirection.Outbound,
+  amountSat: 123000,
+  feesSat: 615,
+  status: PaymentStatus.Pending,
+  statusStr: "pending",
+  note: "🍑🍑🍑🍆🍆🍆😂😂😂",
+  createdAt: 1686938392000,
+);
+
+const Payment dummyInvoiceInboundPending01 = Payment(
+  index:
+      "0000001687140003000-ln_bbe27583bf7ee269387bbad48c48fcae10e41537d35e49b14d81cc7306f486cb",
+  kind: PaymentKind.Invoice,
+  direction: PaymentDirection.Inbound,
+  invoice: Invoice(
+    string:
+        "lnbcrt4693500n1pjgld4pxq8pjglhd3pp5h038tqal0m3xjwrmht2gcj8u4cgwg9fh6d0ynv2ds8x8xph5sm9ssp5d4jx76ttd4ek76tnv3hkv6tpdfekgenvdfkx76t2wdskg6nxda5s9qrsgqdp4wdhk6efqdehhgefqw35x2grfdemx76trv5sxxun9v96x7u3qwdjhgcqpcnp4qgywe59xssrqj004k24477svqtgynw4am39hz06hk4dlu4l0ssk8w2rpkgvpsusjrwde5qym0t9g42px0dahyh7jz9lvn5umk9gzqxtc8r0rdplu9psdewwqnw6t7uvdqtvn6heqfgxvn9a76kkl760cy4rqpewlfe6",
+    description: "some note the invoice creator set",
+    createdAt: 1687140001000,
+    expiresAt: 1687150001000,
+    amountSats: 469350,
+    payeePubkey:
+        "772c84ef57fe5bb5573f714bdcbdba49d0020c7a5fabb2f53d090684a6d0ec082ee2f633d8398b2dd0bade4b2fd2fc78ec881b1296e4834b48c0e73c9edbc774",
+  ),
+  amountSat: 469350,
+  feesSat: 2350,
+  status: PaymentStatus.Pending,
+  statusStr: "claiming",
+  note:
+      "My super long note that really is too long it just keeps going and going",
+  createdAt: 1687140003000,
+);
+
+const Payment dummyInvoiceInboundCompleted01 = Payment(
+  index:
+      "0000001687100002000-ln_801ffce9fbe74fecc7ec6fa72716d7de6167cc5607635062b24797b54f9ba4be",
+  kind: PaymentKind.Invoice,
+  direction: PaymentDirection.Inbound,
+  invoice: Invoice(
+    string:
+        "lnbcrt2234660n1pjg7xnqxq8pjg7stspp5sq0le60mua87e3lvd7njw9khmesk0nzkqa34qc4jg7tm2num5jlqsp58p4rswtywdnx5wtn8pjxv6nnvsukv6mdve4xzernd9nx5mmpv35s9qrsgqdqhg35hyetrwssxgetsdaekjaqcqpcnp4q0tmlmj0gdeksm6el92s4v3gtw2nt3fjpp7czafjpfd9tgmv052jshcgr3e64wp4uum2c336uprxrhl34ryvgnl56y2usgmvpkt0xajyn4qfvguh7fgm6d07n00hxcrktmkz9qnprr3gxlzy2f4q9r68scwsp5d6f6r",
+    createdAt: 1687100000000,
+    expiresAt: 1687110000000,
+    amountSats: 223466,
+    description: "Direct deposit",
+    payeePubkey:
+        "28157d6ca3555a0a3275817d0832c535955b28b20a55f9596f6873434feebfd797d4b245397fab8f8f94dcdd32aac475d64893aa042f18b8d725e116082ae909",
+  ),
+  amountSat: 223466,
+  feesSat: 0,
+  status: PaymentStatus.Completed,
+  statusStr: "completed",
+  note: null,
+  createdAt: 1687100002000,
+  finalizedAt: 1687100005000,
+);
