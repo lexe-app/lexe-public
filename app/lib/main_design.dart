dc16ec0e61c6969c4e2c99038a2f30797f604fa8
Philip Hayes <philiphayes9@gmail.com>
2024-03-29 19:13:33 -0700

    app: receive: sketching out page variations

--- app/lib/main_design.dart
+++ app/lib/main_design.dart
@@ -45,7 +45,13 @@ import 'package:lexeapp/logger.dart';
 import 'package:lexeapp/result.dart';
 import 'package:lexeapp/route/landing.dart' show LandingPage;
 import 'package:lexeapp/route/payment_detail.dart' show PaymentDetailPageInner;
-import 'package:lexeapp/route/receive.dart' show ReceivePaymentPage;
+import 'package:lexeapp/route/receive.dart'
+    show
+        ReceivePaymentPage,
+        ReceivePaymentPage2,
+        ReceivePaymentPage3,
+        ReceivePaymentPage4,
+        ReceivePaymentPage5;
 import 'package:lexeapp/route/scan.dart' show ScanPage;
 import 'package:lexeapp/route/send.dart'
     show
@@ -259,6 +265,14 @@ class _LexeDesignPageState extends State<LexeDesignPage> {
           ),
           Component(
               "ReceivePaymentPage", (context) => const ReceivePaymentPage()),
+          Component(
+              "ReceivePaymentPage2", (context) => const ReceivePaymentPage2()),
+          Component(
+              "ReceivePaymentPage3", (context) => const ReceivePaymentPage3()),
+          Component(
+              "ReceivePaymentPage4", (context) => const ReceivePaymentPage4()),
+          Component(
+              "ReceivePaymentPage5", (context) => const ReceivePaymentPage5()),
           Component(
             "PaymentDetailPage",
             subtitle: "btc failed outbound",
