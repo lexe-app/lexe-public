6f7ed211290145cfc1047098b7e66e63ae2b14a3
Max Fang <protonmail.com@maxfa.ng>
2024-05-08 17:43:14 -0700

    refactor: Clean up `RestClient` constructors
    - `RestClient::new` -> `RestClient::new_insecure`
    - `RestClient::new_tls` -> `RestClient::new`
    - Replace all unnecessary usages of `RestClient::client_builder` with
      `RestClient::new`

--- common/src/client.rs
+++ common/src/client.rs
@@ -94,14 +94,7 @@ impl GatewayClient {
         gateway_url: String,
     ) -> anyhow::Result<Self> {
         let tls_config = lexe_ca::app_gateway_client_config(deploy_env);
-
-        let (from, to) = ("app", "gateway");
-        let reqwest_client = RestClient::client_builder(from)
-            .use_preconfigured_tls(tls_config)
-            .build()
-            .context("Failed to build client")?;
-        let rest = RestClient::from_inner(reqwest_client, from, to);
-
+        let rest = RestClient::new("app", "gateway", tls_config);
         Ok(Self { rest, gateway_url })
     }
 }
@@ -238,14 +231,13 @@ impl NodeClient {
                 rng, deploy_env, root_seed,
             )?;
 
-            let from = "app";
+            let (from, to) = ("app", "node-run");
             let reqwest_client = RestClient::client_builder(from)
                 .proxy(proxy)
                 .use_preconfigured_tls(tls_config)
                 .build()
                 .context("Failed to build client")?;
 
-            let to = "node-run";
             RestClient::from_inner(reqwest_client, from, to)
         };
 
@@ -369,14 +361,13 @@ impl NodeClient {
             measurement,
         );
 
-        let from = "app";
+        let (from, to) = ("app", "node-provision");
         let reqwest_client = RestClient::client_builder(from)
             .proxy(proxy)
             .use_preconfigured_tls(tls_config)
             .build()
             .context("Failed to build client")?;
 
-        let to = "node-provision";
         let provision_rest = RestClient::from_inner(reqwest_client, from, to);
 
         Ok(provision_rest)
