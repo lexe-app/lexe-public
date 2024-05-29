f9c7af03022d3858599d81c6f9c5ab7a1d8f34bc
Philip Hayes <philiphayes9@gmail.com>
2024-05-28 17:11:54 -0700

    app: ffi: get linux app build sort of working

--- app/lib/bindings.dart
+++ app/lib/bindings.dart
@@ -12,14 +12,10 @@ import 'package:lexeapp/bindings_generated.dart' show AppRsImpl;
 import 'package:lexeapp/cfg.dart' as cfg;
 
 /// Android only supports ffi via dynamically linked libraries.
-/// We'll statically link our ffi library for all other platforms.
+/// I couldn't figure out how to statically link against our lib on Linux.
+/// Prefer to statically link our ffi library for all platforms.
 ffi.DynamicLibrary _loadLibraryNormal() {
-  // TODO(phlip9): Linux build
-  if (io.Platform.isLinux) {
-    return ffi.DynamicLibrary.open("../target/debug/libapp_rs.so");
-  }
-
-  final lib = (io.Platform.isAndroid)
+  final lib = (io.Platform.isAndroid || io.Platform.isLinux)
       ? ffi.DynamicLibrary.open("libapp_rs.so")
       : ffi.DynamicLibrary.process();
   return lib;
@@ -28,11 +24,8 @@ ffi.DynamicLibrary _loadLibraryNormal() {
 /// Unit tests are run on the host and `flutter test` (with unit test only)
 /// just straight up runs `dart run` on the test file without giving us any
 /// build hooks or letting us link our library, so we have to load the dynamic
-/// library out of the cargo target dir.
-//
-// TODO(phlip9): how can we ensure the native library is always up-to-date?
-// since this is run on the host, maybe we literally just shell out to a quick
-// `cargo build -p app-rs`??
+/// library out of the cargo target dir. Assumes we've just run
+/// `cargo build -p app-rs` just before.
 ffi.DynamicLibrary _loadLibraryUnitTest() {
   if (io.Platform.isMacOS) {
     return ffi.DynamicLibrary.open("../target/debug/libapp_rs.dylib");
