f7af34e44c37789579393d8ef9fb18cc018446f7
Philip Hayes <philiphayes9@gmail.com>
2024-06-20 12:46:41 -0700

    app: qr: remove `gal`. just copy on long-press.
    Couldn't get the save QR image thing working well enough, so I'm just
    going to cut scope and copy the QR code string on long press.

--- app/windows/flutter/generated_plugin_registrant.cc
+++ app/windows/flutter/generated_plugin_registrant.cc
@@ -7,11 +7,8 @@
 #include "generated_plugin_registrant.h"
 
 #include <file_selector_windows/file_selector_windows.h>
-#include <gal/gal_plugin_c_api.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   FileSelectorWindowsRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("FileSelectorWindows"));
-  GalPluginCApiRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("GalPluginCApi"));
 }
