279a2863e077f8ea08c954cffeda583dbe330fac
Philip Kannegaard Hayes <philiphayes9@gmail.com>
2024-10-01 18:39:01 -0700

    app: channels: add channels list

--- app/lib/route/channels.dart
+++ app/lib/route/channels.dart
@@ -1,23 +1,31 @@
+import 'dart:math' show max, min;
+
 import 'package:app_rs_dart/ffi/api.dart' show FiatRate;
 import 'package:app_rs_dart/ffi/app.dart' show AppHandle;
 import 'package:app_rs_dart/ffi/types.dart' show LxChannelDetails;
 import 'package:flutter/material.dart';
 import 'package:lexeapp/components.dart'
     show
+        ChannelBalanceBar,
         FilledCircle,
         FilledTextPlaceholder,
         ListIcon,
         LxBackButton,
         LxRefreshButton,
         ScrollableSinglePageBody,
-        SplitAmountText;
+        SplitAmountText,
+        ValueStreamBuilder;
 import 'package:lexeapp/currency_format.dart' as currency_format;
-import 'package:lexeapp/style.dart' show Fonts, LxColors, Space;
+import 'package:lexeapp/style.dart' show Fonts, LxColors, LxRadius, Space;
+import 'package:rxdart/rxdart.dart';
 
 class ChannelsPage extends StatefulWidget {
-  const ChannelsPage({super.key, required this.app});
+  const ChannelsPage({super.key, required this.app, required this.fiatRate});
 
   final AppHandle app;
+
+  /// Updating stream of fiat rates.
+  final ValueStream<FiatRate?> fiatRate;
 
   @override
   State<ChannelsPage> createState() => _ChannelsPageState();
@@ -35,8 +43,37 @@ class _ChannelsPageState extends State<ChannelsPage> {
       fiatRate: FiatRate(fiat: "USD", rate: 63344.0),
     ),
   );
-
-  final ValueNotifier<List<LxChannelDetails>?> channels = ValueNotifier(null);
+  // final ValueNotifier<ChannelsList?> channels = ValueNotifier(null);
+  final ValueNotifier<ChannelsList?> channels =
+      ValueNotifier(const ChannelsList(
+    maxValueSats: 776231 + 226787,
+    channels: [
+      Channel(
+        channelId:
+            "261111111111111116f7e7e2d110b0c67bc1f01b9bb9a89bbe98c144f0f4b04c",
+        isUsable: false,
+        channelValueSats: 776231 + 226787,
+        ourBalanceSats: 776231,
+        theirBalanceSats: 226787,
+      ),
+      Channel(
+        channelId:
+            "262222222222222226f7e7e2d110b0c67bc1f01b9bb9a89bbe98c144f0f4b04c",
+        isUsable: true,
+        channelValueSats: 300231 + 477788,
+        ourBalanceSats: 300231,
+        theirBalanceSats: 477788,
+      ),
+      Channel(
+        channelId:
+            "263333333333333336f7e7e2d110b0c67bc1f01b9bb9a89bbe98c144f0f4b04c",
+        isUsable: false,
+        channelValueSats: 254116 + 43844,
+        ourBalanceSats: 254116,
+        theirBalanceSats: 43844,
+      ),
+    ],
+  ));
 
   @override
   void dispose() {
@@ -66,36 +103,101 @@ class _ChannelsPageState extends State<ChannelsPage> {
         ],
       ),
       body: ScrollableSinglePageBody(
-        body: [
-          // Heading
-          const Padding(
-            padding: EdgeInsets.only(top: Space.s300, bottom: Space.s100),
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
+        bodySlivers: [
+          SliverToBoxAdapter(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
               children: [
-                ListIcon.lightning(),
-                SizedBox(width: Space.s200),
-                Text("Lightning channels", style: Fonts.fontHeadlineSmall),
+                // Heading
+                const Padding(
+                  padding: EdgeInsets.only(top: Space.s300, bottom: Space.s100),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      ListIcon.lightning(),
+                      SizedBox(width: Space.s200),
+                      Text("Lightning channels",
+                          style: Fonts.fontHeadlineSmall),
+                    ],
+                  ),
+                ),
+                const Text(
+                  "Open channels to send payments instantly over the Lightning network",
+                  style: Fonts.fontSubheading,
+                ),
+                const SizedBox(height: Space.s600),
+
+                // Send up to/Receive up to
+                ValueListenableBuilder(
+                  valueListenable: this.totalChannelBalance,
+                  builder: (context, totalChannelBalance, child) =>
+                      TotalChannelBalanceWidget(
+                          totalChannelBalance: totalChannelBalance),
+                ),
+                const SizedBox(height: Space.s800),
+
+                // You/Lexe LSP channels heading
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    ChannelsPartyChip(name: "You"),
+                    ChannelsPartyChip(name: "Lexe LSP"),
+                  ],
+                ),
+                const SizedBox(height: Space.s200),
               ],
             ),
           ),
-          const Text(
-            "Open channels to send payments instantly over the Lightning network",
-            style: Fonts.fontSubheading,
-          ),
-          const SizedBox(height: Space.s600),
 
-          // Send up to/Receive up to
+          // Channels list
           ValueListenableBuilder(
-            valueListenable: this.totalChannelBalance,
-            builder: (context, totalChannelBalance, child) =>
-                TotalChannelBalanceWidget(
-                    totalChannelBalance: totalChannelBalance),
+            valueListenable: this.channels,
+            builder: (context, channelsList, child) =>
+                SliverFixedExtentList.list(
+              itemExtent: Space.s850,
+              children: (channelsList != null)
+                  ? channelsList.channels
+                      .map((channel) => ChannelsListEntry(
+                            maxValueSats: channelsList.maxValueSats,
+                            channel: channel,
+                            fiatRate: this.widget.fiatRate,
+                          ))
+                      .toList()
+                  : [],
+            ),
           ),
         ],
       ),
     );
   }
+}
+
+// The You/Lexe LSP header chip things
+class ChannelsPartyChip extends StatelessWidget {
+  const ChannelsPartyChip({super.key, required this.name});
+
+  final String name;
+
+  @override
+  Widget build(BuildContext context) => DecoratedBox(
+        decoration: const BoxDecoration(
+          color: LxColors.grey850,
+          borderRadius: BorderRadius.all(Radius.circular(LxRadius.r200)),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            vertical: Space.s200,
+            horizontal: Space.s300,
+          ),
+          child: Text(
+            this.name,
+            style: Fonts.fontUI.copyWith(
+              fontSize: Fonts.size200,
+              color: LxColors.fgSecondary,
+            ),
+          ),
+        ),
+      );
 }
 
 class FiatAmount {
@@ -219,7 +321,7 @@ class TotalChannelBalanceRow extends StatelessWidget {
     final primaryStyle = Fonts.fontUI.copyWith(
       fontSize: Fonts.size400,
       fontVariations: [Fonts.weightMedium],
-      fontFeatures: [Fonts.featTabularNumbers],
+      // fontFeatures: [Fonts.featTabularNumbers],
       height: 1.25,
       letterSpacing: -0.5,
     );
@@ -239,7 +341,7 @@ class TotalChannelBalanceRow extends StatelessWidget {
       fontSize: Fonts.size300,
       color: LxColors.fgTertiary,
       fontVariations: [Fonts.weightMedium],
-      fontFeatures: [Fonts.featTabularNumbers],
+      // fontFeatures: [Fonts.featTabularNumbers],
       height: 1.25,
       letterSpacing: -0.5,
     );
@@ -292,6 +394,185 @@ class TotalChannelBalanceRow extends StatelessWidget {
             secondaryAmount,
           ],
         ),
+      ],
+    );
+  }
+}
+
+class ChannelsList {
+  const ChannelsList({required this.maxValueSats, required this.channels});
+
+  final int maxValueSats;
+  final List<Channel> channels;
+}
+
+class Channel {
+  const Channel({
+    required this.channelId,
+    required this.isUsable,
+    required this.channelValueSats,
+    required this.ourBalanceSats,
+    required this.theirBalanceSats,
+  });
+
+  Channel.fromApi(LxChannelDetails c)
+      : channelId = c.channelId,
+        isUsable = c.isUsable,
+        channelValueSats = c.channelValueSats,
+        ourBalanceSats = c.ourBalanceSats,
+        theirBalanceSats = c.theirBalanceSats;
+
+  final String channelId;
+  final bool isUsable;
+  final int channelValueSats;
+  final int ourBalanceSats;
+  final int theirBalanceSats;
+}
+
+class ChannelsListEntry extends StatelessWidget {
+  const ChannelsListEntry({
+    super.key,
+    required this.maxValueSats,
+    required this.channel,
+    required this.fiatRate,
+  });
+
+  final int maxValueSats;
+  final Channel channel;
+
+  /// Updating stream of fiat rates.
+  final ValueStream<FiatRate?> fiatRate;
+
+  @override
+  Widget build(BuildContext context) {
+    final primaryStyle = Fonts.fontUI.copyWith(
+      color: LxColors.foreground,
+      fontSize: Fonts.size300,
+      fontVariations: [Fonts.weightMedium],
+      fontFeatures: [Fonts.featTabularNumbers],
+      height: 1.25,
+      letterSpacing: -0.5,
+    );
+
+    final secondaryStyle = primaryStyle.copyWith(color: LxColors.fgTertiary);
+
+    final ourBalanceFiat = ValueStreamBuilder(
+      stream: this.fiatRate,
+      builder: (context, fiatRate) => (fiatRate != null)
+          ? SplitAmountText(
+              amount: FiatAmount.fromSats(fiatRate, this.channel.ourBalanceSats)
+                  .amount,
+              fiatName: fiatRate.fiat,
+              style: primaryStyle,
+            )
+          : FilledTextPlaceholder(
+              width: Space.s1000,
+              style: primaryStyle,
+            ),
+    );
+
+    final ourBalanceSats = Text(
+      currency_format.formatSatsAmount(this.channel.ourBalanceSats,
+          satsSuffix: true),
+      style: secondaryStyle,
+    );
+
+    final theirBalanceFiat = ValueStreamBuilder(
+      stream: this.fiatRate,
+      builder: (context, fiatRate) => (fiatRate != null)
+          ? Text(
+              currency_format.formatFiat(
+                  FiatAmount.fromSats(fiatRate, this.channel.ourBalanceSats)
+                      .amount,
+                  fiatRate.fiat),
+              style: secondaryStyle,
+            )
+          : FilledTextPlaceholder(
+              width: Space.s900,
+              style: secondaryStyle,
+            ),
+    );
+
+    final theirBalanceSats = Text(
+      currency_format.formatSatsAmount(this.channel.theirBalanceSats,
+          satsSuffix: true),
+      style: secondaryStyle,
+    );
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10.0),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          ChannelBalanceBarRow(
+            value: this.channel.ourBalanceSats / this.channel.channelValueSats,
+            width: this.channel.channelValueSats / this.maxValueSats,
+            isUsable: this.channel.isUsable,
+          ),
+          const SizedBox(height: Space.s200),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.baseline,
+            textBaseline: TextBaseline.alphabetic,
+            children: [
+              ourBalanceFiat,
+              theirBalanceFiat,
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.baseline,
+            textBaseline: TextBaseline.alphabetic,
+            children: [
+              ourBalanceSats,
+              theirBalanceSats,
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ChannelBalanceBarRow extends StatelessWidget {
+  const ChannelBalanceBarRow({
+    super.key,
+    required this.value,
+    required this.width,
+    required this.isUsable,
+  });
+
+  final double value;
+  final double width;
+  final bool isUsable;
+
+  @override
+  Widget build(BuildContext context) {
+    final flex = max(5, min(100, (this.width * 100.0).truncate()));
+
+    final bar = (this.isUsable)
+        ? ChannelBalanceBar.usable(value: this.value)
+        : ChannelBalanceBar.pending(value: this.value);
+
+    return Row(
+      children: [
+        // Display small channels as proportionally smaller bars.
+        Expanded(flex: flex, child: bar),
+        // Show a spinner for opening/closing channels.
+        // TODO(phlip9): add subdued "pending"/"closing" text here?
+        if (!this.isUsable)
+          const Padding(
+            padding: EdgeInsets.only(left: Space.s300),
+            child: SizedBox.square(
+              dimension: 10.0,
+              child: CircularProgressIndicator(
+                strokeWidth: 2.0,
+                color: LxColors.grey775,
+                strokeCap: StrokeCap.round,
+              ),
+            ),
+          ),
+        Expanded(flex: 100 - flex, child: const SizedBox()),
       ],
     );
   }
