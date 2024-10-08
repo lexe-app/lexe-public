55285e0b6663313e0519f046ac935f0842e63cf1
Philip Kannegaard Hayes <philiphayes9@gmail.com>
2024-10-07 17:32:10 -0700

    app: lib: add `Stream.asyncMapUnbuffered`

--- app/lib/stream_ext.dart
+++ app/lib/stream_ext.dart
@@ -1,4 +1,4 @@
-import 'dart:async' show Stream, StreamController;
+import 'dart:async' show FutureOr, Stream, StreamController, StreamSubscription;
 
 import 'package:lexeapp/logger.dart';
 import 'package:rxdart_ext/rxdart_ext.dart';
@@ -14,6 +14,7 @@ extension StreamControllerExt<T> on StreamController<T> {
 }
 
 extension StreamExt<T> on Stream<T> {
+  /// Log for each event or error that occurs on this stream.
   Stream<T> log({
     required String id,
   }) =>
@@ -21,4 +22,81 @@ extension StreamExt<T> on Stream<T> {
         data: (data) => info("$id: $data"),
         error: (err, trace) => error("$id: error: $err, $trace"),
       );
+
+  /// Alias for [Stream.where].
+  Stream<T> filter(bool Function(T event) test) => this.where(test);
+
+  /// Alias for [Stream.mapNotNull].
+  Stream<R> filterMap<R extends Object>(R? Function(T) transform) =>
+      MapNotNullStreamTransformer<T, R>(transform).bind(this);
+
+  /// Creates a new stream with each data event of this stream asynchronously
+  /// mapped to a new event.
+  ///
+  /// This is like [Stream.asyncMap] but it throws away any events that occur
+  /// while the Future is running.
+  ///
+  /// The returned stream is a broadcast stream if this stream is.
+  Stream<E> asyncMapUnbuffered<E>(FutureOr<E> Function(T event) convert) {
+    StreamController<E> controller;
+    if (this.isBroadcast) {
+      controller = StreamController<E>.broadcast(sync: true);
+    } else {
+      controller = StreamController<E>(sync: true);
+    }
+
+    controller.onListen = () {
+      FutureOr<Null> add(E value) {
+        // Since the future might resolve after the stream has been closed, and
+        // since dart futures are not cancelable, we need to check if the stream
+        // is closed before adding the resolved value.
+        controller.addIfNotClosed(value);
+      }
+
+      // Set this flag whenever we're running the `convert` Future. We'll skip
+      // any new events while this flag is set.
+      bool isProcessing = false;
+
+      FutureOr<void> resume() {
+        isProcessing = false;
+      }
+
+      final StreamSubscription<T> subscription = this.listen(
+        onError: controller.addError,
+        onDone: controller.close,
+        (event) {
+          // Skip events while we're processing.
+          if (isProcessing) return;
+          isProcessing = true;
+
+          FutureOr<E> newValue;
+          try {
+            newValue = convert(event);
+          } catch (e, s) {
+            controller.addError(e, s);
+            isProcessing = false;
+            return;
+          }
+
+          if (newValue is Future<E>) {
+            newValue
+                .then(add, onError: controller.addError)
+                .whenComplete(resume);
+          } else {
+            controller.add(newValue);
+            isProcessing = false;
+          }
+        },
+      );
+
+      controller.onCancel = subscription.cancel;
+      if (!this.isBroadcast) {
+        controller
+          ..onPause = subscription.pause
+          ..onResume = subscription.resume;
+      }
+    };
+
+    return controller.stream;
+  }
 }
---
+++ app/test/stream_ext_test.dart
@@ -0,0 +1,35 @@
+import 'dart:async' show StreamController;
+
+import 'package:flutter_test/flutter_test.dart' show expect, test;
+import 'package:lexeapp/mock_time.dart' show MockTime;
+import 'package:lexeapp/stream_ext.dart' show StreamExt;
+
+Future<void> main() async {
+  test("asyncMapUnbuffered", () {
+    MockTime().run((time) async {
+      final StreamController<int> source =
+          StreamController.broadcast(sync: true);
+
+      final outFut = source.stream.asyncMapUnbuffered((foo) async {
+        await Future.delayed(const Duration(milliseconds: 500));
+        return foo * 2;
+      }).toList();
+
+      source.add(9); // -> 18
+      source.add(5); // <skip>
+      source.add(7); // <skip>
+
+      time.advance(const Duration(milliseconds: 1000));
+
+      source.add(3); // -> 6
+      source.add(1); // <skip>
+
+      time.advance(const Duration(milliseconds: 1000));
+
+      await source.close();
+      expect(await outFut, [18, 6]);
+
+      assert(time.isQuiescent());
+    });
+  });
+}
