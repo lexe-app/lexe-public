643e31149b3211a68c745b8d5cb085eff2c70e2a
Philip Kannegaard Hayes <philiphayes9@gmail.com>
2024-10-28 16:21:40 -0700

    nix: update flake.lock
    • Updated input 'crane':
        'github:ipetkov/crane/7e4586bad4e3f8f97a9271def747cf58c4b68f3c' (2024-09-04)
      → 'github:ipetkov/crane/498d9f122c413ee1154e8131ace5a35a80d8fa76' (2024-10-27)
    • Updated input 'fenix':
        'github:nix-community/fenix/a13cbbf5407a16a60fcf82cdceb301cf87a65b41' (2024-09-03)
      → 'github:nix-community/fenix/482b57f3f27a9336e0fbc62fa99ee0f624ccf4d0' (2024-10-28)
    • Updated input 'nixpkgs':
        'github:nixos/nixpkgs/12228ff1752d7b7624a54e9c1af4b222b3c1073b' (2024-08-31)
      → 'github:nixos/nixpkgs/18536bf04cd71abd345f9579158841376fdd0c5a' (2024-10-25)
    
    The SGX-cross env needed some slight tweaks (`CC_<target>` => `TARGET_CC_<target>`)
    since `clang` started picking up the `CC_<target>` env and got confused, as it's
    not aware of any Fortanix SGX targets. The `cc` crate still picks up the
    `TARGET_CC_<target>`, so this should work.
    
    The `fenix` nix flake dep also needs to be updated separately, along with the
    `rustc` toolchain version.

--- nix/pkgs/sgxCrossEnvBuildHook.nix
+++ nix/pkgs/sgxCrossEnvBuildHook.nix
@@ -13,6 +13,14 @@
   #
   sgx-libc-shim,
 }: let
+  # Use the base, "unwrapped" clang toolchain without all the nix
+  # cross-compiling magic.
+  #
+  # 1. Less magic moving parts makes it easier to see what's happening.
+  # 2. We don't yet have a proper nix system definition for Fortanix SGX (and
+  #    probably won't, because it doesn't have a proper libc).
+  # 3. Targeting Fortanix SGX is like targeting embedded (no-libc), which nix's
+  #    "wrapped" toolchains don't handle very well.
   lld = llvmPackages.lld;
   clang-unwrapped = llvmPackages.clang-unwrapped;
   clangVersion = lib.versions.major clang-unwrapped.version;
@@ -30,10 +38,12 @@ in
         # Use llvm toolchain for sgx since it's significantly better for
         # cross-compiling.
 
-        # CC and CFLAGS are for `cc` build.rs helper crate
-        export CC_x86_64_fortanix_unknown_sgx="${clang-unwrapped}/bin/clang"
-        export CFLAGS_x86_64_fortanix_unknown_sgx="${cflagsSgx}"
-        # CARGO_TARGET is for `cargo`
+        # `TARGET_CC` and `TARGET_CFLAGS` are for the `cc` build.rs helper crate.
+        # See: <https://docs.rs/cc/latest/cc/#external-configuration-via-environment-variables>
+        export TARGET_CC_x86_64_fortanix_unknown_sgx="${clang-unwrapped}/bin/clang"
+        export TARGET_CFLAGS_x86_64_fortanix_unknown_sgx="${cflagsSgx}"
+        # `CARGO_TARGET` is for `cargo`.
+        # See: <https://doc.rust-lang.org/cargo/reference/environment-variables.html#configuration-environment-variables>
         export CARGO_TARGET_X86_64_FORTANIX_UNKNOWN_SGX_LINKER="${lld}/bin/ld.lld"
         # `RUSTC_BOOTSTRAP=1` allows us to enable the one nightly feature we
         # need (sgx_platform) for a few SGX-patched crates, even with a stable
