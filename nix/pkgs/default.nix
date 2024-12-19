1dc0a566fc0afa5fb8d5bf96d023336ba1d41209
Philip Kannegaard Hayes <philiphayes9@gmail.com>
2024-12-18 19:19:30 -0800

    nix: add `mkMinShell` for devShells that need precise control
    Standard `pkgs.mkShell` and `pkgs.mkShellNoCC` propagate all of nixpkgs
    `stdenv.mkDerivation` to the devShell, which adds a TON of envs/fns/etc.
    
    Normally this is not an issue; however, our iOS/macOS tooling needs to use
    many system packages/bins managed by Apple tooling. The darwin stdenv does a
    lot of work to get (mostly) full control over the environment and AVOID using
    the system install. So it's hurting rather than helping when I have to patch
    $PATH in multiple places, unset/munge various envs, etc... to get things to
    use the Apple system tools.
    
    This `lexePubPkgs.mkMinShell` helps as it lets us build our devShell from
    scratch.

--- nix/pkgs/default.nix
+++ nix/pkgs/default.nix
@@ -328,6 +328,63 @@
     ${run-sgx}/bin/run-sgx ${sgx-test}/bin/sgx-test.sgxs --debug
   '';
 
+  # Minimal `pkgs.mkShellNoCC` for `nix develop` that only
+  #
+  # 1. passes through `env`
+  # 2. adds `packages` to the PATH.
+  #
+  # We use this for Apple dev toolchains, where nixpkgs's `stdenv` clobbers
+  # several things that we need to get from the system instead.
+  mkMinShell = {
+    name,
+    packages ? [],
+    env ? {},
+    shellHook ? "",
+  }: let
+    # Need to filter out attrNames used above so we don't accidentally clobber
+    # TODO(phlip9): make this an assert
+    envClean = builtins.removeAttrs env [
+      "args"
+      "builder"
+      "name"
+      "outputs"
+      "packages"
+      "shellHook"
+      "stdenv"
+      "system"
+    ];
+  in
+    builtins.derivation ({
+        name = name;
+        system = pkgs.hostPlatform.system;
+        builder = "${pkgs.bash}/bin/bash";
+        outputs = ["out"];
+        # The args are ignored in `nix develop`, but we need to create an output
+        # to pass CI, which just builds the derivation.
+        args = ["-c" "echo -n '' > $out"];
+
+        # Explanation:
+        #
+        # `nix develop` builds a modified version of this derivation that changes
+        # the derivation args to `get-env.sh`, a script packaged with `nix` itself.
+        # It then builds the modified derivation, which runs `get-env.sh` with our
+        # envs/packages/shellHook.
+        #
+        # 1. `get-env.sh` looks for a `$stdenv` env and runs `source $stdenv/setup`.
+        #    -> we make this just dump all envs to $out
+        # 2. `get-env.sh` looks for an `$outputs` env and for each output reads all
+        #    the serialized envs from each line, returning them in a form that
+        #    `nix develop` understands.
+        #
+        # So we add a `$stdenv` env which points to a directory containing a `setup`
+        # bash script. This script then prints out the final envs to `$out`.
+        stdenv = "${./min-stdenv}";
+
+        packages = packages;
+        shellHook = shellHook;
+      }
+      // envClean);
+
   #
   # app
   #
