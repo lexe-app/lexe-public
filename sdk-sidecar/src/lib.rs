08aea97629ded127e821ceca2f01e14f266627ea
Philip Kannegaard Hayes <philiphayes9@gmail.com>
2025-04-15 20:31:02 -0700

    sdk: sidecar: init

---
+++ sdk-sidecar/src/lib.rs
@@ -0,0 +1,197 @@
+//! # Lexe SDK sidecar
+//!
+//! ## Overview
+//!
+//! This module contains the API sidecar for the Lexe SDK. It runs as a separate
+//! process/sidecar and manages the connection to the user's Lexe node. The
+//! sidecar handles provisioning new node versions, user node auth, and
+//! connecting to the node via mTLS, while presenting a simplified API to the
+//! SDK user.
+//!
+//! The sidecar is stateless, stores no data on-disk, and does no internal
+//! caching. This helps reduce complexity and makes it easier to deploy and
+//! manage.
+//!
+//! ## Why a sidecar?
+//!
+//! The sidecar lets us avoid building full language bindings / SDKs for each
+//! language/platform we want to support. We don't currently have enough
+//! engineering resources to tackle per-language SDKs, so we've opted for a
+//! sidecar with a simplified REST API in the meantime.
+//!
+//! The sidecar also handles the tricky remote attestation process and
+//! mTLS-in-TLS connection to the remote user node enclave.
+
+// Full app API:
+//  GET  /app/node_info
+//  GET  /app/list_channels
+// POST  /app/sign_message
+// POST  /app/verify_message
+// POST  /app/open_channel
+// POST  /app/preflight_open_channel
+// POST  /app/close_channel
+// POST  /app/preflight_close_channel
+// POST  /app/create_invoice
+// POST  /app/pay_invoice
+// POST  /app/preflight_pay_invoice
+// POST  /app/create_offer
+// POST  /app/pay_offer
+// POST  /app/preflight_pay_offer
+// POST  /app/get_address
+// POST  /app/pay_onchain
+// POST  /app/preflight_pay_onchain
+// POST  /app/payments/indexes
+//  GET  /app/payments/new
+//  PUT  /app/payments/note
+//
+// Basic prototype sdk API:
+//  GET  /sdk/health
+//  GET  /sdk/node_info
+// POST  /sdk/create_invoice
+// POST  /sdk/pay_invoice
+// POST  /sdk/preflight_pay_invoice
+// POST  /sdk/payments/indexes
+//  GET  /sdk/payments/new
+//  PUT  /sdk/payments/note
+
+// design decisions:
+//
+// - provide the sidecar as a
+//   1. docker container (amd64, arm64)
+//   2. standalone binary (x86_64, aarch64)
+//
+// - if it has root seed available, sidecar does NOT need a data volume
+//   + we can just blindly reprovision at startup
+//     + also reprovision automatically every day, handles long running sidecar
+// - if we don't have root seed available (fine-grained auth), we can
+//   + (1) bad - blindly trust whatever node version is currently provisioned.
+//     allows rollbacks to vulnerable versions.
+//   + (2) prone to outages - only trust explicit `TRUSTED_NODE_MEASUREMENT`.
+//     imagine app provisions a new version, the sidecar won't be synced on the
+//     measurement and will fail to connect.
+//   + (3) alright - trust any node version as long as it's >= a version we
+//     currently trust.
+//
+// - if we remove the gDrive requirement, a user could also operate without a
+//   mobile app by generating the root seed locally and having the sidecar
+//   attempt to signup on startup.
+//
+// - codegen an OpenAPI spec from the rust code to build our docs site and/or
+//   client libraries
+//
+//   + Use `utoipa` / `utoipa-axum` crates
+//     + ```rust
+//     | #[derive(Serialize, Deserialize)]
+//     | #[cfg_attr(feature = "openapi", derive(ToSchema))]`
+//     | pub struct NodeInfo {
+//     |     // ..
+//     | }
+//     | ```
+//
+//     + ```rust
+//     | #[utoipa::path(get, path="/sdk/node_info")]
+//     | async fn node_info(
+//     |     State(state): State<Arc<SidecarState>>,
+//     | ) -> Result<NodeInfo> {
+//     |     // ..
+//     | }
+//     | ```
+//
+//   + use <https://scalar.com/> for API docs
+//     + <https://www.npmjs.com/package/@scalar/api-reference>
+//     + ```html
+//     | <html>
+//     | <head></head>
+//     | <body>
+//     |   <script id="api-reference" type="application/json">
+//     |     /* the OpenAPI spec json */
+//     |   </script>
+//     |   <script src="https://cdn.jsdelivr.net/npm/@scalar/api-reference"></script>
+//     | </body>
+//     | </html>
+//     | ```
+//
+//   + plug openapi specs into postman
+//     + <https://learning.postman.com/docs/integrations/available-integrations/working-with-openAPI/>
+//
+// - envs:
+//   - (mandatory)
+//   + ROOT_SEED_PATH=<path/root_seed> (default=/run/secrets/root_seed)
+//   + API_KEY_PATH=<path/api_key> (default=/run/secrets/api_key)
+//
+//   - (optional)
+//   + LISTEN_ADDR=<ip:port> (default=[::1]:8080)
+//   + NETWORK=<mainnet|testnet|regtest> (default=mainnet)
+//   + DEPLOY_ENV=<prod|staging|dev> (default=prod)
+//   + TRUSTED_NODE_MEASUREMENT=<measurement> (optional)
+//
+// - future:
+//   + fine-grained auth and non-root-seed derived mTLS to node so users don't
+//     need to unsafely export their root seed.
+//   + actual (python, typescript) language bindings that directly connect to
+//     the node.
+//
+// usage:
+//
+// Run the sidecar as a standalone binary:
+//
+// ```bash
+// $ cargo install lexe-sdk-sidecar
+// $ mkdir .secrets
+// $ head -c 16 /dev/urandom | base64 | tr -d '\n' > .secrets/api_key \
+//     && chmod 600 .secrets/api_key
+//
+// # TODO(phlip9): copying root seed from mobile app is not secure...
+// # (macOS)
+// $ pbpaste > .secrets/root_seed
+// # (Linux)
+// $ wl-paste -n > .secrets/root_seed
+// $ chmod 600 .secrets/root_seed
+//
+// $ ROOT_SEED_PATH=<.secrets/root_seed> API_KEY_PATH=<.secrets/api_key> \
+//     lexe-sdk-sidecar
+// ```
+//
+// Run the sidecar as a docker container:
+//
+// ```bash
+// # TODO(phlip9): copying root seed from mobile app is not secure...
+// # (macOS)
+// $ pbpaste | docker secret create root_seed -
+// # (Linux)
+// $ wl-paste -n | docker secret create root_seed -
+//
+// $ head -c 16 /dev/urandom | base64 | tr -d '\n' | docker secret create api_key -
+// $ docker service create \
+//     --name lexe-sdk-sidecar \
+//     --secret api_key \
+//     --secret root_seed \
+//     lexe/sdk-sidecar:latest
+// ```
+//
+// Run as docker compose:
+//
+// ```yaml
+// services:
+//   lexe:
+//     image: lexe/sdk-sidecar:latest
+//     ports:
+//       - "8080:8080"
+//     secrets:
+//       - root_seed
+//       - api_key
+//
+// secrets:
+//   root_seed:
+//     file: .secrets/root_seed
+//   api_key:
+//     file: .secrets/api_key
+// ```
+//
+// test:
+//
+// ```bash
+// $ curl http://localhost:8080/sdk/node_info \
+//     --accept "application/json" \
+//     --header "x-api-key: $(cat .secrets/api_key)"
+// ```
