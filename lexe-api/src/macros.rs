17ff7cbdf6ed7ba65757d9cdea647e6b32e874c3
Philip Kannegaard Hayes <philiphayes9@gmail.com>
2025-04-16 18:42:17 -0700

    api: add `lexe_api::user_agent_internal!()` macro

--- lexe-api/src/macros.rs
+++ lexe-api/src/macros.rs
@@ -1,3 +1,14 @@
+/// Build the user agent string used for requests to internal services at
+/// compile time.
+///
+/// Example: "node-v0.6.20".
+#[macro_export]
+macro_rules! user_agent_internal {
+    () => {
+        concat!(env!("CARGO_PKG_NAME"), "-v", env!("CARGO_PKG_VERSION"))
+    };
+}
+
 /// Build the user agent string used for requests to external services at
 /// compile time.
 ///
@@ -5,11 +16,6 @@
 #[macro_export]
 macro_rules! user_agent_external {
     () => {
-        concat!(
-            "lexe-",
-            env!("CARGO_PKG_NAME"),
-            "-v",
-            env!("CARGO_PKG_VERSION")
-        )
+        concat!("lexe-", $crate::user_agent_internal!())
     };
 }
