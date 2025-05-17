467d5e937368a9ec56261b41cb5f08b16634b7de
Max Fang <protonmail.com@maxfa.ng>
2025-05-18 03:26:24 +0800

    root: Create `lexe-std` crate

---
+++ lexe-std/src/lib.rs
@@ -0,0 +1,7 @@
+//! # `lexe-std`
+//!
+//! This crate contains "std extensions" which other Lexe crates can use without
+//! having to pull in any dependencies.
+//!
+//! Traits, macros, copies of unstable `std` APIs, a small number of types, are
+//! all fair game so long as they do NOT depend on anything outside of [`std`].
