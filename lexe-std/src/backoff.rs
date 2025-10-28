856abd6ae9fe1267f64499b6c69e07674e59a26a
Philip Kannegaard Hayes <philiphayes9@gmail.com>
2025-10-28 15:33:10 -0700

    std: allow resetting backoff and customizing max wait

--- lexe-std/src/backoff.rs
+++ lexe-std/src/backoff.rs
@@ -1,10 +1,8 @@
 use std::{cmp::min, time::Duration};
 
 const INITIAL_WAIT_MS: u64 = 250;
-const MAXIMUM_WAIT_MS: u64 = 32_000;
+const MAX_WAIT_MS: u64 = 32_000;
 const EXP_BASE: u64 = 2;
-
-crate::const_assert!(INITIAL_WAIT_MS != 0);
 
 /// Get a iterator of [`Duration`]s which can be passed into e.g.
 /// `tokio::time::sleep` to observe time-based exponential backoff.
@@ -19,27 +17,67 @@ crate::const_assert!(INITIAL_WAIT_MS != 0);
 /// }
 /// # }
 /// ```
-pub fn get_backoff_iter() -> impl Iterator<Item = Duration> {
-    iter_with_initial_wait_ms(INITIAL_WAIT_MS)
+pub fn get_backoff_iter() -> Backoff {
+    Backoff::default()
 }
 
 /// Like [`get_backoff_iter`], but allows specifying the initial wait time in
 /// milliseconds.
-// Haven't seen a good use case for customizing the maximum wait time yet, so
-// for now we don't expose `maximum_wait_ms` as a parameter.
-pub fn iter_with_initial_wait_ms(
-    initial_wait_ms: u64,
-) -> impl Iterator<Item = Duration> {
+pub fn iter_with_initial_wait_ms(initial_wait_ms: u64) -> Backoff {
     // The initial wait being greater than the maximum wait won't cause any
     // problems, but the programmer probably didn't intend this.
-    debug_assert!(initial_wait_ms <= MAXIMUM_WAIT_MS);
+    debug_assert!(initial_wait_ms <= MAX_WAIT_MS);
 
-    (0u32..).map(move |index| {
-        let factor = EXP_BASE.saturating_pow(index);
-        let wait_ms = initial_wait_ms.saturating_mul(factor);
-        let bounded_wait_ms = min(wait_ms, MAXIMUM_WAIT_MS);
+    Backoff {
+        initial_wait_ms,
+        ..Backoff::default()
+    }
+}
+
+/// Exponential backoff iterator yielding [`Duration`]s.
+///
+/// ```text
+/// delay_i := min(2^i * initial_wait_ms, max_wait_ms)
+/// ```
+pub struct Backoff {
+    pub initial_wait_ms: u64,
+    pub max_wait_ms: u64,
+    pub attempt: u32,
+}
+
+impl Backoff {
+    /// Reset the backoff to the initial wait delay.
+    pub fn reset(&mut self) {
+        self.attempt = 0;
+    }
+
+    /// Get the next delay duration according to the exponential backoff.
+    pub fn next_delay(&mut self) -> Duration {
+        let factor = EXP_BASE.saturating_pow(self.attempt);
+        let wait_ms = self.initial_wait_ms.saturating_mul(factor);
+        let bounded_wait_ms = min(wait_ms, self.max_wait_ms);
+        self.attempt = self.attempt.saturating_add(1);
         Duration::from_millis(bounded_wait_ms)
-    })
+    }
+}
+
+impl Default for Backoff {
+    fn default() -> Self {
+        Self {
+            initial_wait_ms: INITIAL_WAIT_MS,
+            max_wait_ms: MAX_WAIT_MS,
+            attempt: 0,
+        }
+    }
+}
+
+impl Iterator for Backoff {
+    type Item = Duration;
+
+    #[inline]
+    fn next(&mut self) -> Option<Self::Item> {
+        Some(self.next_delay())
+    }
 }
 
 #[cfg(test)]
