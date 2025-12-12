dcb7480407008a2581d941d7ed6e256bc7ed8f76
Philip Kannegaard Hayes <philiphayes9@gmail.com>
2025-12-11 20:02:38 -0800

    sdk: py: successfully calling gateway

--- sdk/python/tests/test_all.py
+++ sdk/python/tests/test_all.py
@@ -4,5 +4,13 @@ import lexe
 def test_add():
     assert lexe.add(1, 2) == 3
 
-def test_hex_encode():
-    assert lexe.hex_encode(b"hello") == "68656c6c6f"
+@pytest.mark.skip(reason="Requires network access")
+async def test_gateway_client_latest_enclave():
+    deploy_env = lexe.DeployEnv.STAGING
+    gateway_url = "https://lexe-staging-sgx.uswest2.staging.lexe.app"
+    user_agent = "sdk-python/0.1.0"
+    client = lexe.GatewayClient(deploy_env, gateway_url, user_agent)
+    enclave = await client.latest_enclave()
+
+    print("enclave:", enclave)
+    assert enclave != ""
--- sdk/src/lib.rs
+++ sdk/src/lib.rs
@@ -1,3 +1,12 @@
+//! Lexe SDK
+
+use std::fmt;
+
+use anyhow::Context;
+use common::env::DeployEnv as DeployEnvRs;
+use lexe_api_core::{def::AppGatewayApi, error::GatewayApiError};
+use node_client::client::GatewayClient as GatewayClientRs;
+
 uniffi::setup_scaffolding!("lexe");
 
 #[uniffi::export]
@@ -5,25 +14,75 @@ fn add(a: u32, b: u32) -> u32 {
     a + b
 }
 
-pub mod hex {
-    #[uniffi::export]
-    pub fn hex_encode(data: &[u8]) -> String {
-        hex::encode(data)
+#[derive(uniffi::Enum)]
+pub enum DeployEnv {
+    Dev,
+    Staging,
+    Prod,
+}
+
+impl From<DeployEnv> for DeployEnvRs {
+    fn from(env: DeployEnv) -> Self {
+        match env {
+            DeployEnv::Dev => Self::Dev,
+            DeployEnv::Staging => Self::Staging,
+            DeployEnv::Prod => Self::Prod,
+        }
     }
 }
 
-// #[uniffi::export(async_runtime = "tokio")]
-// pub async fn latest_enclave() -> Result<String, AnyhowError> {
-//     let deploy_env = DeployEnv::Staging;
-//     let gateway_url = "https://lexe-staging-sgx.uswest2.staging.lexe.app";
-//     let user_agent = "sdk-python/0.1.0";
-//     let client = app_rs::client::GatewayClient::new(
-//         deploy_env,
-//         gateway_url.to_string(),
-//         user_agent,
-//     )?;
-//
-//     let enclaves = client.current_enclaves().await.context("oops")?.enclaves;
-//
-//     Ok(format!("{enclaves:?}"))
-// }
+#[derive(Debug, uniffi::Object)]
+pub struct FfiError {
+    message: String,
+}
+
+impl std::error::Error for FfiError {}
+impl From<anyhow::Error> for FfiError {
+    fn from(err: anyhow::Error) -> Self {
+        Self {
+            message: format!("{err:#}"),
+        }
+    }
+}
+impl From<GatewayApiError> for FfiError {
+    fn from(err: GatewayApiError) -> Self {
+        Self {
+            message: format!("{err:#}"),
+        }
+    }
+}
+impl fmt::Display for FfiError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.message)
+    }
+}
+
+pub type FfiResult<T> = std::result::Result<T, FfiError>;
+
+/// A client for interacting with the Lexe API Gateway.
+#[derive(uniffi::Object)]
+pub struct GatewayClient {
+    inner: GatewayClientRs,
+}
+
+#[uniffi::export(async_runtime = "tokio")]
+impl GatewayClient {
+    /// Creates a new `GatewayClient` instance.
+    #[uniffi::constructor]
+    pub fn new(
+        deploy_env: DeployEnv,
+        gateway_url: String,
+        user_agent: String,
+    ) -> FfiResult<Self> {
+        let deploy_env = DeployEnvRs::from(deploy_env);
+        let client = GatewayClientRs::new(deploy_env, gateway_url, user_agent)?;
+        Ok(Self { inner: client })
+    }
+
+    /// Fetches the latest available node enclave versions from the gateway.
+    pub async fn latest_enclave(&self) -> FfiResult<String> {
+        let enclaves = self.inner.current_enclaves().await?;
+        let enclave = enclaves.latest().context("no latest enclave found")?;
+        Ok(format!("{enclave:?}"))
+    }
+}
